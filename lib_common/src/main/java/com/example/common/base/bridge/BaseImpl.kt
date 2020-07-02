087eb11508101a086ffb562a74efeb98c780b275
wyb <wangyanbin@fenxianglife.com>
2020-07-02 10:14:59 +0800

    修整分页工具类

--- lib_common/src/main/java/com/example/common/base/bridge/BaseImpl.kt
+++ lib_common/src/main/java/com/example/common/base/bridge/BaseImpl.kt
@@ -1,5 +1,7 @@
 package com.example.common.base.bridge
 
+import android.view.View
+
 /**
  * Created by WangYanBin on 2020/6/11.
  * 定义基类可以调取的一些方法
@@ -20,5 +22,45 @@ interface BaseImpl {
      * 初始化数据
      */
     fun initData()
+
+    /**
+     * 对象判空（批量）
+     */
+    fun isEmpty(vararg objs: Any?): Boolean
+
+    /**
+     * 虚拟键盘开启
+     */
+    fun openDecor(view: View?)
+
+    /**
+     * 虚拟键盘关闭
+     */
+    fun closeDecor()
+
+    /**
+     * 让一个view获得焦点
+     */
+    fun setViewFocus(view: View?)
+
+    /**
+     * 获取控件的基础值
+     */
+    fun getViewValue(view: View?): String?
+
+    /**
+     * 控件显示
+     */
+    fun VISIBLE(vararg views: View?)
+
+    /**
+     * 控件隐藏（占位）
+     */
+    fun INVISIBLE(vararg views: View?)
+
+    /**
+     * 控件隐藏（不占位）
+     */
+    fun GONE(vararg views: View?)
 
 }
--- lib_common/src/main/java/com/example/common/base/bridge/BaseView.kt
+++ lib_common/src/main/java/com/example/common/base/bridge/BaseView.kt
@@ -45,44 +45,4 @@ interface BaseView {
      */
     fun navigation(path: String, params: PageParams): Activity
 
-    /**
-     * 对象判空（批量）
-     */
-    fun isEmpty(vararg objs: Any?): Boolean
-
-    /**
-     * 虚拟键盘开启
-     */
-    fun openDecor(view: View?)
-
-    /**
-     * 虚拟键盘关闭
-     */
-    fun closeDecor()
-
-    /**
-     * 让一个view获得焦点
-     */
-    fun setViewFocus(view: View?)
-
-    /**
-     * 获取控件的基础值
-     */
-    fun getViewValue(view: View?): String?
-
-    /**
-     * 控件显示
-     */
-    fun VISIBLE(vararg views: View?)
-
-    /**
-     * 控件隐藏（占位）
-     */
-    fun INVISIBLE(vararg views: View?)
-
-    /**
-     * 控件隐藏（不占位）
-     */
-    fun GONE(vararg views: View?)
-
 }
