f932f63e029175814cc6f9c4e9dfc0cb70161485
wyb <wangyanbin@fenxianglife.com>
2020-07-08 16:33:56 +0800

    命名修整

--- app/src/main/java/com/example/mvvm/activity/MainActivity.java
+++ app/src/main/java/com/example/mvvm/activity/MainActivity.java
@@ -90,7 +90,6 @@ public class MainActivity extends BaseTitleActivity<ActivityMainBinding> {
                                     });
                                 }
                             });
-
                     break;
             }
         }
