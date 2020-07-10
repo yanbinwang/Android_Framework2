169c502413228f5d998144998a5e6de9bccbba75
wyb <wangyanbin@fenxianglife.com>
2020-07-10 16:01:20 +0800

    app内的进程类kt化

---
+++ app/src/main/java/com/example/mvvm/adapter/TestListAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.mvvm.adapter
+
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.viewholder.BaseDataBindingHolder
+import com.example.mvvm.R
+import com.example.mvvm.databinding.ItemTestBinding
+import com.example.mvvm.model.TestListModel
+
+/**
+ * Created by WangYanBin on 2020/7/10.
+ */
+class TestListAdapter : BaseQuickAdapter<TestListModel?, BaseDataBindingHolder<*>>(R.layout.item_test) {
+
+    override fun onItemViewHolderCreated(viewHolder: BaseDataBindingHolder<*>, viewType: Int) {
+        super.onItemViewHolderCreated(viewHolder, viewType)
+        DataBindingUtil.bind<ViewDataBinding>(viewHolder.itemView)
+    }
+
+    override fun convert(holder: BaseDataBindingHolder<*>, item: TestListModel?) {
+        if (item == null) {
+            return
+        }
+        //设置数据
+        val binding: ItemTestBinding = holder.dataBinding as ItemTestBinding
+        binding.model = item
+    }
+
+}
