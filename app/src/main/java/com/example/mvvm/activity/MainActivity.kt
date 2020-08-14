a849e1d49958663fce1d6381c2bf33a96f3c830b
wyb <wangyanbin@fenxianglife.com>
2020-08-14 11:44:53 +0800

    application改为kt化

--- app/src/main/java/com/example/mvvm/activity/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/activity/MainActivity.kt
@@ -1,66 +1,49 @@
-package com.example.mvvm.activity;
+package com.example.mvvm.activity
 
-
-import android.view.View;
-
-import androidx.lifecycle.Observer;
-
-import com.alibaba.android.arouter.facade.annotation.Route;
-import com.example.base.utils.ToastUtil;
-import com.example.common.base.BaseTitleActivity;
-import com.example.common.bus.LiveDataBus;
-import com.example.common.constant.ARouterPath;
-import com.example.common.constant.Constants;
-import com.example.mvvm.BR;
-import com.example.mvvm.MyApplication;
-import com.example.mvvm.R;
-import com.example.mvvm.databinding.ActivityMainBinding;
+import android.view.View
+import androidx.lifecycle.Observer
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.example.common.base.BaseTitleActivity
+import com.example.common.bus.LiveDataBus
+import com.example.common.constant.ARouterPath
+import com.example.common.constant.Constants
+import com.example.mvvm.BR
+import com.example.mvvm.R
+import com.example.mvvm.databinding.ActivityMainBinding
 
 /**
+ * Created by WangYanBin on 2020/8/14.
  * MVVM中，Activity的代码量应该非常少，它并不和数据及控件直接交互，可不写页面的ViewModel，但有xml存在的页面则必须传入Binding文件
  * 1）如果页面涉及到View操作必须在xml文件里套入layout和data标签，让系统生成对应的Binding文件传入基类注入
  * 2）不处理逻辑的代码以及从任何入口进到Activity内的数据放在Activity处理（例如上一个类传过来的数据，事件点击---类似P层回调）其余都丢给ViewModel处理，然后通过LiveData回调给Activity
  * 3）需要处理逻辑的方法，例如网络请求获取的数据，都放在ViewModel中，涉及到UI刷新则通过回调让Activity去修改数据
  */
 @Route(path = ARouterPath.MainActivity)
-public class MainActivity extends BaseTitleActivity<ActivityMainBinding> {
+open class MainActivity : BaseTitleActivity<ActivityMainBinding>() {
 
-    @Override
-    protected int getLayoutResID() {
-        return R.layout.activity_main;
+    override fun getLayoutResID(): Int {
+        return R.layout.activity_main
     }
 
-    @Override
-    public void initView() {
-        super.initView();
-        titleBuilder.setTitle("10086").getDefault();
-        binding.setVariable(BR.event, new PageEvent());
-        ToastUtil.mackToastSHORT("dfsdfsd", MyApplication.instance.getApplicationContext());
+    override fun initView() {
+        super.initView()
+        titleBuilder.setTitle("10086").getDefault()
+        binding.setVariable(BR.event, PageEvent())
     }
 
-    @Override
-    public void initEvent() {
-        super.initEvent();
+    override fun initEvent() {
+        super.initEvent()
         //注册订阅
-        LiveDataBus.get().toFlowable(Constants.APP_USER_LOGIN_OUT, String.class).observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(String s) {
-                titleBuilder.setTitle(s).getDefault();
-            }
-        });
+        LiveDataBus.get().toFlowable(Constants.APP_USER_LOGIN_OUT, String::class.java)
+            .observe(this, Observer { s -> titleBuilder.setTitle(s!!).getDefault() })
     }
 
-    public class PageEvent {
-
-        public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.btn_login:
-                    navigation(ARouterPath.LoginActivity);
-                    break;
-                case R.id.btn_list:
-                    navigation(ARouterPath.TestListActivity);
-                    break;
-                case R.id.btn_download:
+    class PageEvent : MainActivity() {
+        fun onClick(v: View) {
+            when (v.id) {
+                R.id.btn_login -> navigation(ARouterPath.LoginActivity)
+                R.id.btn_list -> navigation(ARouterPath.TestListActivity)
+                R.id.btn_download -> {
 //                    PermissionHelper.with(context.get())
 //                            .getPermissions(Permission.Group.STORAGE)
 //                            .setPermissionCallBack(isGranted -> {
@@ -85,11 +68,9 @@ public class MainActivity extends BaseTitleActivity<ActivityMainBinding> {
 //                                    });
 //                                }
 //                            });
-
-                    break;
+                }
             }
         }
-
     }
 
 }
