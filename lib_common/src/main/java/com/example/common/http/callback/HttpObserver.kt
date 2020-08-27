c13b846787237d7bf1384850c2d5fbab7cf22f07
wyb <wangyanbin@fenxianglife.com>
2020-08-27 11:44:29 +0800

    下载协程顺序规整

--- lib_common/src/main/java/com/example/common/http/callback/HttpObserver.kt
+++ lib_common/src/main/java/com/example/common/http/callback/HttpObserver.kt
@@ -2,7 +2,7 @@ package com.example.common.http.callback
 
 /**
  * Created by WangYanBin on 2020/6/8.
- * 针对网络请求回调的解析处理，由于livedata本身并没有整体的生命周期监听，故手动封装了3个回调
+ * 针对网络请求回调的解析处理，手动封装了3个回调
  * 如果是框架内约定的格式，可以使用其子类HttpSubscriber，如果是访问外部地址，则使用该类
  */
 interface HttpObserver<T> {
