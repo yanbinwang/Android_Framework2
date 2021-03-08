90edd12cf150bc4d2bba8bbee705f71ba82c1c15
王彦彬 <wyb@dataqin.com>
2021-03-08 17:26:59 +0800

    动态添加空布局

--- lib_common/src/main/java/com/example/common/base/BaseTitleActivity.kt
+++ lib_common/src/main/java/com/example/common/base/BaseTitleActivity.kt
@@ -2,6 +2,7 @@ package com.example.common.base
 
 import android.view.View
 import androidx.databinding.ViewDataBinding
+import com.example.common.base.page.PageHandler
 import com.example.common.databinding.ActivityBaseBinding
 import com.example.common.utils.builder.TitleBuilder
 
@@ -12,12 +13,18 @@ import com.example.common.utils.builder.TitleBuilder
 abstract class BaseTitleActivity<VDB : ViewDataBinding> : BaseActivity<VDB>() {
     private val baseBinding by lazy { ActivityBaseBinding.inflate(layoutInflater) }
     protected val titleBuilder by lazy { TitleBuilder(this, baseBinding.titleContainer) } //标题栏
+    protected val emptyLayout by lazy { PageHandler.getEmpty(baseBinding.flBaseContainer) }
 
     // <editor-fold defaultstate="collapsed" desc="基类方法">
     override fun setContentView(view: View?) {
         baseBinding.flBaseContainer.addView(binding.root)
         super.setContentView(baseBinding.root)
     }
+
+//    override fun initView() {
+//        super.initView()
+//        emptyLayout?.showLoading()
+//    }
     // </editor-fold>
 
 }
