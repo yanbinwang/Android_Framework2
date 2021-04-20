67cb903347c4808b33027728cb5231d43903b062
王彦彬 <wyb@dataqin.com>
2021-04-20 10:18:45 +0800

    规整和排除一些依赖

--- lib_common/src/main/java/com/example/common/widget/xrecyclerview/refresh/callback/OnBottomRefreshListener.kt
+++ lib_common/src/main/java/com/example/common/widget/xrecyclerview/refresh/callback/OnBottomRefreshListener.kt
@@ -5,15 +5,15 @@ import com.example.common.widget.xrecyclerview.refresh.SwipeRefreshLayout
 /**
  * 重新定义刷新接口
  */
-interface OnXRefreshTopListener : SwipeRefreshLayout.OnRefreshListener {
+interface OnBottomRefreshListener : SwipeRefreshLayout.OnRefreshListener {
 
     override fun onRefresh(index: Int) {
-        onRefresh()
     }
 
     override fun onLoad(index: Int) {
+        onLoad()
     }
 
-    fun onRefresh()
+    fun onLoad()
 
 }
