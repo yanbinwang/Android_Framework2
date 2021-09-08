da11a259780063255a2328c2674c58d296685db1
lzh <Ub9g9SMr5w>
2021-09-08 10:09:54 +0800

    sdk升级

---
+++ lib_common/src/main/java/com/example/common/widget/advertising/callback/OnAdvertisingClickListener.kt
@@ -0,0 +1,11 @@
+package com.example.common.widget.advertising.callback
+
+/**
+ * Created by wangyanbin
+ * 广告条目点击(数组中的下标)
+ */
+interface OnAdvertisingClickListener {
+
+    fun onItemClick(index: Int)
+
+}
