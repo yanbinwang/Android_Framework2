74c027a91fcf9244a24f667223c9ba4baf6f3010
王彦彬 <wyb@dataqin.com>
2022-06-06 17:19:02 +0800

    sdk升级

--- lib_common/src/main/java/com/example/common/utils/file/download/DownloadFactory.kt
+++ lib_common/src/main/java/com/example/common/utils/file/download/DownloadFactory.kt
@@ -21,10 +21,6 @@ import kotlin.coroutines.CoroutineContext
  * 故下载的完成回调需要在线程内外做判断
  */
 class DownloadFactory private constructor(override val coroutineContext: CoroutineContext) : CoroutineScope {
-//    class DownloadFactory private constructor() : CoroutineScope {
-//    override val coroutineContext: CoroutineContext
-//        get() = (Dispatchers.Main)
-    private var job: Job? = null
 
     companion object {
         @JvmStatic
@@ -38,61 +34,7 @@ class DownloadFactory private constructor(override val coroutineContext: Corouti
             ToastUtil.mackToastSHORT("链接地址不合法", BaseApplication.instance?.applicationContext!!)
             return
         }
-//        launch(Dispatchers.Main) {
-//            //清除目录下的所有文件
-//            FileUtil.deleteDir(filePath)
-//            //开启一个获取下载对象的协程，监听中如果对象未获取到，则中断携程，并且完成这一次下载
-//            val body = getDownloadApi(downloadUrl).call(object : ResourceSubscriber<ResponseBody>() {
-//                    override fun onStart() {
-//                        super.onStart()
-//                        onDownloadListener?.onStart()
-//                    }
-//
-//                    override fun onNext(t: ResponseBody?) {
-//                        super.onNext(t)
-//                        if (null == t) onError(null)
-//                    }
-//
-//                    override fun onError(throwable: Throwable?) {
-//                        super.onError(throwable)
-//                        onDownloadListener?.apply {
-//                            onFailed(throwable)
-//                            onComplete()
-//                        }
-//                        cancel()
-//                    }
-//            })
-//            //在上一步协程成功并拿到对象后开始执行，创建一个安装的文件，开启io协程，写入
-//            val file = File(FileUtil.isExistDir(filePath), fileName)
-//            withContext(Dispatchers.IO) {
-//                var inputStream: InputStream? = null
-//                var fileOutputStream: FileOutputStream? = null
-//                try {
-//                    val buf = ByteArray(2048)
-//                    val total = body.contentLength()
-//                    inputStream = body.byteStream()
-//                    fileOutputStream = FileOutputStream(file)
-//                    var len: Int
-//                    var sum: Long = 0
-//                    while (((inputStream.read(buf)).also { len = it }) != -1) {
-//                        fileOutputStream.write(buf, 0, len)
-//                        sum += len.toLong()
-//                        val progress = (sum * 1.0f / total * 100).toInt()
-//                        withContext(Dispatchers.Main) { onDownloadListener?.onLoading(progress) }
-//                    }
-//                    fileOutputStream.flush()
-//                    withContext(Dispatchers.Main) { onDownloadListener?.onSuccess(file.path) }
-//                } catch (e: Exception) {
-//                    withContext(Dispatchers.Main) { onDownloadListener?.onFailed(e) }
-//                } finally {
-//                    inputStream?.close()
-//                    fileOutputStream?.close()
-//                    withContext(Dispatchers.Main) { onDownloadListener?.onComplete() }
-//                    cancel()
-//                }
-//            }
-//        }
-        job = launch {
+        launch {
             //清除目录下的所有文件
             FileUtil.deleteDir(filePath)
             //开启一个获取下载对象的协程，监听中如果对象未获取到，则中断携程，并且完成这一次下载
@@ -113,7 +55,7 @@ class DownloadFactory private constructor(override val coroutineContext: Corouti
                         onFailed(throwable)
                         onComplete()
                     }
-                    job?.cancel()
+                    cancel()
                 }
             })
             //在上一步协程成功并拿到对象后开始执行，创建一个安装的文件，开启io协程，写入
@@ -142,7 +84,7 @@ class DownloadFactory private constructor(override val coroutineContext: Corouti
                     inputStream?.close()
                     fileOutputStream?.close()
                     withContext(Dispatchers.Main) { onDownloadListener?.onComplete() }
-                    job?.cancel()
+                    cancel()
                 }
             }
         }
