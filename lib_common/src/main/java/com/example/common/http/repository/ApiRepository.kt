88391a65b21cef0d6431a3ab610680c16cba6a69
王彦彬 <wyb@dataqin.com>
2022-08-25 10:30:02 +0800

    sdk升级

--- lib_common/src/main/java/com/example/common/http/repository/ApiRepository.kt
+++ lib_common/src/main/java/com/example/common/http/repository/ApiRepository.kt
@@ -24,33 +24,27 @@ import kotlinx.coroutines.withContext
  */
 fun BaseViewModel.launch(block: suspend CoroutineScope.() -> Unit) = viewModelScope.launch(block = block)
 
-//fun BaseViewModel.async(block: suspend CoroutineScope.() -> Unit) = viewModelScope.async(block = block)
-
 fun Fragment.launch(block: suspend CoroutineScope.() -> Unit) = lifecycleScope.launch(block = block)
-
-//fun Fragment.async(block: suspend CoroutineScope.() -> Unit) = lifecycleScope.async(block = block)
 
 fun AppCompatActivity.launch(block: suspend CoroutineScope.() -> Unit) = lifecycleScope.launch(block = block)
 
-//fun AppCompatActivity.async(block: suspend CoroutineScope.() -> Unit) = lifecycleScope.async(block = block)
-
-/**
- * 针对项目请求编号处理,需要处理的在请求文件里书写此扩展函数
- */
-fun <T> ApiResponse<T>.invoke(): ApiResponse<T> {
-    when (code) {
-        //账号还没有登录，解密失败，重新获取
-        100005, 100008 -> {
-            AccountHelper.signOut()
-            ARouter.getInstance().build(ARouterPath.LoginActivity).navigation()
-        }
-        //账号被锁定--进入账号锁定页（其余页面不关闭）
-        100002 -> {
-//                         ARouter.getInstance().build(ARouterPath.UnlockIPActivity).navigation()
-        }
-    }
-    return this
-}
+///**
+// * 针对项目请求编号处理,需要处理的在请求文件里书写此扩展函数
+// */
+//fun <T> ApiResponse<T>.invoke(): ApiResponse<T> {
+//    when (code) {
+//        //账号还没有登录，解密失败，重新获取
+//        100005, 100008 -> {
+//            AccountHelper.signOut()
+//            ARouter.getInstance().build(ARouterPath.LoginActivity).navigation()
+//        }
+//        //账号被锁定--进入账号锁定页（其余页面不关闭）
+//        100002 -> {
+////                         ARouter.getInstance().build(ARouterPath.UnlockIPActivity).navigation()
+//        }
+//    }
+//    return this
+//}
 
 /**
  * 项目请求监听扩展
@@ -70,6 +64,15 @@ suspend fun <T> T.call(subscriber: ResourceSubscriber<T>?): T {
         subscriber?.onComplete()
     }
     return this
+}
+
+/**
+ *  部分请求需要监听开始和结束，采用此请求结构
+ */
+suspend fun <T> execute(block: T, subscriber: ResourceSubscriber<T>?) {
+    subscriber?.onStart()
+    block.call(subscriber)
+    subscriber?.onComplete()
 }
 
 ///**
