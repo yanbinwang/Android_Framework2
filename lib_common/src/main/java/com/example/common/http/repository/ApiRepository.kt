900fd9d2e1a0cb33919553a1a55005f2cbca500d
wangyanbin <wangyanbin_29@126.com>
2022-09-06 13:15:05 +0800

    修正

--- lib_common/src/main/java/com/example/common/http/repository/ApiRepository.kt
+++ lib_common/src/main/java/com/example/common/http/repository/ApiRepository.kt
@@ -17,14 +17,9 @@ import kotlinx.coroutines.withContext
 //------------------------------------针对协程返回的参数(协程只有成功和失败)------------------------------------
 /**
  * ViewModel的KTX库中具备扩展函数，但不能像继承CoroutineScope那样直接launch点出，这里再做一个扩展
- * CoroutineScope接受一个参数是线程的上下文，返回一个CoroutineScope对象
- * 顺序执行请求
  */
 fun BaseViewModel.launch(block: suspend CoroutineScope.() -> Unit) = viewModelScope.launch(block = block)
 
-/**
- * 并发执行多个请求时使用，通过await()可以拿取到最后得到的值
- */
 fun BaseViewModel.async(block: suspend CoroutineScope.() -> Unit) = viewModelScope.async(block = block)
 
 fun Fragment.launch(block: suspend CoroutineScope.() -> Unit) = lifecycleScope.launch(block = block)
@@ -36,7 +31,8 @@ fun AppCompatActivity.launch(block: suspend CoroutineScope.() -> Unit) = lifecyc
 fun AppCompatActivity.async(block: suspend CoroutineScope.() -> Unit) = lifecycleScope.async(block = block)
 
 /**
- * 请求的协程切换
+ * 请求的协程线程切换
+ * 切到子线程进行处理，请求失败会被catch，返回null
  */
 suspend fun <T> T?.call(): T? {
     try {
@@ -48,20 +44,6 @@ suspend fun <T> T?.call(): T? {
 }
 
 /**
- * 项目接口的解析
- */
-fun <T> ApiResponse<T>?.response(): T? {
-    return if (null != this) {
-        if (200 == code) {
-            if (null == data) Any() as T else data
-        } else {
-            if (408 == code) AccountHelper.signOut()
-            null
-        }
-    } else null
-}
-
-/**
  * 串行网络请求
  * 如需并行，直接调用async
  */
@@ -69,7 +51,7 @@ fun <T> CoroutineScope.loadHttp(
     start: () -> Unit = {},
     request: suspend CoroutineScope.() -> ApiResponse<T>,
     resp: (T?) -> Unit = {},
-    err: (e: Pair<Int?, Exception?>?) -> Unit = {},
+    err: (e: Triple<Int?, String?, Exception?>?) -> Unit = {},
     end: () -> Unit = {},
     isShowToast: Boolean = false
 ) {
@@ -85,14 +67,28 @@ fun <T> CoroutineScope.loadHttp(
             val body = data.response()
             if (null != body) resp(body) else {
                 if (isShowToast) data.msg.doResponse()
-                err(Pair(data.code, Exception(data.msg)))
+                err(Triple(data.code, data.msg, null))
             }
         } catch (e: Exception) {
             if (isShowToast) "".doResponse()
-            err(Pair(-1, e))  //可根据具体异常显示具体错误提示
+            err(Triple(-1, "", e))  //可根据具体异常显示具体错误提示
         } finally {
             LogUtil.e("repository", "3:${Thread.currentThread().name}")
             end()
         }
     }
+}
+
+/**
+ * 项目接口返回对象的解析
+ */
+fun <T> ApiResponse<T>?.response(): T? {
+    return if (null != this) {
+        if (200 == code) {
+            if (null == data) Any() as T else data
+        } else {
+            if (408 == code) AccountHelper.signOut()
+            null
+        }
+    } else null
 }
