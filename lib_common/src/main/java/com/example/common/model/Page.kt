fa89c8c49ed0ec77c8a01c34e637afd206083b47
wangyanbin <wangyanbin_29@126.com>
2022-09-08 11:06:04 +0800

    修正

--- lib_common/src/main/java/com/example/common/model/Page.kt
+++ lib_common/src/main/java/com/example/common/model/Page.kt
@@ -4,7 +4,7 @@ package com.example.common.model
  *  Created by wangyanbin
  *  项目中分页统一外层套该类(合并部分接口带有data部分带有list)
  */
-class Page<T>(
+data class Page<T>(
     var code: Int = 0,
     var count: Int = 0,
     var data: MutableList<T> = ArrayList(),
