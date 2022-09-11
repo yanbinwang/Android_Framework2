5a77cb2473d54a210b32e5f0fce5ea9fa275e805
wangyanbin <wangyanbin_29@126.com>
2022-09-11 11:09:54 +0800

    sdk版本升级

--- lib_base/src/main/java/com/example/base/utils/function/view/FunctionsTextView.kt
+++ lib_base/src/main/java/com/example/base/utils/function/view/FunctionsTextView.kt
@@ -123,22 +123,16 @@ fun EditText.inhibitInputSpace() {
 /**
  * 简易Edittext监听
  */
-fun TextWatcher.textWatcher(vararg views: EditText) {
+fun SimpleTextWatcher.textWatcher(vararg views: EditText) {
     for (view in views) {
         view.addTextChangedListener(this)
-    }
-}
-
-fun textWatcher(listener:SimpleTextWatcher, vararg views: EditText) {
-    for (view in views) {
-        view.addTextChangedListener(listener)
     }
 }
 
 /**
  * 简易的输入监听
  */
-abstract class SimpleTextWatcher : TextWatcher {
+interface SimpleTextWatcher : TextWatcher {
     override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
     }
 
@@ -148,3 +142,26 @@ abstract class SimpleTextWatcher : TextWatcher {
     override fun afterTextChanged(s: Editable) {
     }
 }
+
+///**
+// * 简易Edittext监听
+// */
+//fun TextWatcher.textWatcher(vararg views: EditText) {
+//    for (view in views) {
+//        view.addTextChangedListener(this)
+//    }
+//}
+//
+///**
+// * 简易的输入监听
+// */
+//abstract class SimpleTextWatcher : TextWatcher {
+//    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+//    }
+//
+//    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+//    }
+//
+//    override fun afterTextChanged(s: Editable) {
+//    }
+//}
