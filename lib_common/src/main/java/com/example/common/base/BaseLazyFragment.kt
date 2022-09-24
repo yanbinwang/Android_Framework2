c97e27bd4c5265bccd670a405f56bebfb5a413e1
wangyanbin <wangyanbin_29@126.com>
2022-09-24 17:11:18 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/base/BaseLazyFragment.kt
+++ lib_common/src/main/java/com/example/common/base/BaseLazyFragment.kt
@@ -1,28 +1,22 @@
 package com.example.common.base
 
-import android.os.Bundle
 import androidx.databinding.ViewDataBinding
 
 /**
  * Created by WangYanBin on 2020/6/10.
  * 数据懒加载，当界面不可展示时，不执行加载数据的方法
  * 适配器构造方法中加入行为参数BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+ * 加载数据实现initLazyData方法，不要走initData
  */
 abstract class BaseLazyFragment<VDB : ViewDataBinding> : BaseFragment<VDB>() {
     private var isLoaded = false//是否被加载
 
     // <editor-fold defaultstate="collapsed" desc="基类方法">
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        initView()
-        initEvent()
-    }
-
     override fun onResume() {
         super.onResume()
         if (!isLoaded && !isHidden) {
-            initData()
             isLoaded = true
+            initLazyData()
         }
     }
 
@@ -30,6 +24,8 @@ abstract class BaseLazyFragment<VDB : ViewDataBinding> : BaseFragment<VDB>() {
         super.onDestroyView()
         isLoaded = false
     }
+
+    abstract fun initLazyData()
     // </editor-fold>
 
 }
