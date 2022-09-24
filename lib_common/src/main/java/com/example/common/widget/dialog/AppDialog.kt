64fa50e6e2e17dc74ece9291a10e7f181284c99b
wangyanbin <wangyanbin_29@126.com>
2022-09-24 14:00:35 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/widget/dialog/AppDialog.kt
+++ lib_common/src/main/java/com/example/common/widget/dialog/AppDialog.kt
@@ -6,6 +6,7 @@ import android.text.TextUtils
 import android.view.Gravity
 import android.view.View
 import android.view.WindowManager
+import com.example.base.utils.function.view.gone
 import com.example.common.base.BaseDialog
 import com.example.common.databinding.ViewDialogBinding
 
@@ -22,22 +23,22 @@ class AppDialog(context: Context) : BaseDialog<ViewDialogBinding>(context) {
         initialize(true, true)
     }
 
-    fun setParams(title: String? = "", message: String = "", positiveText: String = "", negativeText: String = "", center: Boolean = true): AppDialog {
+    fun setParams(title: String = "", message: String = "", positiveText: String = "", negativeText: String = "", center: Boolean = true): AppDialog {
         binding.apply {
             //如果没有传入标题字段,则隐藏标题view
-            if (TextUtils.isEmpty(title)) tvTip.visibility = View.GONE
+            if (TextUtils.isEmpty(title)) tvTip.gone()
             //如果没有传入取消字段,则隐藏取消view
             if (TextUtils.isEmpty(negativeText)) {
-                viewLine.visibility = View.GONE
-                tvCancel.visibility = View.GONE
+                viewLine.gone()
+                tvCancel.gone()
             }
             //文案方向
             tvContainer.gravity = if (center) Gravity.CENTER else Gravity.LEFT
             //对控件赋值
-            tvTip.text = if (TextUtils.isEmpty(title)) "" else title
-            tvContainer.text = if (TextUtils.isEmpty(message)) "" else message
-            tvSure.text = if (TextUtils.isEmpty(positiveText)) "" else positiveText
-            tvCancel.text = if (TextUtils.isEmpty(negativeText)) "" else negativeText
+            tvTip.text = title
+            tvContainer.text = message
+            tvSure.text = positiveText
+            tvCancel.text = negativeText
             //点击了取消按钮的回调
             tvCancel.setOnClickListener {
                 dismiss()
