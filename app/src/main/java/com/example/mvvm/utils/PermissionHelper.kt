e3fcfd02c84d641e28c1227dbd7b80557aa1cc2d
wangyanbin <wangyanbin_29@126.com>
2022-09-25 18:21:55 +0800

    sdk版本升级

--- app/src/main/java/com/example/mvvm/utils/PermissionHelper.kt
+++ app/src/main/java/com/example/mvvm/utils/PermissionHelper.kt
@@ -16,14 +16,22 @@ import java.text.MessageFormat
  * 权限库帮助类
  */
 class PermissionHelper(context: Context) {
+    val CALENDAR = arrayOf(Permission.READ_CALENDAR, Permission.WRITE_CALENDAR)
+    val CAMERA = arrayOf(Permission.CAMERA)
+    val CONTACTS = arrayOf(Permission.READ_CONTACTS, Permission.WRITE_CONTACTS, Permission.GET_ACCOUNTS)
+    val LOCATION = arrayOf(Permission.ACCESS_FINE_LOCATION, Permission.ACCESS_COARSE_LOCATION)
+    val MICROPHONE = arrayOf(Permission.RECORD_AUDIO)
+    val PHONE = arrayOf(Permission.READ_PHONE_STATE, Permission.CALL_PHONE, Permission.READ_CALL_LOG, Permission.WRITE_CALL_LOG, Permission.ADD_VOICEMAIL, Permission.USE_SIP, Permission.PROCESS_OUTGOING_CALLS)
+    val SENSORS = arrayOf(Permission.BODY_SENSORS)
+    val SMS = arrayOf(Permission.SEND_SMS, Permission.RECEIVE_SMS, Permission.READ_SMS, Permission.RECEIVE_WAP_PUSH, Permission.RECEIVE_MMS)
+    val STORAGE = arrayOf(Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE)
     private var denied = true
     private val context = WeakReference(context).get()!!
     private val permsGroup = arrayOf(
-        arrayOf(Permission.ACCESS_FINE_LOCATION, Permission.ACCESS_COARSE_LOCATION),//定位
-        arrayOf(Permission.CAMERA),//拍摄照片，录制视频
-        arrayOf(Permission.RECORD_AUDIO),//录制音频(腾讯x5)
-        arrayOf(Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE)//访问照片。媒体。内容和文件
-    )
+        LOCATION,//定位
+        CAMERA,//拍摄照片，录制视频
+        MICROPHONE,//录制音频(腾讯x5)
+        STORAGE)//访问照片。媒体。内容和文件
     private var onRequest: ((hasPermissions: Boolean) -> Unit)? = null
 
     //检测权限(默认拿全部，可单独拿某个权限组)
@@ -36,7 +44,7 @@ class PermissionHelper(context: Context) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             XXPermissions.with(context)
                 .permission(*groups)
-                .request(object :OnPermissionCallback{
+                .request(object : OnPermissionCallback {
                     override fun onGranted(permissions: MutableList<String>?, all: Boolean) {
                         //all->标记是否是获取部分权限成功，部分未正常授予，true全拿，false部分拿到
                         onRequest?.invoke(all)
@@ -63,8 +71,21 @@ class PermissionHelper(context: Context) {
                             }
                             //如果用户拒绝了开启权限
                             AndDialog.with(context)
-                                .setOnDialogListener({ XXPermissions.startPermissionActivity(context, permissions) })
-                                .setParams(context.getString(R.string.label_window_title), MessageFormat.format(context.getString(R.string.label_window_permission), rationale), context.getString(R.string.label_window_sure), context.getString(R.string.label_window_cancel))
+                                .setOnDialogListener({
+                                    XXPermissions.startPermissionActivity(
+                                        context,
+                                        permissions
+                                    )
+                                })
+                                .setParams(
+                                    context.getString(R.string.label_window_title),
+                                    MessageFormat.format(
+                                        context.getString(R.string.label_window_permission),
+                                        rationale
+                                    ),
+                                    context.getString(R.string.label_window_sure),
+                                    context.getString(R.string.label_window_cancel)
+                                )
                                 .show()
 //                            //被永久拒绝授权，请手动授予
 //                            if (never) {
@@ -83,7 +104,10 @@ class PermissionHelper(context: Context) {
     /**
      * 全局回调
      */
-    fun onRequest(onRequest: ((hasPermissions: Boolean) -> Unit), denied: Boolean = true): PermissionHelper {
+    fun onRequest(
+        onRequest: ((hasPermissions: Boolean) -> Unit),
+        denied: Boolean = true
+    ): PermissionHelper {
         this.onRequest = onRequest
         this.denied = denied
         return this
@@ -94,7 +118,11 @@ class PermissionHelper(context: Context) {
      */
     private fun checkSelfPermission(vararg permission: String): Boolean {
         for (perm in permission) {
-            if (PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(context, perm)) return false
+            if (PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(
+                    context,
+                    perm
+                )
+            ) return false
         }
         return true
     }
@@ -102,12 +130,14 @@ class PermissionHelper(context: Context) {
     /**
      * 定位权限组
      */
-    fun checkSelfLocation() = checkSelfPermission(Permission.ACCESS_FINE_LOCATION, Permission.ACCESS_COARSE_LOCATION)
+    fun checkSelfLocation() =
+        checkSelfPermission(Permission.ACCESS_FINE_LOCATION, Permission.ACCESS_COARSE_LOCATION)
 
     /**
      * 存储权限组
      */
-    fun checkSelfStorage() = checkSelfPermission(Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE)
+    fun checkSelfStorage() =
+        checkSelfPermission(Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE)
 
     companion object {
         @JvmStatic
