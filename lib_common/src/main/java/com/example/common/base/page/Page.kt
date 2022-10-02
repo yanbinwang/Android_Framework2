c8e54721b49e2d9612c19e2e4af6b9c8fa5e130d
wangyanbin <wangyanbin_29@126.com>
2022-10-02 14:43:27 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/base/page/Page.kt
+++ lib_common/src/main/java/com/example/common/base/page/Page.kt
@@ -5,9 +5,9 @@ package com.example.common.base.page
  *  项目中分页统一外层套该类(合并部分接口带有data部分带有list)
  */
 data class Page<T>(
-    var code: Int = 0,
-    var count: Int = 0,
-    var data: MutableList<T> = ArrayList(),
-    var list: MutableList<T> = ArrayList(),
+    var code: Int? = null,
+    var count: Int? = null,
+    var data: MutableList<T>? = null,
+    var list: MutableList<T>? = null,
     var obs: String? = null//图片域名
 )
--- lib_common/src/main/java/com/example/common/utils/builder/StatusBarBuilder.kt
+++ lib_common/src/main/java/com/example/common/utils/builder/StatusBarBuilder.kt
@@ -24,6 +24,20 @@ import com.example.common.constant.Constants
 class StatusBarBuilder(private val window: Window) {
 
     /**
+     * 全屏
+     */
+    fun fullScreen() {
+        val decorView = window.decorView
+        val uiOptions = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        decorView.systemUiVisibility = uiOptions
+    }
+
+    /**
      * 隐藏导航栏
      */
     fun hideStatusBar() = window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
