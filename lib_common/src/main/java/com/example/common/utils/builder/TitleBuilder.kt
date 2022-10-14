725a13f9a197b46a0ef4ee3d2c245df9404697cd
wangyanbin <wangyanbin_29@126.com>
2022-10-14 18:22:25 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/utils/builder/TitleBuilder.kt
+++ lib_common/src/main/java/com/example/common/utils/builder/TitleBuilder.kt
@@ -40,10 +40,8 @@ class TitleBuilder(private val activity: Activity, private val binding: ViewTitl
      */
     fun setTransparentTitle(title: String = "", titleColor: Int = R.color.grey_333333, light: Boolean = true): TitleBuilder {
         statusBarBuilder.transparent(light)
-        binding.clContainer.apply {
-            statusBarMargin()
-            setBackgroundColor(0)
-        }
+        binding.guideLine.visible()
+        binding.clContainer.setBackgroundColor(0)
         binding.tvTitle.setParam(title, titleColor)
         return this
     }
