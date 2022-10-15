93728d199a64def51473f73a775d498ed0195841
wangyanbin <wangyanbin_29@126.com>
2022-10-15 18:32:04 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/utils/builder/TitleBuilder.kt
+++ lib_common/src/main/java/com/example/common/utils/builder/TitleBuilder.kt
@@ -16,6 +16,17 @@ class TitleBuilder(private val activity: Activity, private val binding: ViewTitl
     private val statusBarBuilder by lazy { StatusBarBuilder(activity.window) }
 
     /**
+     * 由于5.0响应系统导航栏颜色样式，但是不响应电池样式，故而全局设定的颜色是黑色
+     * 初始化导航栏时，如果app是白色主题或者其余主题，在初始化时用代码修改导航栏颜色和黑白电池样式
+     */
+    init {
+        statusBarBuilder.apply {
+            statusBarLightMode(true)
+            statusBarColor(activity.color(R.color.white))
+        }
+    }
+
+    /**
      * 默认二级页面标题配置
      * title->标题
      * titleColor->标题颜色
@@ -26,8 +37,9 @@ class TitleBuilder(private val activity: Activity, private val binding: ViewTitl
     @JvmOverloads
     fun setTitle(title: String = "", titleColor: Int = R.color.grey_333333, bgColor: Int = R.color.white, light: Boolean = true, shade: Boolean = false): TitleBuilder {
         statusBarBuilder.apply {
-            statusBarLightMode(light)
-            statusBarColor(activity.color(bgColor))
+            //调取样式切换耗费资源，故而做一个检测
+            if(!light) statusBarLightMode(false)
+            if(bgColor!= R.color.white) statusBarColor(activity.color(bgColor))
         }
         binding.clContainer.setBackgroundColor(activity.color(bgColor))
         binding.tvTitle.setParam(title, titleColor)
