5d37e45571e8060f523c04371a8c8f6f259d0ae3
wangyanbin <wangyanbin_29@126.com>
2022-10-20 18:31:53 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/bus/Event.kt
+++ lib_common/src/main/java/com/example/common/bus/Event.kt
@@ -13,6 +13,7 @@ import java.io.Serializable
 class Event {
     private var action: String? = null //广播名称
     private var args: Bundle? = null //额外参数
+    private var clazz: Class<*>? = null
 
     /**
      * 不含任何参数的广播
@@ -26,6 +27,7 @@ class Event {
      */
     constructor(action: String, value: Boolean) {
         this.action = action
+        this.clazz = Boolean::class.java
         if (args == null) args = Bundle()
         args?.putBoolean(action, value)
     }
@@ -35,6 +37,7 @@ class Event {
      */
     constructor(action: String, value: Int) {
         this.action = action
+        this.clazz = Int::class.java
         if (args == null) args = Bundle()
         args?.putInt(action, value)
     }
@@ -44,6 +47,7 @@ class Event {
      */
     constructor(action: String, value: String) {
         this.action = action
+        this.clazz = String::class.java
         if (args == null) args = Bundle()
         args?.putString(action, value)
     }
@@ -53,6 +57,7 @@ class Event {
      */
     constructor(action: String, args: Bundle) {
         this.action = action
+        this.clazz = Bundle::class.java
         this.args = args
     }
 
@@ -61,6 +66,7 @@ class Event {
      */
     constructor(action: String, any: Serializable) {
         this.action = action
+        this.clazz = Serializable::class.java
         this.args = Bundle().apply { putSerializable(Extras.BUNDLE_BEAN, any) }
     }
 
@@ -69,6 +75,7 @@ class Event {
      */
     constructor(action: String, any: Parcelable) {
         this.action = action
+        this.clazz = Parcelable::class.java
         this.args = Bundle().apply { putParcelable(Extras.BUNDLE_BEAN, any) }
     }
 
@@ -84,7 +91,9 @@ class Event {
         return if (args == null) defaultValue else args?.getBoolean(action, defaultValue) ?: false
     }
 
-    //获取默认int值
+    /**
+     * 获取默认int值
+     */
     fun getInt(defaultValue: Int = 0): Int {
         return if (args == null) defaultValue else args?.getInt(action, defaultValue) ?: 0
     }
@@ -117,6 +126,18 @@ class Event {
     fun Event?.isEvent(action: String, block: () -> Unit): Event? {
         this ?: return null
         if (this.action == action) block()
+        return this
+    }
+
+    fun <K> Event?.isEvent(action: String, block: K?.() -> Unit): Event? {
+        this ?: return null
+        if (this.action == action) {
+            if (this.clazz == String::class.java) block(this.getString() as? K)
+            if (this.clazz == Int::class.java) block(this.getInt() as? K)
+            if (this.clazz == Boolean::class.java) block(this.getBoolean() as? K)
+            if (this.clazz == Serializable::class.java) block(this.getSerializable() as? K)
+            if (this.clazz == Parcelable::class.java) block(this.getParcelable() as? K)
+        }
         return this
     }
 
