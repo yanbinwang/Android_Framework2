21b7642400b93c0157ae6dde7e2b5475af1ae6d2
wangyanbin <wangyanbin_29@126.com>
2022-10-21 10:36:33 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/bus/Event.kt
+++ lib_common/src/main/java/com/example/common/bus/Event.kt
@@ -1,124 +1,21 @@
 package com.example.common.bus
 
-import android.os.Bundle
-import android.os.Parcelable
-import com.example.common.constant.Extras
-import java.io.Serializable
-
 /**
  * author: wyb
  * date: 2018/4/16.
  * 传递事件类
  */
-class Event {
-    private var action: String? = null //广播名称
-    private var args: Bundle? = null //额外参数
-    private var clazz: Class<*>? = null
+class Event(var action: String, var value: Any? = null) {
 
-    /**
-     * 不含任何参数的广播
-     */
-    constructor(action: String) {
+    fun setAction(action: String): Event {
         this.action = action
+        return this
     }
 
-    /**
-     * 带布尔类型的广播
-     */
-    constructor(action: String, value: Boolean) {
-        this.action = action
-        this.clazz = Boolean::class.java
-        if (args == null) args = Bundle()
-        args?.putBoolean(action, value)
+    fun setValue(value: Any?): Event {
+        this.value = value
+        return this
     }
-
-    /**
-     * 带int类型的广播
-     */
-    constructor(action: String, value: Int) {
-        this.action = action
-        this.clazz = Int::class.java
-        if (args == null) args = Bundle()
-        args?.putInt(action, value)
-    }
-
-    /**
-     * 带字符串类型的广播
-     */
-    constructor(action: String, value: String) {
-        this.action = action
-        this.clazz = String::class.java
-        if (args == null) args = Bundle()
-        args?.putString(action, value)
-    }
-
-    /**
-     * 带数据类的广播
-     */
-    constructor(action: String, args: Bundle) {
-        this.action = action
-        this.clazz = Bundle::class.java
-        this.args = args
-    }
-
-    /**
-     * 带对象的广播
-     */
-    constructor(action: String, any: Serializable) {
-        this.action = action
-        this.clazz = Serializable::class.java
-        this.args = Bundle().apply { putSerializable(Extras.BUNDLE_BEAN, any) }
-    }
-
-    /**
-     * 带对象的广播
-     */
-    constructor(action: String, any: Parcelable) {
-        this.action = action
-        this.clazz = Parcelable::class.java
-        this.args = Bundle().apply { putParcelable(Extras.BUNDLE_BEAN, any) }
-    }
-
-    /**
-     * 获取广播名
-     */
-    fun getAction() = action
-
-    /**
-     * 获取默认布尔值
-     */
-    fun getBoolean(defaultValue: Boolean = false): Boolean {
-        return if (args == null) defaultValue else args?.getBoolean(action, defaultValue) ?: false
-    }
-
-    /**
-     * 获取默认int值
-     */
-    fun getInt(defaultValue: Int = 0): Int {
-        return if (args == null) defaultValue else args?.getInt(action, defaultValue) ?: 0
-    }
-
-    /**
-     * 获取默认字符串值
-     */
-    fun getString(): String {
-        return if (args == null) "" else args?.getString(action) ?: ""
-    }
-
-    /**
-     * 获取默认类值
-     */
-    fun getBundle() = args
-
-    /**
-     * 获取默认对象
-     */
-    fun getSerializable() = args?.getSerializable(Extras.BUNDLE_BEAN)
-
-    /**
-     * 获取默认对象
-     */
-    fun <T : Parcelable> getParcelable() = args?.getParcelable<T>(Extras.BUNDLE_BEAN)
 
     /**
      * 是否是当前对象
@@ -129,15 +26,9 @@ class Event {
         return this
     }
 
-    fun <K> Event?.isEvent(action: String, block: K?.() -> Unit): Event? {
+    fun <T> Event?.isEvent(action: String, block: T?.() -> Unit): Event? {
         this ?: return null
-        if (this.action == action) {
-            if (this.clazz == String::class.java) block(this.getString() as? K)
-            if (this.clazz == Int::class.java) block(this.getInt() as? K)
-            if (this.clazz == Boolean::class.java) block(this.getBoolean() as? K)
-            if (this.clazz == Serializable::class.java) block(this.getSerializable() as? K)
-            if (this.clazz == Parcelable::class.java) block(this.getParcelable() as? K)
-        }
+        if (this.action == action) block(value as? T)
         return this
     }
 
--- lib_common/src/main/java/com/example/common/bus/EventBus.kt
+++ lib_common/src/main/java/com/example/common/bus/EventBus.kt
@@ -1,12 +1,9 @@
 package com.example.common.bus
 
-import android.os.Bundle
 import android.os.Looper
-import android.os.Parcelable
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import java.io.Serializable
 
 /**
  * author: wyb
@@ -14,6 +11,12 @@ import java.io.Serializable
  * 传递事件类
  */
 class EventBus private constructor() {
+
+    companion object {
+        @JvmStatic
+        val instance by lazy { EventBus() }
+    }
+
     private val busDefault get() = org.greenrobot.eventbus.EventBus.getDefault()
 
     fun register(subscriber: Any) = run { if (!busDefault.isRegistered(subscriber)) busDefault.register(subscriber) }
@@ -29,40 +32,11 @@ class EventBus private constructor() {
         }
     }
 
-    companion object {
-        @JvmStatic
-        val instance by lazy { EventBus() }
-    }
-
 }
 
 /**
  * 单独某一个对象发送
  */
-fun String.post() = EventBus.instance.post(Event(this))
+fun String.post(any: Any? = null) = EventBus.instance.post(this.event(any))
 
-fun String.post(value: Boolean) = EventBus.instance.post(Event(this, value))
-
-fun String.post(value: Int) = EventBus.instance.post(Event(this, value))
-
-fun String.post(value: String) = EventBus.instance.post(Event(this, value))
-
-fun String.post(args: Bundle) = EventBus.instance.post(Event(this, args))
-
-fun String.post(any: Serializable) = EventBus.instance.post(Event(this, any))
-
-fun String.post(any: Parcelable) = EventBus.instance.post(Event(this, any))
-
-fun String.event() = Event(this)
-
-fun String.event(value: Boolean) = Event(this, value)
-
-fun String.event(value: Int) = Event(this, value)
-
-fun String.event(value: String) = Event(this, value)
-
-fun String.event(args: Bundle) = Event(this, args)
-
-fun String.event(any: Serializable) = Event(this, any)
-
-fun String.event(any: Parcelable) = Event(this, any)
+fun String.event(any: Any? = null) = Event(this, any)
