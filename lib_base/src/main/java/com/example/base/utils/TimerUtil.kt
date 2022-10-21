28425698bcfc30596145470660378221d3c6fb69
wangyanbin <wangyanbin_29@126.com>
2022-10-21 13:17:56 +0800

    sdk版本升级

--- lib_base/src/main/java/com/example/base/utils/TimerUtil.kt
+++ lib_base/src/main/java/com/example/base/utils/TimerUtil.kt
@@ -3,17 +3,16 @@ package com.example.base.utils
 import android.os.CountDownTimer
 import android.os.Looper
 import java.util.*
+import java.util.concurrent.ConcurrentHashMap
 
 /**
  *  Created by wangyanbin
  *  时间工具类
  *  默认1秒，分计数和倒计时
- *  一个页面计时和倒计时有且只有一个存在，多种可考虑使用rxjava自带
  */
 object TimerUtil {
-    private var timer: Timer? = null
-    private var timerTask: TimerTask? = null
-    private var countDownTimer: CountDownTimer? = null
+    private val timerMap by lazy { ConcurrentHashMap<String, Pair<Timer?, TimerTask?>>() }
+    private val countDownMap by lazy { ConcurrentHashMap<String, CountDownTimer?>() }
     private val weakHandler by lazy { WeakHandler(Looper.getMainLooper()) }
 
     /**
@@ -22,10 +21,8 @@ object TimerUtil {
     @JvmOverloads
     @JvmStatic
     fun schedule(run: (() -> Unit)?, millisecond: Long = 1000) {
-        Timer().schedule(object : TimerTask() {
-            override fun run() {
-                weakHandler.post { run?.invoke() }
-            }
+        weakHandler.postDelayed({
+            run?.invoke()
         }, millisecond)
     }
 
@@ -34,64 +31,78 @@ object TimerUtil {
      */
     @JvmOverloads
     @JvmStatic
-    fun startTask(run: (() -> Unit)?, millisecond: Long = 1000) {
-        stopTask()
-        timer = Timer()
-        timerTask = object : TimerTask() {
-            override fun run() {
-                weakHandler.post { run?.invoke() }
+    fun startTask(tag: String = "TIMER_DEFULT", run: (() -> Unit)?, millisecond: Long = 1000) {
+        if (timerMap[tag] == null) {
+            val timer = Timer()
+            val timerTask = object : TimerTask() {
+                override fun run() {
+                    weakHandler.post { run?.invoke() }
+                }
             }
+            timerMap[tag] = timer to timerTask
         }
-        timer?.schedule(timerTask, 0, millisecond)
+        timerMap[tag]?.apply { first?.schedule(second, 0, millisecond) }
     }
 
     /**
      * 计时（累加）-结束
      */
     @JvmStatic
-    fun stopTask() {
-        timerTask?.cancel()
-        timer?.cancel()
-        timerTask = null
-        timer = null
+    fun stopTask(tag: String = "TIMER_DEFULT") {
+        timerMap[tag]?.apply {
+            first?.cancel()
+            second?.cancel()
+        }
+        timerMap.remove(tag)
     }
 
     /**
      * 倒计时-开始
      * second-秒
      */
-    @JvmOverloads
     @JvmStatic
-    fun startDownTask(onTick: ((second: Long) -> Unit)?, onFinish: (() -> Unit)?, second: Long = 1) {
-        stopDownTask()
-        countDownTimer = object : CountDownTimer(second * 1000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                onTick?.invoke((millisUntilFinished / 1000))
-            }
+    fun startDownTask(tag: String = "COUNT_DOWN_DEFULT", onTick: ((second: Long) -> Unit)?, onFinish: (() -> Unit)?, second: Long = 1) {
+        if (countDownMap[tag] == null) {
+            val countDownTimer = object : CountDownTimer(second * 1000, 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    onTick?.invoke((millisUntilFinished / 1000))
+                }
 
-            override fun onFinish() {
-                onFinish?.invoke()
-                stopDownTask()
+                override fun onFinish() {
+                    onFinish?.invoke()
+                    stopDownTask(tag)
+                }
             }
-        }.start()
+            countDownMap[tag] = countDownTimer
+        }
+        countDownMap[tag]?.start()
     }
 
     /**
      * 倒计时-结束
      */
     @JvmStatic
-    fun stopDownTask() {
-        countDownTimer?.cancel()
-        countDownTimer = null
+    fun stopDownTask(tag: String = "COUNT_DOWN_DEFULT") {
+        countDownMap[tag]?.cancel()
+        countDownMap.remove(tag)
     }
 
     /**
-     * 页面销毁时调取-如同时使用了俩种计时方法
+     * 完全销毁所有定时器
      */
     @JvmStatic
     fun destroy() {
-        stopTask()
-        stopDownTask()
+        for ((_, value) in timerMap) {
+            value.apply {
+                first?.cancel()
+                second?.cancel()
+            }
+        }
+        for ((_, value) in countDownMap) {
+            value?.cancel()
+        }
+        timerMap.clear()
+        countDownMap.clear()
     }
 
 }
