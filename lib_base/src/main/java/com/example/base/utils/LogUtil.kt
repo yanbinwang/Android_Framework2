85fe682b5dc32001a9a3bc6c79366bb14eabe1bb
wangyanbin <wangyanbin_29@126.com>
2022-10-22 23:50:03 +0800

    sdk版本升级

--- lib_base/src/main/java/com/example/base/utils/LogUtil.kt
+++ lib_base/src/main/java/com/example/base/utils/LogUtil.kt
@@ -6,54 +6,50 @@ import com.example.base.BuildConfig
 /**
  * 日志输出类
  */
+private const val TAG = "dota" // 默认的tag
+
 object LogUtil {
     private const val debug = BuildConfig.ISDEBUG
-    private const val TAG = "dota" // 默认的tag
 
     @JvmStatic
-    fun v(msg: String) = run { if (debug) Log.v(TAG, msg) }
+    fun v(tag: String = TAG, msg: String) = run { if (debug) Log.v(tag, msg) }
 
     @JvmStatic
-    fun v(tag: String, msg: String) = run { if (debug) Log.v(tag, msg) }
+    fun d(tag: String = TAG, msg: String) = run { if (debug) Log.d(tag, msg) }
 
     @JvmStatic
-    fun d(msg: String) = run { if (debug) Log.d(TAG, msg) }
+    fun i(tag: String = TAG, msg: String) = run { if (debug) Log.i(tag, msg) }
 
     @JvmStatic
-    fun d(tag: String, msg: String) = run { if (debug) Log.d(tag, msg) }
+    fun w(tag: String = TAG, msg: String) = run { if (debug) Log.w(tag, msg) }
 
     @JvmStatic
-    fun i(msg: String) = run { if (debug) Log.i(TAG, msg) }
-
-    @JvmStatic
-    fun i(tag: String, msg: String) = run { if (debug) Log.i(tag, msg) }
-
-    @JvmStatic
-    fun w(msg: String) = run { if (debug) Log.w(TAG, msg) }
-
-    @JvmStatic
-    fun w(tag: String, msg: String) = run { if (debug) Log.w(tag, msg) }
-
-    @JvmStatic
-    fun e(msg: String) = run { if (debug) Log.e(TAG, msg) }
-
-    @JvmStatic
-    fun e(tag: String, msg: String) = run { if (debug) Log.e(tag, msg) }
+    fun e(tag: String = TAG, msg: String) = run { if (debug) Log.e(tag, msg) }
 
     @JvmStatic
     fun wtf(msg: String) = run { if (debug) Log.println(Log.ASSERT, TAG, msg) }
 
     @JvmStatic
-    fun wtf(tag: String, msg: String) = run { if (debug) Log.println(Log.ASSERT, tag, msg) }
+    fun wtf(tag: String = TAG, msg: String) = run { if (debug) Log.println(Log.ASSERT, tag, msg) }
 
 }
 
-val String?.logV: Unit get() { if (this != null) LogUtil.v(this) }
+val String?.logV: Unit get() { if (this != null) LogUtil.v(msg = this) }
 
-val String?.logD: Unit get() { if (this != null) LogUtil.d(this) }
+fun String?.logV(tag: String = TAG) = run { if (this != null) LogUtil.v(tag, this) }
 
-val String?.logW: Unit get() { if (this != null) LogUtil.w(this) }
+val String?.logD: Unit get() { if (this != null) LogUtil.d(msg = this) }
 
-val String?.logE: Unit get() { if (this != null) LogUtil.e(this) }
+fun String?.logD(tag: String = TAG) = run { if (this != null) LogUtil.d(tag, this) }
 
-val String?.logWTF: Unit get() { if (this != null) LogUtil.wtf(this) }
+val String?.logW: Unit get() { if (this != null) LogUtil.w(msg = this) }
+
+fun String?.logW(tag: String = TAG) = run { if (this != null) LogUtil.w(tag, this) }
+
+val String?.logE: Unit get() { if (this != null) LogUtil.e(msg = this) }
+
+fun String?.logE(tag: String = TAG) = run { if (this != null) LogUtil.e(tag, this) }
+
+val String?.logWTF: Unit get() { if (this != null) LogUtil.wtf(msg = this) }
+
+fun String?.logWTF(tag: String = TAG) = run { if (this != null) LogUtil.wtf(tag, this) }
