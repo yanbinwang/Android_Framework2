b21933d0192f7e8316fbfef8485c690f02e5a240
wangyanbin <wangyanbin_29@126.com>
2022-11-02 16:50:29 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/utils/IndicatorBlockHelper.kt
+++ lib_common/src/main/java/com/example/common/utils/IndicatorBlockHelper.kt
@@ -1,10 +1,9 @@
-package com.example.common.widget
+package com.example.common.utils
 
 import com.example.base.utils.function.inflate
 import com.example.common.R
 import com.example.common.databinding.ItemTabBinding
 import com.example.common.utils.builder.TabLayoutBuilder
-import com.example.common.utils.setArguments
 import com.google.android.material.tabs.TabLayout
 
 /**
@@ -12,7 +11,7 @@ import com.google.android.material.tabs.TabLayout
  * 掏空系统tablayout，全部自定义
  * @author yan
  */
-class IndicatorLayout constructor(tab: TabLayout, tabTitle: MutableList<String>) : TabLayoutBuilder<String,ItemTabBinding>(tab, tabTitle) {
+class IndicatorBlockHelper constructor(tab: TabLayout, tabTitle: MutableList<String>) : TabLayoutBuilder<String,ItemTabBinding>(tab, tabTitle) {
 
     override fun getBindView() = ItemTabBinding.bind(context.inflate(R.layout.item_tab))
 
