1d8909402bf9ffe406436601e0dfeb312fee2625
wangyanbin <wangyanbin_29@126.com>
2022-11-02 17:15:51 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/utils/helper/MediatorBlockHelper.kt
+++ lib_common/src/main/java/com/example/common/utils/helper/MediatorBlockHelper.kt
@@ -1,9 +1,10 @@
-package com.example.common.utils
+package com.example.common.utils.helper
 
 import com.example.base.utils.function.inflate
 import com.example.common.R
 import com.example.common.databinding.ItemTabBinding
 import com.example.common.utils.builder.TabLayoutBuilder
+import com.example.common.utils.setArguments
 import com.google.android.material.tabs.TabLayout
 
 /**
@@ -11,7 +12,7 @@ import com.google.android.material.tabs.TabLayout
  * 掏空系统tablayout，全部自定义
  * @author yan
  */
-class IndicatorBlockHelper constructor(tab: TabLayout, tabTitle: MutableList<String>) : TabLayoutBuilder<String,ItemTabBinding>(tab, tabTitle) {
+class MediatorBlockHelper constructor(tab: TabLayout, tabTitle: MutableList<String>) : TabLayoutBuilder<String,ItemTabBinding>(tab, tabTitle) {
 
     override fun getBindView() = ItemTabBinding.bind(context.inflate(R.layout.item_tab))
 
