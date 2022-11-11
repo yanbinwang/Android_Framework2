7a2ebe11632b699c321739bee276d7acc2fbe647
wangyanbin <wangyanbin_29@126.com>
2022-11-11 09:59:17 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/base/BaseTitleActivity.kt
+++ lib_common/src/main/java/com/example/common/base/BaseTitleActivity.kt
@@ -10,13 +10,19 @@ import com.example.common.utils.builder.TitleBuilder
  * 带标题的基类，将整一个xml插入容器
  */
 abstract class BaseTitleActivity<VDB : ViewDataBinding> : BaseActivity<VDB>() {
-    protected val baseBinding by lazy { ActivityBaseBinding.inflate(layoutInflater) }
+    private val baseBinding by lazy { ActivityBaseBinding.inflate(layoutInflater) }
     protected val titleBuilder by lazy { TitleBuilder(this, baseBinding.titleContainer) } //标题栏
+    protected val viewGroup get() = baseBinding.flBaseContainer//标题页面的父容器，用于添加empty，如果不需要标题头的baseactivity，则在外层绘制一个FrameLayout
 
     // <editor-fold defaultstate="collapsed" desc="基类方法">
     override fun setContentView(view: View?) {
         baseBinding.flBaseContainer.addView(binding.root)
         super.setContentView(baseBinding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        baseBinding.unbind()
     }
     // </editor-fold>
 
