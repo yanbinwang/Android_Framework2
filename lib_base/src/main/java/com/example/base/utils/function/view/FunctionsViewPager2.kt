c4cb767887d91ec17b8c7cd6d8e4ab1cb85b2255
wangyanbin <wangyanbin_29@126.com>
2022-11-14 17:36:17 +0800

    sdk版本升级

--- lib_base/src/main/java/com/example/base/utils/function/view/FunctionsViewPager2.kt
+++ lib_base/src/main/java/com/example/base/utils/function/view/FunctionsViewPager2.kt
@@ -71,9 +71,7 @@ fun ViewPager2?.prevPage(isSmooth: Boolean = true) {
  */
 fun ViewPager2?.desensitization() {
     try {
-        val recyclerViewField = ViewPager2::class.java.getDeclaredField("mRecyclerView")
-        recyclerViewField.isAccessible = true
-        val recyclerView = recyclerViewField.get(this) as RecyclerView
+        val recyclerView = getRecyclerView()
         val touchSlopField = RecyclerView::class.java.getDeclaredField("mTouchSlop")
         touchSlopField.isAccessible = true
         val touchSlop = touchSlopField.get(recyclerView) as Int
