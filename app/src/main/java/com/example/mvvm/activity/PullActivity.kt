922fb40fca95e8ec8a56a9f79029cc15cc0493d6
wangyanbin <wangyanbin_29@126.com>
2022-11-28 14:25:37 +0800

    sdk版本升级

--- app/src/main/java/com/example/mvvm/activity/PullActivity.kt
+++ app/src/main/java/com/example/mvvm/activity/PullActivity.kt
@@ -1,9 +1,7 @@
 package com.example.mvvm.activity
 
 import android.os.Bundle
-import android.view.View
 import com.alibaba.android.arouter.facade.annotation.Route
-import com.example.base.utils.function.color
 import com.example.base.utils.function.view.click
 import com.example.common.base.BaseActivity
 import com.example.common.constant.ARouterPath
@@ -18,8 +16,6 @@ import com.example.mvvm.databinding.ActivityPullBinding
 class PullActivity : BaseActivity<ActivityPullBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-//        window.navigationBarColor = color(R.color.white)
-//        window.decorView.apply { systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION }
         super.onCreate(savedInstanceState)
 //        overridePendingTransition(R.anim.slide_in, R.anim.alpha_out)
         overridePendingTransition(R.anim.slide_in, R.anim.alpha_out)
@@ -27,13 +23,13 @@ class PullActivity : BaseActivity<ActivityPullBinding>() {
 
     override fun initEvent() {
         super.initEvent()
-//        binding.btnFinish.click { finish() }
-        binding.root.click { finish() }
+        binding.btnFinish.click { finish() }
+//        binding.root.click { finish() }
     }
 
     override fun finish() {
         super.finish()
-//        overridePendingTransition(0, R.anim.slide_out)
-        overridePendingTransition(0, 0)
+        overridePendingTransition(0, R.anim.slide_out)
+//        overridePendingTransition(0, 0)
     }
 }
