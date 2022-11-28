f318aa34fc7d4caf579342721f08f7c739b09d51
wangyanbin <wangyanbin_29@126.com>
2022-11-28 14:22:24 +0800

    sdk版本升级

--- app/src/main/java/com/example/mvvm/activity/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/activity/MainActivity.kt
@@ -5,34 +5,31 @@ import com.example.base.utils.function.value.toSafeFloat
 import com.example.common.base.BaseActivity
 import com.example.common.constant.ARouterPath
 import com.example.common.utils.builder.shortToast
-import com.example.common.widget.xrecyclerview.refresh.init
 import com.example.mvvm.databinding.ActivityMainBinding
 import com.example.mvvm.widget.TwoLevelRefreshHeader
 import com.scwang.smart.refresh.layout.api.RefreshHeader
 import com.scwang.smart.refresh.layout.api.RefreshLayout
 import com.scwang.smart.refresh.layout.constant.RefreshState
-import com.scwang.smart.refresh.layout.listener.OnRefreshListener
 import com.scwang.smart.refresh.layout.simple.SimpleMultiListener
 
 @Route(path = ARouterPath.MainActivity)
-class MainActivity : BaseActivity<ActivityMainBinding>(), OnRefreshListener {
+class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override fun initView() {
         super.initView()
-//        binding.xRefresh.init(this)
         binding.header.setRefreshHeader(TwoLevelRefreshHeader(this))
-        binding.xRefresh.setOnRefreshListener(this)
         binding.xRefresh.setOnMultiListener(object : SimpleMultiListener() {
             override fun onRefresh(refreshLayout: RefreshLayout) {
                 super.onRefresh(refreshLayout)
                 "触发刷新事件".shortToast()
+                binding.xRefresh.finishRefresh()
 //                refreshLayout.finishRefresh(2000)
             }
 
-            override fun onLoadMore(refreshLayout: RefreshLayout) {
-                super.onLoadMore(refreshLayout)
-//                refreshLayout.finishLoadMore(2000)
-            }
+//            override fun onLoadMore(refreshLayout: RefreshLayout) {
+//                super.onLoadMore(refreshLayout)
+////                refreshLayout.finishLoadMore(2000)
+//            }
 
             override fun onHeaderMoving(header: RefreshHeader?, isDragging: Boolean, percent: Float, offset: Int, headerHeight: Int, maxDragHeight: Int) {
                 super.onHeaderMoving(header, isDragging, percent, offset, headerHeight, maxDragHeight)
@@ -43,14 +40,11 @@ class MainActivity : BaseActivity<ActivityMainBinding>(), OnRefreshListener {
 
     }
 
-    override fun onRefresh(refreshLayout: RefreshLayout) {
-        "触发刷新".shortToast()
-        binding.xRefresh.finishRefresh()
-    }
-
     override fun onStop() {
         super.onStop()
-        if (binding.xRefresh.state == RefreshState.TwoLevel) {
+        if (binding.xRefresh.state == RefreshState.TwoLevel ||
+            binding.xRefresh.state == RefreshState.TwoLevelReleased ||
+            binding.xRefresh.state == RefreshState.TwoLevelFinish) {
             binding.xRefresh.closeHeaderOrFooter()
         }
     }
