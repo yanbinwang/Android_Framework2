52e4c86097856ecd7f5699605ff6c2663af84854
wangyanbin <wangyanbin_29@126.com>
2022-12-14 10:18:50 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/utils/FormActivityUtil.kt
+++ lib_common/src/main/java/com/example/common/utils/FormActivityUtil.kt
@@ -1,7 +1,6 @@
 package com.example.common.utils
 
 import android.app.Activity
-import android.os.Build
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowInsets
@@ -13,38 +12,34 @@ import android.view.WindowInsets
 object FormActivityUtil {
 
     fun setAct(activity: Activity, extra: (bottom: Int) -> Unit = {}) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH) {
-            val rootView = (activity.findViewById<View>(android.R.id.content) as ViewGroup).getChildAt(0)
-            rootView.fitsSystemWindows = true
-            rootView.setOnApplyWindowInsetsListener(object : View.OnApplyWindowInsetsListener {
-                var bottomBefore = 0
-                override fun onApplyWindowInsets(v: View, insets: WindowInsets): WindowInsets {
-                    val bottom = insets.systemWindowInsetBottom
-                    if (bottomBefore != bottom) {
-                        bottomBefore = bottom
-                        extra(bottom)
-                    }
-                    return v.onApplyWindowInsets(insets.replaceSystemWindowInsets(0, 0, 0, bottom))
+        val rootView = (activity.findViewById<View>(android.R.id.content) as ViewGroup).getChildAt(0)
+        rootView.fitsSystemWindows = true
+        rootView.setOnApplyWindowInsetsListener(object : View.OnApplyWindowInsetsListener {
+            var bottomBefore = 0
+            override fun onApplyWindowInsets(v: View, insets: WindowInsets): WindowInsets {
+                val bottom = insets.systemWindowInsetBottom
+                if (bottomBefore != bottom) {
+                    bottomBefore = bottom
+                    extra(bottom)
                 }
-            })
-        }
+                return v.onApplyWindowInsets(insets.replaceSystemWindowInsets(0, 0, 0, bottom))
+            }
+        })
     }
 
     fun setView(view: View, extra: (bottom: Int) -> Unit = {}) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH) {
-            view.fitsSystemWindows = true
-            view.setOnApplyWindowInsetsListener(object : View.OnApplyWindowInsetsListener {
-                var bottomBefore = 0
-                override fun onApplyWindowInsets(v: View, insets: WindowInsets): WindowInsets {
-                    val bottom = insets.systemWindowInsetBottom
-                    if (bottomBefore != bottom) {
-                        bottomBefore = bottom
-                        extra(bottom)
-                    }
-                    return v.onApplyWindowInsets(insets.replaceSystemWindowInsets(0, 0, 0, bottom))
+        view.fitsSystemWindows = true
+        view.setOnApplyWindowInsetsListener(object : View.OnApplyWindowInsetsListener {
+            var bottomBefore = 0
+            override fun onApplyWindowInsets(v: View, insets: WindowInsets): WindowInsets {
+                val bottom = insets.systemWindowInsetBottom
+                if (bottomBefore != bottom) {
+                    bottomBefore = bottom
+                    extra(bottom)
                 }
-            })
-        }
+                return v.onApplyWindowInsets(insets.replaceSystemWindowInsets(0, 0, 0, bottom))
+            }
+        })
     }
 
 }
