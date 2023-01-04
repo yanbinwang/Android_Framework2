fccc552551448ee9bab55f45e6e6aecbbeeefa26
wangyanbin <wangyanbin_29@126.com>
2023-01-04 14:39:18 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/config/Constants.kt
+++ lib_common/src/main/java/com/example/common/config/Constants.kt
@@ -1,6 +1,7 @@
 package com.example.common.config
 
 import com.example.common.BaseApplication
+import com.example.common.utils.helper.ConfigHelper
 
 /**
  * Created by wyb on 2017/3/7.
@@ -9,22 +10,15 @@ import com.example.common.BaseApplication
 object Constants {
     //app内的一些默认值
     //------gradle4.1.0开始，库中不提供VERSION_CODE和VERSION_NAME，在主app中获取，赋值给全局静态变量------
-    @JvmField
-    var VERSION_CODE: Long = 0//版本号
-    @JvmField
-    var VERSION_NAME: String? = null//版本名
+    //版本号
+    val VERSION_CODE: Long get() { return ConfigHelper.getAppVersionCode() }
+    //版本名
+    val VERSION_NAME: String get() { return ConfigHelper.getAppVersionName() }
     //------------
-//    @JvmField
-//    var IP: String? = null//当前手机ip
-//    @JvmField
-//    var MAC: String? = null//当前手机mac地址
-//    @JvmField
-//    var DEVICE_ID: String? = null//当前手机设备id
-    @JvmField
-    var APPLICATION_FILE_PATH: String? = null//默认文件保存路径，sd卡下的应用名文件夹
-    @JvmField
-    var SDCARD_PATH = BaseApplication.instance.getExternalFilesDir(null)?.absolutePath//sd卡的根路径mnt/sdcard-访问这个目录不需要动态申请STORAGE权限
-
+    //默认文件保存路径，sd卡下的应用名文件夹
+    val APPLICATION_FILE_PATH: String get() { return "${SDCARD_PATH}/${APPLICATION_NAME}" }
+    //sd卡的根路径mnt/sdcard-访问这个目录不需要动态申请STORAGE权限
+    val SDCARD_PATH by lazy { BaseApplication.instance.getExternalFilesDir(null)?.absolutePath }
     //app内接口的一些默认配置字段
     const val APPLICATION_ID = "com.sqkj.oea"//当前包名
     const val APPLICATION_NAME = "简证"//当前应用名
--- lib_common/src/main/java/com/example/common/utils/helper/ConfigHelper.kt
+++ lib_common/src/main/java/com/example/common/utils/helper/ConfigHelper.kt
@@ -12,7 +12,6 @@ import com.app.hubert.guide.core.Controller
 import com.app.hubert.guide.listener.OnGuideChangedListener
 import com.app.hubert.guide.model.GuidePage
 import com.example.common.R
-import com.example.common.config.Constants
 import com.example.common.utils.MmkvUtil
 import com.example.framework.utils.function.color
 import java.lang.ref.WeakReference
@@ -28,19 +27,6 @@ object ConfigHelper {
     @JvmStatic
     fun initialize(application: Application) {
         context = application
-        Constants.apply {
-//            //获取手机的网络ip
-//            IP = getIp()
-//            //获取手机的Mac地址
-//            MAC = getMac()
-//            //获取手机的DeviceId
-//            DEVICE_ID = getDeviceId()
-            //版本名，版本号
-            VERSION_CODE = getAppVersionCode()
-            VERSION_NAME = getAppVersionName()
-            //获取应用名。包名。默认保存文件路径
-            APPLICATION_FILE_PATH = "${SDCARD_PATH}/${APPLICATION_NAME}"
-        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="公用方法">
@@ -54,20 +40,17 @@ object ConfigHelper {
      *  .setLayoutRes(R.layout.view_guide_simple))
      */
     @JvmStatic
-    fun showGuide(activity: Activity, label: String, vararg pages: GuidePage, color: Int = R.color.white) {
+    fun showGuide(activity: Activity, label: String, vararg pages: GuidePage) {
         if (!MmkvUtil.decodeBool(label)) {
             MmkvUtil.encode(label, true)
             WeakReference(activity).get()?.apply {
-//                val statusBarUtil = StatusBarUtil(window)
                 val builder = NewbieGuide.with(this)//传入activity
                     .setLabel(label)//设置引导层标示，用于区分不同引导层，必传！否则报错
                     .setOnGuideChangedListener(object : OnGuideChangedListener {
                         override fun onShowed(controller: Controller?) {
-//                            statusBarUtil.statusBarColor(activity.color(R.color.black_4c000000))
                         }
 
                         override fun onRemoved(controller: Controller?) {
-//                            statusBarUtil.statusBarColor(activity.color(color))
                         }
                     })
                     .alwaysShow(true)
@@ -95,101 +78,10 @@ object ConfigHelper {
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="私有调取方法">
-//    /**
-//     * 获取当前设备ip地址
-//     */
-//    private fun getIp(): String {
-//        val connectivityManager = (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            val networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-//            if (networkCapabilities != null) {
-//                if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
-//                    return getMobileIp()
-//                } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-//                    return getWifiIp()
-//                }
-//            }
-//        } else {
-//            val networkInfo = connectivityManager.activeNetworkInfo
-//            if (networkInfo != null && networkInfo.isConnected) {
-//                if (networkInfo.type == ConnectivityManager.TYPE_MOBILE) {
-//                    return getMobileIp()
-//                } else if (networkInfo.type == ConnectivityManager.TYPE_WIFI) {
-//                    return getWifiIp()
-//                }
-//            }
-//        }
-//        return ""
-//    }
-//
-//    private fun getMobileIp(): String {
-//        try {
-//            val enumeration = NetworkInterface.getNetworkInterfaces()
-//            while (enumeration.hasMoreElements()) {
-//                val networkInterface = enumeration.nextElement()
-//                val inetAddresses = networkInterface.inetAddresses
-//                while (inetAddresses.hasMoreElements()) {
-//                    val inetAddress = inetAddresses.nextElement()
-//                    if (!inetAddress.isLoopbackAddress && inetAddress is Inet4Address) return inetAddress.getHostAddress()
-//                }
-//            }
-//        } catch (ignored: SocketException) {
-//            return ""
-//        }
-//        return ""
-//    }
-//
-//    private fun getWifiIp(): String {
-//        val wifiManager = context.applicationContext?.getSystemService(Context.WIFI_SERVICE) as WifiManager
-//        val wifiInfo = wifiManager.connectionInfo
-//        val ipInt = wifiInfo.ipAddress
-//        return (ipInt and 0xFF).toString() + "." +
-//                (ipInt shr 8 and 0xFF) + "." +
-//                (ipInt shr 16 and 0xFF) + "." +
-//                (ipInt shr 24 and 0xFF)
-//    }
-//
-//    /**
-//     * 获取当前设备的mac地址
-//     */
-//    private fun getMac(): String? {
-//        try {
-//            val all = Collections.list(NetworkInterface.getNetworkInterfaces())
-//            for (nif in all) {
-//                if (!nif.name.equals("wlan0")) continue
-//                val macBytes = nif.hardwareAddress ?: return null
-//                val res1 = StringBuilder()
-//                for (b in macBytes) {
-//                    res1.append(String.format("%02X:", b))
-//                }
-//                if (res1.isNotEmpty()) res1.deleteCharAt(res1.length - 1)
-//                return res1.toString()
-//            }
-//        } catch (ignored: Exception) {
-//        }
-//        return null
-//    }
-//
-//    /**
-//     * 获取当前设备的id
-//     */
-//    private fun getDeviceId(): String? {
-//        val telephonyManager = (context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager)
-//        return try {
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//                telephonyManager.imei
-//            } else {
-//                telephonyManager.deviceId
-//            }
-//        } catch (ignored: SecurityException) {
-//            null
-//        }
-//    }
-
     /**
      * 获取当前app version code
      */
-    private fun getAppVersionCode(): Long {
+    fun getAppVersionCode(): Long {
         var appVersionCode: Long = 0
         try {
             val packageInfo = context.applicationContext.packageManager.getPackageInfo(context.packageName, 0)
@@ -206,7 +98,7 @@ object ConfigHelper {
     /**
      * 获取当前app version name
      */
-    private fun getAppVersionName(): String {
+    fun getAppVersionName(): String {
         var appVersionName = ""
         try {
             val packageInfo = context.applicationContext.packageManager.getPackageInfo(context.packageName, 0)
