3ecc09bc3f981c3b155ff640d307858b98e08950
wangyanbin <wangyanbin_29@126.com>
2023-03-06 10:50:28 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/config/Constants.kt
+++ lib_common/src/main/java/com/example/common/config/Constants.kt
@@ -25,13 +25,13 @@ object Constants {
     //默认文件保存路径，sd卡下的应用名文件夹
     val APPLICATION_PATH get() = "${SDCARD_PATH}/${APPLICATION_NAME}"
     //sd卡的根路径/android/data/{包名}->访问这个目录不需要动态申请STORAGE权限
-    val SDCARD_PATH by lazy { BaseApplication.instance.getExternalFilesDir(null)?.absolutePath }
+    val SDCARD_PATH get() = BaseApplication.instance.getExternalFilesDir(null)?.absolutePath
     //无数据占位符
-    val NO_DATA: String get() = string(R.string.unit_no_data)
+    val NO_DATA get() = string(R.string.unit_no_data)
     //无数据占位符
-    val NO_DATA_DOLLAR: String get() = string(R.string.unit_no_data_dollar)
+    val NO_DATA_DOLLAR get() = string(R.string.unit_no_data_dollar)
     //无数据占位符
-    val NO_DATA_PERCENT: String get() = string(R.string.unit_no_data_percent)
+    val NO_DATA_PERCENT get() = string(R.string.unit_no_data_percent)
     //------app内接口的一些默认配置字段------
     const val LOGIN_INTERCEPTOR_CODE = 1 //阿里路由登录全局拦截器编号
     const val PUSH_NOTIFY_ID = 0 //固定通知id
