41a488ab3cfd250d02eb448f6eb6e8587616e122
wangyanbin <wangyanbin_29@126.com>
2023-04-27 18:29:58 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/network/repository/ApiRepository.kt
+++ lib_common/src/main/java/com/example/common/network/repository/ApiRepository.kt
@@ -26,7 +26,7 @@ import okhttp3.RequestBody.Companion.toRequestBody
  * }
  */
 class MultiReqUtil(
-    var view: BaseView?,
+    private var view: BaseView?,
     private val isShowDialog: Boolean = true,
     private val err: (e: Triple<Int?, String?, Exception?>?) -> Unit = {},
 ) {
