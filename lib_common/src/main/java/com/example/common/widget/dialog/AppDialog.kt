b70a3721a8db85a667ca41809f2395ada65a24e1
wangyanbin <wangyanbin_29@126.com>
2023-05-04 13:57:10 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/widget/dialog/AppDialog.kt
+++ lib_common/src/main/java/com/example/common/widget/dialog/AppDialog.kt
@@ -17,7 +17,7 @@ class AppDialog(context: Context) : BaseDialog<ViewDialogBinding>(context, close
     var onConfirm: (() -> Unit)? = null
     var onCancel: (() -> Unit)? = null
 
-    fun setParams(title: String? = "", message: String? = "", positiveText: String? = "", negativeText: String? = "", center: Boolean = true) {
+    fun setParams(title: String? = "", message: String? = "", positiveText: String? = "", negativeText: String? = "", gravity: Int = Gravity.CENTER) {
         binding.apply {
             if (title.isNullOrEmpty()) {
                 tvTip.gone()
@@ -25,7 +25,7 @@ class AppDialog(context: Context) : BaseDialog<ViewDialogBinding>(context, close
                 tvTip.visible()
                 tvTip.text = title
             }
-            tvMessage.gravity = if (center) Gravity.CENTER else Gravity.LEFT
+            tvMessage.gravity = gravity
             tvMessage.text = message
             tvSure.text = positiveText
             tvSure.click {
