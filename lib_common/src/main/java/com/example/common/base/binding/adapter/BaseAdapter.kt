a9834fb235a0f570e9d1009473e79d78e79b0b0f
wangyanbin <wangyanbin_29@126.com>
2023-05-09 13:53:57 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/base/binding/adapter/BaseAdapter.kt
+++ lib_common/src/main/java/com/example/common/base/binding/adapter/BaseAdapter.kt
@@ -34,7 +34,7 @@ abstract class BaseAdapter<T> : RecyclerView.Adapter<BaseViewDataBindingHolder> 
     /**
      * 点击回调，返回对象和下标
      */
-    var onItemClick: ((t: T?, position: Int) -> Unit)? = null
+    private var onItemClick: ((t: T?, position: Int) -> Unit)? = null
 
     /**
      * 默认是返回对象
@@ -254,6 +254,14 @@ abstract class BaseAdapter<T> : RecyclerView.Adapter<BaseViewDataBindingHolder> 
     fun insert(item: T) {
         data.add(item)
         notifyItemInserted(data.size - 1)
+    }
+
+
+    /**
+     * 设置item的点击
+     */
+    fun setOnItemClickListener(onItemClick: (t: T?, position: Int) -> Unit) {
+        this.onItemClick = onItemClick
     }
 
 }
