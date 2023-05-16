0b380e2213a3cbcb80eb7e4647136bf8fba02cdc
wangyanbin <wangyanbin_29@126.com>
2023-05-16 15:24:05 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/network/interceptor/UserAgentInterceptor.kt
+++ lib_common/src/main/java/com/example/common/network/interceptor/UserAgentInterceptor.kt
@@ -23,18 +23,6 @@ internal class UserAgentInterceptor : Interceptor {
     }
 
     private fun defaultHeaders(): Headers {
-////        val token: String? = AccountHelper.getToken()//取得本地token
-////        if (!TextUtils.isEmpty(token)) {
-////            params["Authorization"] = "basic $token"
-////        }
-//        val params = ArrayMap<String, String>()
-//        params["system-name"] = "Android"
-//        params["phone-model"] = Build.MODEL
-//        val builder = Headers.Builder()
-//        for (key in params.keys) {
-//            builder.add(key, params[key].orEmpty())
-//        }
-//        return builder.build()
         val builder = Headers.Builder()
         builder.add("system-name", "Android")
         builder.add("phone-model", Build.MODEL)
