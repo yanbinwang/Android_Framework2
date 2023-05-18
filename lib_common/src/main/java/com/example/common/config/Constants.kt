4d232166c93eb81dd1cd20dcb6dfb1bbe915ca8c
wangyanbin <wangyanbin_29@126.com>
2023-05-18 14:07:37 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/config/Constants.kt
+++ lib_common/src/main/java/com/example/common/config/Constants.kt
@@ -33,7 +33,7 @@ object Constants {
 //    //无数据占位符
 //    val NO_DATA_PERCENT get() = string(R.string.unit_no_data_percent)
     //------app内接口的一些默认配置字段------
-    const val LOGIN_INTERCEPTOR_CODE = 1 //阿里路由登录全局拦截器编号
+    const val INTERCEPTOR_LOGIN_CODE = 1 //阿里路由登录全局拦截器编号
     const val PUSH_CHANNEL_ID = "dataqin" //推送渠道id
     const val PUSH_CHANNEL_NAME = "数秦科技" //推送渠道名
     const val PAGE_LIMIT = "10"//固定配置页数
