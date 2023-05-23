3db0e987ce04bf4cece5e62256abb0dbee14b996
wangyanbin <wangyanbin_29@126.com>
2023-05-23 13:43:30 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/network/factory/OkHttpFactory.kt
+++ lib_common/src/main/java/com/example/common/network/factory/OkHttpFactory.kt
@@ -11,17 +11,19 @@ import java.util.concurrent.TimeUnit
  * okhttp单例
  */
 class OkHttpFactory private constructor() {
-    val okHttpClient = OkHttpClient.Builder()
-        .connectTimeout(6, TimeUnit.SECONDS)//建立连接所用的时间，适用于网络状况正常的情况下，两端连接所用的时间
-        .callTimeout(120, TimeUnit.SECONDS)//从调用call.execute()和enqueue()这两个方法开始计时,时间到后网络还未请求完成将调用cancel()方法
+    val okHttpClient by lazy {
+        OkHttpClient.Builder()
+            .connectTimeout(6, TimeUnit.SECONDS)//建立连接所用的时间，适用于网络状况正常的情况下，两端连接所用的时间
+            .callTimeout(120, TimeUnit.SECONDS)//从调用call.execute()和enqueue()这两个方法开始计时,时间到后网络还未请求完成将调用cancel()方法
 //        .pingInterval(5, TimeUnit.SECONDS)//只有http2和webSocket中有使用,如果设置了这个值会定时的向服务器发送一个消息来保持长连接
-        .readTimeout(60, TimeUnit.SECONDS)//设置读超时
-        .writeTimeout(60, TimeUnit.SECONDS)//设置写超时
-        .retryOnConnectionFailure(true)
-        .addInterceptor(UserAgentInterceptor())//请求加头
-        .addInterceptor(LoggingInterceptor())//日志监听
+            .readTimeout(60, TimeUnit.SECONDS)//设置读超时
+            .writeTimeout(60, TimeUnit.SECONDS)//设置写超时
+            .retryOnConnectionFailure(true)
+            .addInterceptor(UserAgentInterceptor())//请求加头
+            .addInterceptor(LoggingInterceptor())//日志监听
 //            .addInterceptor(RetryServerInterceptor())//重新构建请求
-        .build()
+            .build()
+    }
 
     companion object {
         val instance by lazy { OkHttpFactory() }
