29313f09bc5b526b008e82d06e3294456257fa4d
wangyanbin <wangyanbin_29@126.com>
2023-05-23 11:43:00 +0800

    sdk版本升级

--- lib_common/src/main/java/com/example/common/config/CacheData.kt
+++ lib_common/src/main/java/com/example/common/config/CacheData.kt
@@ -9,10 +9,12 @@ import com.example.common.utils.DataStringCacheUtil
  * @author yan
  */
 object CacheData {
-    private const val USER_BEAN = "user_bean" //用户类
-    val userBean = DataCacheUtil(USER_BEAN, UserBean::class.java)
-
     //设备id
     private const val DEVICE_ID = "device_id"
     val deviceIdCache = DataStringCacheUtil(DEVICE_ID)
+
+    //用户类
+    private const val USER_BEAN = "user_bean"
+    val userBean = DataCacheUtil(USER_BEAN, UserBean::class.java)
+
 }
