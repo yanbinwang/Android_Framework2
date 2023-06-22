fab7718d7479ebfe86f8cce46a3f1976d564a775
wangyanbin <wangyanbin_29@126.com>
2023-06-22 10:10:21 +0800

    调整

--- lib_glide/src/main/java/com/example/glide/GlideBindingAdapter.kt
+++ lib_glide/src/main/java/com/example/glide/GlideBindingAdapter.kt
@@ -2,6 +2,8 @@ package com.example.glide
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.example.framework.utils.function.defTypeDrawable
+import com.example.framework.utils.function.defTypeMipmap
 import com.example.framework.utils.function.value.toSafeInt
 import com.example.framework.utils.function.view.disable
 import com.example.framework.utils.function.view.enable
@@ -16,6 +18,7 @@ object GlideBindingAdapter {
     /**
      * 加载图片（比例缩放）
      */
+    @JvmStatic
     @BindingAdapter(value = ["display_zoom"])
     fun bindingDisplayZoom(view: ImageView, url: String) {
         ImageLoader.instance.displayZoom(view, url, { view.disable() }, { view.enable() })
@@ -24,25 +27,52 @@ object GlideBindingAdapter {
     /**
      * 加载图片
      */
-    @BindingAdapter(value = ["display"], requireAll = false)
-    fun bindingDisplay(view: ImageView, url: String) {
-        ImageLoader.instance.display(view, url, onStart = { view.disable() }, onComplete = { view.enable() })
+    @JvmStatic
+    @BindingAdapter(value = ["display", "placeholder_id"], requireAll = false)
+    fun bindingDisplay(view: ImageView, url: String, placeholderId: Int?) {
+        ImageLoader.instance.display(view, url, placeholderId.toSafeInt(R.drawable.shape_glide_bg), onStart = { view.disable() }, onComplete = { view.enable() })
+    }
+
+    @JvmStatic
+    @BindingAdapter(value = ["display_resource", "placeholder_id"], requireAll = false)
+    fun bindingDisplayResource(view: ImageView, resource: String, placeholderId: Int?) {
+        ImageLoader.instance.display(view, view.context.defTypeMipmap(resource), placeholderId.toSafeInt(R.drawable.shape_glide_bg), onStart = { view.disable() }, onComplete = { view.enable() })
     }
 
     /**
      * 加载图片（带圆角）
      */
+    @JvmStatic
     @BindingAdapter(value = ["display_round", "round_radius"], requireAll = false)
     fun bindingDisplayRound(view: ImageView, url: String, roundRadius: Int?) {
         ImageLoader.instance.displayRound(view, url, roundRadius.toSafeInt(5))
     }
 
+    @JvmStatic
+    @BindingAdapter(value = ["display_round_resource", "round_radius"], requireAll = false)
+    fun bindingDisplayRoundResource(view: ImageView, resource: String, roundRadius: Int?) {
+        ImageLoader.instance.displayRound(view, view.context.defTypeMipmap(resource), roundRadius.toSafeInt(5))
+    }
+
     /**
      * 加载图片（圆形）
      */
+    @JvmStatic
     @BindingAdapter(value = ["display_circle"])
     fun bindingDisplayCircle(view: ImageView, url: String) {
         ImageLoader.instance.displayCircle(view, url)
+    }
+
+    @JvmStatic
+    @BindingAdapter(value = ["display_circle_resource"])
+    fun bindingDisplayCircleResource(view: ImageView, resource: String) {
+        ImageLoader.instance.displayCircle(view, view.context.defTypeMipmap(resource))
+    }
+
+    @JvmStatic
+    @BindingAdapter(value = ["display_gif_resource"])
+    fun bindingDisplayGif(view: ImageView, resource: String) {
+        ImageLoader.instance.displayGif(view, view.context.defTypeDrawable(resource))
     }
     // </editor-fold>
 
