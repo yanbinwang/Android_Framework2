e7560e570c386fa9142207bba8234878b5aac288
wangyanbin <wangyanbin_29@126.com>
2023-06-27 11:03:48 +0800

    调整

---
+++ lib_glide/src/main/java/com/example/glide/widget/GifImageView.kt
@@ -0,0 +1,22 @@
+package com.example.glide.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+import com.example.glide.ImageLoader
+import com.example.glide.R
+
+/**
+ * @description 加载动图
+ * @author yan
+ */
+class GifImageView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : AppCompatImageView(context, attrs, defStyleAttr) {
+
+    init {
+        val typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.GifImageView)
+        val res = typedArray.getResourceId(R.styleable.GifImageView_android_src, -1)
+        if (res != -1) ImageLoader.instance.displayGif(this, res)
+        typedArray.recycle()
+    }
+
+}
