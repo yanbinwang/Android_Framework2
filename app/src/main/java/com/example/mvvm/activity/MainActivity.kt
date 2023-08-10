12f51f401b6d68b5791712013db0347ab36c0100
wangyanbin <wangyanbin_29@126.com>
2023-08-10 14:41:49 +0800

    命名调整

--- app/src/main/java/com/example/mvvm/activity/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mvvm.activity
 import androidx.work.Constraints
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import androidx.work.workDataOf
@@ -12,8 +13,10 @@ import com.example.common.config.ARouterPath
 import com.example.framework.utils.logWTF
 import com.example.mvvm.databinding.ActivityMainBinding
 import com.example.mvvm.utils.RecordWork
+import java.util.concurrent.TimeUnit
 
 /**
+ * https://blog.csdn.net/Mr_Tony/article/details/125605692
  */
 @Route(path = ARouterPath.MainActivity)
 class MainActivity : BaseActivity<ActivityMainBinding>() {
@@ -27,15 +30,35 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         val constrains = Constraints.Builder()
             .setRequiredNetworkType(NetworkType.CONNECTED)//网络连接的情况下执行
             .build()
-        //单次请求
-        val workRequest = OneTimeWorkRequestBuilder<RecordWork>()
+        //单次请求->OneTimeWorkRequestBuilder
+        val workRequestA = OneTimeWorkRequestBuilder<RecordWork>()
+            .setConstraints(constrains)
+            //传递数据
+            .setInputData(workDataOf("input_data_key" to "input_work_data"))
+            .build()
+        //多次请求->PeriodicWorkRequestBuilder
+        val workRequestB = PeriodicWorkRequestBuilder<RecordWork>(24, TimeUnit.HOURS)
             .setConstraints(constrains)
             //传递数据
             .setInputData(workDataOf("input_data_key" to "input_work_data"))
             .build()
         //加入队列
-        manager.enqueue(workRequest)
-        manager.getWorkInfoByIdLiveData(workRequest.id).observe(this) {
+        //WorkManager.enqueueUniqueWork()（用于一次性工作）
+        //WorkManager.enqueueUniquePeriodicWork()（用于定期工作）
+        //WorkManager.enqueue()（通用）
+        //————————————————
+        //WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+        //           "sendLogs",
+        //           ExistingPeriodicWorkPolicy.KEEP,
+        //           sendLogsWorkRequest
+        //)
+        //————————————————
+        //uniqueWorkName - 用于唯一标识工作请求的 String。
+        //existingWorkPolicy - 此 enum 可告知 WorkManager：如果已有使用该名称且尚未完成的唯一工作链，应执行什么操作。如需了解详情，请参阅冲突解决政策。
+        //work - 要调度的 WorkRequest
+        //————————————————
+        manager.enqueue(workRequestA)
+        manager.getWorkInfoByIdLiveData(workRequestA.id).observe(this) {
             val state = it.state//状态
             "state:${state}".logWTF
             if (state == WorkInfo.State.SUCCEEDED) {
