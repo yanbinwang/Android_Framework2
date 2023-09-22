98fe38eb1658f9965f28d7df455d5b05746ebb55
wangyanbin <wangyanbin_29@126.com>
2023-09-22 10:35:20 +0800

    命名调整

--- lib_common/src/main/java/com/example/common/widget/NativeIndicator.kt
+++ lib_common/src/main/java/com/example/common/widget/NativeIndicator.kt
@@ -32,6 +32,7 @@ class NativeIndicator constructor(tab: TabLayout, tabTitle: List<String>) : TabL
 
     /**
      * 重写此方法表示部分标题字体字号样式等需要使用非默认配置
+     * 需在调用bind（）方法前调取，一旦绑定就会执行，此时监听没赋值，控件会显示不出
      */
     fun setBindViewListener(onBindView: ((binding: ItemTabBinding?, item: String?, selected: Boolean, index: Int) -> Unit)) {
         this.onBindView = onBindView
