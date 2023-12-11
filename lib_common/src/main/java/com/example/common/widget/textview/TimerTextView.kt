4b5f8bbd6e0946ddb2b12dfcb7684664aeb2d84b
wangyanbin <wangyanbin_29@126.com>
2023-12-11 15:00:35 +0800

    推送框架

--- lib_common/src/main/java/com/example/common/widget/textview/TimerTextView.kt
+++ lib_common/src/main/java/com/example/common/widget/textview/TimerTextView.kt
@@ -7,12 +7,12 @@ import android.view.Gravity
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.lifecycle.LifecycleOwner
 import com.example.common.R
+import com.example.common.utils.function.string
 import com.example.framework.utils.builder.TimerBuilder
 import com.example.framework.utils.function.view.disable
 import com.example.framework.utils.function.view.enable
 import com.example.framework.utils.function.view.textColor
 import com.example.framework.utils.function.view.textSize
-import java.text.MessageFormat
 
 /**
  * Created by wangyanbin
@@ -20,12 +20,11 @@ import java.text.MessageFormat
  */
 @SuppressLint("SetTextI18n")
 class TimerTextView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : AppCompatTextView(context, attrs, defStyleAttr) {
-    private val tickTxt by lazy { "已发送{0}S" }
     private var timerTag = javaClass.simpleName
     private var timerBuilder: TimerBuilder? = null
 
     init {
-        text = "发送验证码"
+        text = string(R.string.timerContent)
         gravity = Gravity.CENTER
         textColor(R.color.appTheme)
         textSize(R.dimen.textSize14)
@@ -39,10 +38,10 @@ class TimerTextView @JvmOverloads constructor(context: Context, attrs: Attribute
         if (!tag.isNullOrEmpty()) timerTag = tag
         timerBuilder?.startCountDown(timerTag, {
             disable()
-            text = MessageFormat.format(tickTxt, it.toString())
+            text = string(R.string.timerPressed, it.toString())
         }, {
             enable()
-            text = "重发验证码"
+            text = string(R.string.timerDisabled)
         }, second)
     }
 
