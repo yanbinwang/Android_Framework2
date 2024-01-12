2d10b17a289ba3ccb143c2d5032e1ca4874143e6
wangyanbin <wangyanbin_29@126.com>
2024-01-12 16:48:09 +0800

    推送框架

--- lib_framework/src/main/java/com/example/framework/utils/builder/NavigationBuilder.kt
+++ lib_framework/src/main/java/com/example/framework/utils/builder/NavigationBuilder.kt
@@ -1,11 +1,14 @@
 package com.example.framework.utils.builder
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
 import androidx.viewpager2.widget.ViewPager2
 import com.example.framework.R
 import com.example.framework.utils.enterAnimation
+import com.example.framework.utils.function.value.orFalse
+import com.example.framework.utils.function.value.orZero
 import com.example.framework.utils.function.view.vibrate
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView
@@ -15,12 +18,15 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
  *  Created by wangyanbin
  *  导航栏帮助类,支持viewpage2绑定，fragment绑定
  */
-class NavigationBuilder(private val navigationView: BottomNavigationView, private val ids: List<Int>, private val animation: Boolean = true) {
+@SuppressLint("RestrictedApi")
+class NavigationBuilder(private val navigationView: BottomNavigationView?, private val ids: List<Int>, private val animation: Boolean = true) {
     private var flipper: ViewPager2? = null
     private var builder: FragmentBuilder? = null
-    private var onItemSelectedListener: ((index: Int, isCurrent: Boolean?) -> Unit)? = null
-    private val menuView
-        get() = navigationView.getChildAt(0) as? BottomNavigationMenuView
+    private var onItemListener: ((index: Int, isCurrent: Boolean?) -> Unit)? = null
+    private var onItemSelectedListener: ((index: Int) -> Unit)? = null
+    private val isPager get() = null != flipper
+    private val menuView get() = navigationView?.getChildAt(0) as? BottomNavigationMenuView
+    var enableSelect = true//默认页面切换是在点击后就能触发的
 
     /**
      * 初始化
@@ -30,24 +36,12 @@ class NavigationBuilder(private val navigationView: BottomNavigationView, privat
         ids.indices.forEachIndexed { index, i ->
             menuView?.getChildAt(index)?.findViewById<View>(i)?.setOnLongClickListener { true }
         }
-//        for (position in ids.indices) {
-//            menuView?.getChildAt(position)?.findViewById<View>(ids.safeGet(position).toSafeInt())?.setOnLongClickListener { true }
-//        }
         //最多配置5个tab，需要注意
-        navigationView.setOnItemSelectedListener { item ->
+        navigationView?.setOnItemSelectedListener { item ->
             //返回第一个符合条件的元素的下标，没有就返回-1
             val index = ids.indexOfFirst { it == item.itemId }
-            //如果频繁点击相同的页面tab，不执行切换代码，只做结果返回
-            val isPager = null != flipper
-            val isCurrent = index == if (isPager) flipper?.currentItem else builder?.getCurrentIndex()
-            if (!isCurrent) {
-                if (isPager) flipper?.setCurrentItem(index, false) else builder?.selectTab(index)
-                if (animation) getItemView(index)?.getChildAt(0)?.apply {
-                    startAnimation(context.enterAnimation())
-                    vibrate(50)
-                }
-            }
-            onItemSelectedListener?.invoke(index, isCurrent)
+            onItemListener?.invoke(index, isCurrent(index))
+            if (enableSelect) selectTab(index)
             true
         }
     }
@@ -64,9 +58,26 @@ class NavigationBuilder(private val navigationView: BottomNavigationView, privat
     }
 
     /**
+     * 选择对应页面
+     */
+    fun selectTab(index: Int) {
+        //如果频繁点击相同的页面tab，不执行切换代码，只做结果返回
+        if (!isCurrent(index)) {
+            if (isPager) flipper?.setCurrentItem(index, false) else builder?.selectTab(index)
+            if (animation) getItemView(index)?.getChildAt(0)?.apply {
+                startAnimation(context.enterAnimation())
+                vibrate(50)
+            }
+        }
+        onItemSelectedListener?.invoke(index)
+    }
+
+    /**
      * 选中下标
      */
-    fun selectedItem(index: Int) = run { navigationView.selectedItemId = navigationView.menu.getItem(index)?.itemId ?: 0 }
+    fun selectedItem(index: Int) {
+        navigationView?.selectedItemId = navigationView?.menu?.getItem(index)?.itemId ?: 0
+    }
 
     /**
      * 获取下标item
@@ -82,12 +93,19 @@ class NavigationBuilder(private val navigationView: BottomNavigationView, privat
      * 获取当前选中的下标
      */
     fun getCurrentIndex(): Int {
-        val menu = navigationView.menu
-        for (i in 0 until menu.size()) {
-            val menuItem = menu.getItem(i)
-            if (menuItem.isChecked) return ids.indexOfFirst { it == menuItem.itemId }
+        val menu = navigationView?.menu
+        for (i in 0 until menu?.size().orZero) {
+            val menuItem = menu?.getItem(i)
+            if (menuItem?.isChecked.orFalse) return ids.indexOfFirst { it == menuItem?.itemId }
         }
         return 0
+    }
+
+    /**
+     * 是否重复选择
+     */
+    fun isCurrent(index: Int): Boolean {
+        return index == if (isPager) flipper?.currentItem else builder?.getCurrentIndex()
     }
 
     /**
@@ -115,7 +133,7 @@ class NavigationBuilder(private val navigationView: BottomNavigationView, privat
      */
     fun addView(resource: Int, index: Int = 0): View {
         //加载我们的角标View，新创建的一个布局
-        val badge = LayoutInflater.from(navigationView.context).inflate(resource, menuView, false)
+        val badge = LayoutInflater.from(navigationView?.context).inflate(resource, menuView, false)
         //添加到Tab上
         getItemView(index)?.addView(badge)
         //返回我们添加的view整体
@@ -125,8 +143,12 @@ class NavigationBuilder(private val navigationView: BottomNavigationView, privat
     /**
      * 设置点击事件
      */
-    fun setOnItemClickListener(onItemSelectedListener: ((index: Int, isCurrent: Boolean?) -> Unit)) {
+    fun setOnItemSelectedListener(onItemSelectedListener: ((index: Int) -> Unit)) {
         this.onItemSelectedListener = onItemSelectedListener
+    }
+
+    fun setOnItemListener(onItemListener: ((index: Int, isCurrent: Boolean?) -> Unit)) {
+        this.onItemListener = onItemListener
     }
 
 }
