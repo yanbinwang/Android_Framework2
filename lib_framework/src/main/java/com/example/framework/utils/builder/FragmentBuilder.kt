f2920eb86b57a53b2579ab4e55a079cf494fe091
wangyanbin <wangyanbin_29@126.com>
2024-01-13 12:01:18 +0800

    推送框架

--- lib_framework/src/main/java/com/example/framework/utils/builder/FragmentBuilder.kt
+++ lib_framework/src/main/java/com/example/framework/utils/builder/FragmentBuilder.kt
@@ -1,10 +1,12 @@
 package com.example.framework.utils.builder
 
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.framework.utils.function.value.getSimpleName
 import com.example.framework.utils.function.value.safeGet
+import java.io.Serializable
 
 /**
  *  Created by wangyanbin
@@ -30,16 +32,16 @@ import com.example.framework.utils.function.value.safeGet
  *
  *  override fun onRestoreInstanceState(savedInstanceState: Bundle) {
  *  super.onRestoreInstanceState(savedInstanceState)
- *  intent.getIntExtra(Extras.TAB_INDEX, 0).also { navigationBuilder.selectedItem(it) }
+ *  intent.getIntExtra(Extras.TAB_INDEX, 0).also { navigationBuilder.selectedItem(it) }//注意selected
  *  }
  *
  */
 class FragmentBuilder(private val manager: FragmentManager, private val containerViewId: Int) {
-    private var arguments = false
+    private var isArguments = false
     private var currentItem = 0
-    private var clazzPair: List<Pair<Class<*>, String>>? = null
-    private var clazzTriple: List<Triple<Class<*>, Pair<String, String>, String>>? = null
-    private var onTabShow: ((tab: Int) -> Unit)? = null
+    private var clazzList: List<Pair<Class<*>, String>>? = null
+    private var clazzBundleList: List<Triple<Class<*>, String, Bundle>>? = null
+    private var onTabShowListener: ((tab: Int) -> Unit)? = null
     private val list by lazy { ArrayList<Fragment>() }
 
     /**
@@ -47,10 +49,10 @@ class FragmentBuilder(private val manager: FragmentManager, private val containe
      *  first：class名
      *  second：tag值，不传默认为class名
      */
-    fun bind(clazzPair: List<Pair<Class<*>, String>>, defaultCurrentItem: Int = 0) {
+    fun bind(clazzList: List<Pair<Class<*>, String>>, defaultCurrentItem: Int = 0) {
         this.list.clear()
-        this.arguments = false
-        this.clazzPair = clazzPair
+        this.isArguments = false
+        this.clazzList = clazzList
         selectTab(defaultCurrentItem)
     }
 
@@ -64,15 +66,15 @@ class FragmentBuilder(private val manager: FragmentManager, private val containe
      * second：pair对象 （first，fragment透传的key second，透传的值）
      * third：内存中存储的tag
      */
-    fun bindArguments(clazzTriple: List<Triple<Class<*>, Pair<String, String>, String>>, defaultCurrentItem: Int = 0) {
+    fun bindBundle(clazzBundleList: List<Triple<Class<*>, String, Bundle>>, defaultCurrentItem: Int = 0) {
         this.list.clear()
-        this.arguments = true
-        this.clazzTriple = clazzTriple
+        this.isArguments = true
+        this.clazzBundleList = clazzBundleList
         selectTab(defaultCurrentItem)
     }
 
-    fun bindArguments(vararg clazzTriple: Triple<Class<*>, Pair<String, String>, String>) {
-        bindArguments(listOf(*clazzTriple))
+    fun bindBundle(vararg clazzTriple: Triple<Class<*>, String, Bundle>) {
+        bindBundle(listOf(*clazzTriple))
     }
 
     /**
@@ -82,19 +84,22 @@ class FragmentBuilder(private val manager: FragmentManager, private val containe
         currentItem = tab
         val transaction = manager.beginTransaction()
         list.forEach { transaction.hide(it) }
-        transaction.show(if (arguments) newInstanceArguments() else newInstance())
-        transaction.commitAllowingStateLoss()
-        onTabShow?.invoke(tab)
+        val fragment = if (isArguments) newInstanceArguments() else newInstance()
+        if (null != fragment) {
+            transaction.show(fragment)
+            transaction.commitAllowingStateLoss()
+            onTabShowListener?.invoke(tab)
+        }
     }
 
-    private fun newInstance(): Fragment {
-        clazzPair.safeGet(currentItem).let {
+    private fun newInstance(): Fragment? {
+        clazzList.safeGet(currentItem).let {
             val transaction = manager.beginTransaction()
             var fragment = manager.findFragmentByTag(it?.second)
             if (null == fragment) {
-//                fragment = it?.first?.newInstance() as Fragment
-                fragment = it?.first?.getDeclaredConstructor()?.newInstance() as Fragment
-                transaction.add(containerViewId, fragment, it.second)
+                fragment = it?.first?.getDeclaredConstructor()?.newInstance() as? Fragment
+                fragment ?: return null
+                transaction.add(containerViewId, fragment, it?.second)
                 transaction.commitAllowingStateLoss()
                 list.add(fragment)
             }
@@ -102,17 +107,15 @@ class FragmentBuilder(private val manager: FragmentManager, private val containe
         }
     }
 
-    private fun newInstanceArguments(): Fragment {
-        clazzTriple.safeGet(currentItem).let {
+    private fun newInstanceArguments(): Fragment? {
+        clazzBundleList.safeGet(currentItem).let {
             val transaction = manager.beginTransaction()
-            var fragment = manager.findFragmentByTag(it?.third)
+            var fragment = manager.findFragmentByTag(it?.second)
             if (null == fragment) {
-//                fragment = it?.first?.newInstance() as Fragment
-                fragment = it?.first?.getDeclaredConstructor()?.newInstance() as Fragment
-                val bundle = Bundle()
-                bundle.putString(it.second.first, it.second.second)
-                fragment.arguments = bundle
-                transaction.add(containerViewId, fragment, it.third)
+                fragment = it?.first?.getDeclaredConstructor()?.newInstance() as? Fragment
+                fragment ?: return null
+                fragment.arguments = it?.third
+                transaction.add(containerViewId, fragment, it?.second)
                 transaction.commitAllowingStateLoss()
                 list.add(fragment)
             }
@@ -138,7 +141,7 @@ class FragmentBuilder(private val manager: FragmentManager, private val containe
      * 设置点击事件
      */
     fun setOnItemClickListener(onTabShow: ((tab: Int) -> Unit)) {
-        this.onTabShow = onTabShow
+        this.onTabShowListener = onTabShow
     }
 
 }
@@ -153,6 +156,33 @@ fun Class<*>.getBind(name: String? = null): Pair<Class<*>, String> {
 /**
  * 默认返回自身和自身class名小写以及请求的id
  */
-fun Class<*>.getBind(data: Pair<String, String>, name: String? = null): Triple<Class<*>, Pair<String, String>, String> {
-    return Triple(this, data, getSimpleName(name))
+fun Class<*>.getBind(name: String? = null, vararg pairs: Pair<String, Any?>): Triple<Class<*>, String, Bundle> {
+    val bundle = Bundle()
+    pairs.forEach {
+        val key = it.first
+        when (val value = it.second) {
+            is Int -> bundle.putInt(key, value)
+            is Byte -> bundle.putByte(key, value)
+            is Char -> bundle.putChar(key, value)
+            is Long -> bundle.putLong(key, value)
+            is Float -> bundle.putFloat(key, value)
+            is Short -> bundle.putShort(key, value)
+            is Double -> bundle.putDouble(key, value)
+            is Boolean -> bundle.putBoolean(key, value)
+            is String? -> bundle.putString(key, value)
+            is Bundle? -> bundle.putBundle(key, value)
+            is IntArray? -> bundle.putIntArray(key, value)
+            is ByteArray? -> bundle.putByteArray(key, value)
+            is CharArray? -> bundle.putCharArray(key, value)
+            is LongArray? -> bundle.putLongArray(key, value)
+            is FloatArray? -> bundle.putFloatArray(key, value)
+            is Parcelable? -> bundle.putParcelable(key, value)
+            is ShortArray? -> bundle.putShortArray(key, value)
+            is DoubleArray? -> bundle.putDoubleArray(key, value)
+            is BooleanArray? -> bundle.putBooleanArray(key, value)
+            is CharSequence? -> bundle.putCharSequence(key, value)
+            is Serializable? -> bundle.putSerializable(key, value)
+        }
+    }
+    return Triple(this, getSimpleName(name), bundle)
 }
