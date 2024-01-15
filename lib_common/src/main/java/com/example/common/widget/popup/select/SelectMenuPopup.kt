d6a3bfe70b1e72a927511929f865c313537b56d7
wangyanbin <wangyanbin_29@126.com>
2024-01-15 12:52:16 +0800

    推送框架

--- lib_common/src/main/java/com/example/common/widget/popup/select/SelectMenuPopup.kt
+++ lib_common/src/main/java/com/example/common/widget/popup/select/SelectMenuPopup.kt
@@ -1,24 +1,32 @@
 package com.example.common.widget.popup.select
 
+import android.view.Gravity
 import androidx.fragment.app.FragmentActivity
 import com.example.common.base.BasePopupWindow
-import com.example.common.base.PopupAnimType.TRANSLATE
-import com.example.common.databinding.ViewPopupSelectLabelBinding
+import com.example.common.databinding.ViewPopupSelectMenuBinding
+import com.example.common.utils.function.pt
 import com.example.framework.utils.function.value.orZero
 import com.example.framework.utils.function.value.safeGet
-import com.example.framework.utils.function.view.click
+import com.example.framework.utils.function.view.layoutGravity
+import com.example.framework.utils.function.view.margin
+import com.example.framework.utils.function.view.size
 
 /**
  * Created by wangyanbin
- * 底部多选弹框
+ * 顶部多选弹框
  * private val occupationPopup by lazy { SelectBottomPopup<String>(this) { it }.apply { setParams(UserAuthBean.jobList) }}
  */
-class SelectLabelPopup<T>(activity: FragmentActivity, var formatter: (T?) -> String?) : BasePopupWindow<ViewPopupSelectLabelBinding>(activity, popupAnimStyle = TRANSLATE) {
+class SelectMenuPopup<T>(activity: FragmentActivity, var formatter: (T?) -> String?) : BasePopupWindow<ViewPopupSelectMenuBinding>(activity, light = false) {
     private var onCurrent: ((item: String, index: Int) -> Unit)? = null
 
-    fun setParams(list: List<T>) {
+    fun setParams(list: List<T>, gravity: Int = Gravity.END, itemWidth: Int = 0, margin: Int = 15.pt) {
         mBinding?.apply {
+            viewArrow.layoutGravity = gravity
+            viewArrow.margin(start = if (gravity == Gravity.START) 10.pt + margin else 0, end = if (gravity == Gravity.END) 10.pt + margin else 0)
             llItem.apply {
+                if (0 != itemWidth) size(width = itemWidth)
+                layoutGravity = gravity
+                margin(start = if (gravity == Gravity.START) margin else 0, end = if (gravity == Gravity.END) margin else 0)
                 removeAllViews()
                 for (index in 0 until list.size.orZero) {
                     addView(SelectItemHolder(llItem, Pair(formatter(list.safeGet(index)).orEmpty(), index)).let {
@@ -30,7 +38,6 @@ class SelectLabelPopup<T>(activity: FragmentActivity, var formatter: (T?) -> Str
                     })
                 }
             }
-            tvCancel.click { hidden() }
         }
     }
 
