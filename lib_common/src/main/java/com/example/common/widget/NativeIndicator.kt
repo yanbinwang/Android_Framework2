8069b96f587823bfc46e53f616d8e201d6410b9d
wangyanbin <wangyanbin_29@126.com>
2024-01-24 14:01:56 +0800

    推送框架

--- lib_common/src/main/java/com/example/common/widget/NativeIndicator.kt
+++ lib_common/src/main/java/com/example/common/widget/NativeIndicator.kt
@@ -4,10 +4,12 @@ import android.widget.TextView
 import com.example.common.R
 import com.example.common.databinding.ItemTabBinding
 import com.example.common.utils.builder.TabLayoutBuilder
+import com.example.common.utils.function.pt
 import com.example.common.utils.function.setArguments
 import com.example.framework.utils.function.inflate
 import com.example.framework.utils.function.value.orZero
 import com.example.framework.utils.function.view.bold
+import com.example.framework.utils.function.view.padding
 import com.example.framework.utils.function.view.textSize
 import com.google.android.material.tabs.TabLayout
 
@@ -24,11 +26,6 @@ class NativeIndicator constructor(tab: TabLayout?, tabTitle: List<String>?) : Ta
     override fun onBindView(mBinding: ItemTabBinding?, item: String?, selected: Boolean, index: Int) {
         if(null == redraw) {
             mBinding?.tvTitle.setTabTheme(item, selected)
-//            binding?.tvTitle.apply {
-//                setArguments(item.orEmpty(), if (selected) R.color.appTheme else R.color.textHint)
-//                textSize(if (selected) 16.pt else 14.pt)
-//                bold(selected)
-//            }
         } else {
             redraw?.invoke(mBinding, item, selected, index)
         }
@@ -47,8 +44,9 @@ class NativeIndicator constructor(tab: TabLayout?, tabTitle: List<String>?) : Ta
 /**
  * 全局默认样式
  */
-fun TextView?.setTabTheme(text: String?, selected: Boolean, colorRes: Pair<Int, Int> = R.color.tabSelected to R.color.tabUnselected, sizeRes: Pair<Int, Int> = R.dimen.textSize16 to R.dimen.textSize15) {
+fun TextView?.setTabTheme(text: String?, selected: Boolean, colorRes: Pair<Int, Int> = R.color.tabSelected to R.color.tabUnselected, sizeRes: Pair<Int, Int> = R.dimen.textSize16 to R.dimen.textSize15, padding: Pair<Int, Int> = 6.pt to 6.pt) {
     setArguments(text.orEmpty(), if (selected) colorRes.first.orZero else colorRes.second.orZero,)
     textSize(if (selected) sizeRes.first.orZero else sizeRes.second.orZero)
+    padding(start = padding.first, end = padding.second)
     bold(selected)
 }
