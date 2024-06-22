198349f51bd901af2df3cedd86326f227a13d95a
wangyanbin <wangyanbin_29@126.com>
2024-06-22 12:50:31 +0800

    推送框架

--- lib_local/glide/src/main/java/com/example/glide/GlideBindingAdapter.kt
+++ lib_local/glide/src/main/java/com/example/glide/GlideBindingAdapter.kt
@@ -38,56 +38,56 @@ object GlideBindingAdapter {
     /**
      * 加载图片
      */
-    @JvmStatic
-    @BindingAdapter(value = ["display", "display_error"], requireAll = false)
-    fun bindingDisplay(view: ImageView, url: String, error: String?) = view.context.execute {
-        ImageLoader.instance.displayDefType(view, url, if (error.isNullOrEmpty()) drawable(R.drawable.shape_glide_bg) else defTypeMipmap(error), { view.disable() }, onComplete = { view.enable() })
-    }
-
 //    @JvmStatic
-//    @BindingAdapter(value = ["display", "display_errorId"], requireAll = false)
-//    fun bindingDisplay(view: ImageView, url: String, errorId: Int?) = view.context.execute {
-//        ImageLoader.instance.display(view, url, errorId, { view.disable() }, onComplete = { view.enable() })
+//    @BindingAdapter(value = ["display", "display_error"], requireAll = false)
+//    fun bindingDisplay(view: ImageView, url: String, error: String?) = view.context.execute {
+//        ImageLoader.instance.displayDefType(view, url, if (error.isNullOrEmpty()) drawable(R.drawable.shape_glide_bg) else defTypeMipmap(error), { view.disable() }, onComplete = { view.enable() })
 //    }
 
     @JvmStatic
-    @BindingAdapter(value = ["display_resource"])
-    fun bindingDisplayResource(view: ImageView, resource: String) = view.context.execute {
-        ImageLoader.instance.displayDefType(view, defTypeMipmap(resource), onStart = { view.disable() }, onComplete = { view.enable() })
+    @BindingAdapter(value = ["display", "display_errorId"], requireAll = false)
+    fun bindingDisplay(view: ImageView, url: String, errorId: Int?) = view.context.execute {
+        ImageLoader.instance.display(view, url, errorId, { view.disable() }, onComplete = { view.enable() })
     }
 
 //    @JvmStatic
-//    @BindingAdapter(value = ["display_resourceId"])
-//    fun bindingDisplayResource(view: ImageView, resourceId: Int) = view.context.execute {
-//        ImageLoader.instance.display(view, resourceId, onStart = { view.disable() }, onComplete = { view.enable() })
+//    @BindingAdapter(value = ["display_resource"])
+//    fun bindingDisplayResource(view: ImageView, resource: String) = view.context.execute {
+//        ImageLoader.instance.displayDefType(view, defTypeMipmap(resource), onStart = { view.disable() }, onComplete = { view.enable() })
 //    }
+
+    @JvmStatic
+    @BindingAdapter(value = ["display_resourceId"])
+    fun bindingDisplayResource(view: ImageView, resourceId: Int) = view.context.execute {
+        ImageLoader.instance.display(view, resourceId, onStart = { view.disable() }, onComplete = { view.enable() })
+    }
 
     /**
      * 加载图片（带圆角）
      */
-    @JvmStatic
-    @BindingAdapter(value = ["display_round", "round_radius", "display_round_error"], requireAll = false)
-    fun bindingDisplayRound(view: ImageView, url: String, roundRadius: Int?, roundError: String?) = view.context.execute {
-        ImageLoader.instance.displayRoundDefType(view, url, if (roundError.isNullOrEmpty()) drawable(R.drawable.shape_glide_bg) else defTypeMipmap(roundError), roundRadius.toSafeInt(5))
-    }
-
 //    @JvmStatic
-//    @BindingAdapter(value = ["display_round", "round_radius", "display_round_errorId"], requireAll = false)
-//    fun bindingDisplayRound(view: ImageView, url: String, roundRadius: Int?, roundErrorId: Int?) = view.context.execute {
-//        ImageLoader.instance.displayRound(view, url, roundErrorId, roundRadius.toSafeInt(5))
+//    @BindingAdapter(value = ["display_round", "round_radius", "display_round_error"], requireAll = false)
+//    fun bindingDisplayRound(view: ImageView, url: String, roundRadius: Int?, roundError: String?) = view.context.execute {
+//        ImageLoader.instance.displayRoundDefType(view, url, if (roundError.isNullOrEmpty()) drawable(R.drawable.shape_glide_bg) else defTypeMipmap(roundError), roundRadius.toSafeInt(5))
 //    }
 
     @JvmStatic
-    @BindingAdapter(value = ["display_round_resource", "round_radius"], requireAll = false)
-    fun bindingDisplayRoundResource(view: ImageView, resource: String, roundRadius: Int?) = view.context.execute {
-        ImageLoader.instance.displayRoundDefType(view, defTypeMipmap(resource), radius = roundRadius.toSafeInt(5))
+    @BindingAdapter(value = ["display_round", "round_radius", "display_round_errorId"], requireAll = false)
+    fun bindingDisplayRound(view: ImageView, url: String, roundRadius: Int?, roundErrorId: Int?) = view.context.execute {
+        ImageLoader.instance.displayRound(view, url, roundErrorId, roundRadius.toSafeInt(5))
     }
 
 //    @JvmStatic
-//    @BindingAdapter(value = ["display_round_resourceId", "round_radius"], requireAll = false)
-//    fun bindingDisplayRoundResource(view: ImageView, resourceId: Int, roundRadius: Int?) = view.context.execute {
-//        ImageLoader.instance.displayRound(view, resourceId, radius = roundRadius.toSafeInt(5))
+//    @BindingAdapter(value = ["display_round_resource", "round_radius"], requireAll = false)
+//    fun bindingDisplayRoundResource(view: ImageView, resource: String, roundRadius: Int?) = view.context.execute {
+//        ImageLoader.instance.displayRoundDefType(view, defTypeMipmap(resource), radius = roundRadius.toSafeInt(5))
 //    }
+
+    @JvmStatic
+    @BindingAdapter(value = ["display_round_resourceId", "round_radius"], requireAll = false)
+    fun bindingDisplayRoundResource(view: ImageView, resourceId: Int, roundRadius: Int?) = view.context.execute {
+        ImageLoader.instance.displayRound(view, resourceId, radius = roundRadius.toSafeInt(5))
+    }
 
     /**
      * 加载图片（圆形）
@@ -98,17 +98,17 @@ object GlideBindingAdapter {
         ImageLoader.instance.displayCircle(view, url)
     }
 
-    @JvmStatic
-    @BindingAdapter(value = ["display_circle_resource", "display_circle_error"], requireAll = false)
-    fun bindingDisplayCircleResource(view: ImageView, resource: String, circleError: String?) = view.context.execute {
-        ImageLoader.instance.displayCircleDefType(view, defTypeMipmap(resource), if (circleError.isNullOrEmpty()) drawable(R.drawable.shape_glide_oval_bg) else defTypeMipmap(circleError))
-    }
-
 //    @JvmStatic
-//    @BindingAdapter(value = ["display_circle_resourceId", "display_circle_errorId"], requireAll = false)
-//    fun bindingDisplayCircleResource(view: ImageView, resourceId: Int, circleErrorId: Int?) = view.context.execute {
-//        ImageLoader.instance.displayCircle(view, resourceId, circleErrorId)
+//    @BindingAdapter(value = ["display_circle_resource", "display_circle_error"], requireAll = false)
+//    fun bindingDisplayCircleResource(view: ImageView, resource: String, circleError: String?) = view.context.execute {
+//        ImageLoader.instance.displayCircleDefType(view, defTypeMipmap(resource), if (circleError.isNullOrEmpty()) drawable(R.drawable.shape_glide_oval_bg) else defTypeMipmap(circleError))
 //    }
+
+    @JvmStatic
+    @BindingAdapter(value = ["display_circle_resourceId", "display_circle_errorId"], requireAll = false)
+    fun bindingDisplayCircleResource(view: ImageView, resourceId: Int, circleErrorId: Int?) = view.context.execute {
+        ImageLoader.instance.displayCircle(view, resourceId, circleErrorId)
+    }
     // </editor-fold>
 
 }
