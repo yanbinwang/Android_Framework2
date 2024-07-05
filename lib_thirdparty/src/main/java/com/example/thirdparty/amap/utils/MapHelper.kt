8be715392daa5ffb78f0dbbbdbb9e6ea3d5c970f
wangyanbin <wangyanbin_29@126.com>
2024-07-05 13:13:12 +0800

    推送框架

--- lib_thirdparty/src/main/java/com/example/thirdparty/amap/utils/MapHelper.kt
+++ lib_thirdparty/src/main/java/com/example/thirdparty/amap/utils/MapHelper.kt
@@ -33,8 +33,8 @@ import kotlin.math.roundToInt
  */
 class MapHelper(private val mActivity: FragmentActivity) : LifecycleEventObserver {
     private var mapView: MapView? = null
-    private val location by lazy { LocationHelper(mActivity) }
     private val mapLatLng by lazy { aMapLatLng.get().toObj(LatLng::class.java) }//默认地图经纬度-杭州
+    private val location by lazy { LocationHelper(mActivity) }
     var aMap: AMap? = null
         private set
 
