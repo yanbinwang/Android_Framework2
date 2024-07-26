881c9d932cf02485fbd6153da2dc1566808b6305
wangyanbin <wangyanbin_29@126.com>
2024-07-26 14:42:44 +0800

    推送框架

--- module_home/src/main/java/com/example/home/activity/WebActivity.kt
+++ module_home/src/main/java/com/example/home/activity/WebActivity.kt
@@ -7,9 +7,11 @@ import com.example.common.base.BaseTitleActivity
 import com.example.common.base.page.Extra
 import com.example.common.bean.WebBundle
 import com.example.common.config.ARouterPath
+import com.example.common.utils.function.pt
 import com.example.framework.utils.function.intentSerializable
 import com.example.framework.utils.function.value.orFalse
 import com.example.framework.utils.function.value.orTrue
+import com.example.framework.utils.function.view.gone
 import com.example.home.R
 import com.example.home.databinding.ActivityWebBinding
 import com.example.home.utils.WebHelper
@@ -18,7 +20,7 @@ import com.example.home.utils.WebImpl
 @Route(path = ARouterPath.WebActivity)
 class WebActivity : BaseTitleActivity<ActivityWebBinding>(), WebImpl {
     private val bean by lazy { intentSerializable<WebBundle>(Extra.BUNDLE_BEAN) }
-    private val webHelper by lazy { WebHelper(this, mBinding).apply { setBundle(bean, this@WebActivity) } }
+    private val helper by lazy { WebHelper(this, mBinding).apply { setBundle(bean, this@WebActivity) } }
 
     override fun initView(savedInstanceState: Bundle?) {
         super.initView(savedInstanceState)
@@ -27,7 +29,6 @@ class WebActivity : BaseTitleActivity<ActivityWebBinding>(), WebImpl {
         titleBuilder.apply {
             if (bean?.getTitleRequired().orTrue) {
                 setTitle(bean?.getTitle().orEmpty())
-                setRight(R.mipmap.ic_refresh) { webHelper.refresh() }
             } else {
                 hideTitle()
             }
@@ -36,19 +37,26 @@ class WebActivity : BaseTitleActivity<ActivityWebBinding>(), WebImpl {
 
     override fun initEvent() {
         super.initEvent()
-        webHelper.setClientListener({}, {
-            if (bean?.isTitleRequired().orFalse && !it.isNullOrEmpty()) titleBuilder.setTitle(it)
+        helper.setClientListener({
+            titleBuilder.ivRight.gone()
+        }, {
+            if (bean?.isTitleRequired().orFalse) {
+                titleBuilder
+                    //当传输的title为空时，取一次网页自带的标题并且刷新按钮浮现
+                    .setTitle(bean?.getTitle() ?: it.orEmpty())
+                    .setRight(R.mipmap.ic_refresh, R.color.bgBlack, 60.pt, 60.pt) { helper.refresh() }
+            }
         })
     }
 
     override fun initData() {
         super.initData()
-        webHelper.load()
+        helper.load()
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
-            webHelper.onKeyDown()
+            helper.onKeyDown()
         }
         return true
     }
