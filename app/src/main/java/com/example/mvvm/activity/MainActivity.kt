4e9b3ac292abeaeee78b957cea7596b255f34269
wangyanbin <wangyanbin_29@126.com>
2024-08-06 12:29:07 +0800

    推送框架

--- app/src/main/java/com/example/mvvm/activity/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/activity/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.mvvm.activity
 
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.example.common.base.BaseActivity
 import com.example.common.config.ARouterPath
@@ -10,5 +15,30 @@ import com.example.mvvm.databinding.ActivityMainBinding
  */
 @Route(path = ARouterPath.MainActivity)
 class MainActivity : BaseActivity<ActivityMainBinding>() {
+    private val STREAM_TYPES = arrayOf("Video-Audio", "Audio", "Import", "Screen")
 
+    override fun initView(savedInstanceState: Bundle?) {
+        super.initView(savedInstanceState)
+        initStreamTypeSpinner()
+    }
+
+    private fun initStreamTypeSpinner() {
+        mBinding?.spStreamTypes?.adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            STREAM_TYPES
+        ).apply { setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) }
+        mBinding?.spStreamTypes?.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
+    }
 }
