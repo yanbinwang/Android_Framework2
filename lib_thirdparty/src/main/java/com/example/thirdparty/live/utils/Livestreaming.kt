1982f0dc20fb49885511ecc971ac32f42a349c31
wangyanbin <wangyanbin_29@126.com>
2024-09-15 10:36:38 +0800

    推送框架

--- lib_thirdparty/src/main/java/com/example/thirdparty/live/utils/Livestreaming.kt
+++ lib_thirdparty/src/main/java/com/example/thirdparty/live/utils/Livestreaming.kt
@@ -21,7 +21,8 @@ import java.io.File
 
 /**
  * 直播库初始化
- * 由于隐私协议问题，会在调取initPrivacyAgreed后调取init方法
+ * 1.遇到app隐私协议问题，会在调取initPrivacyAgreed后调取init方法
+ * 2.如需接入日志上报，开启XCrash的依赖
  */
 object Livestreaming {
     private var initialized = false
@@ -31,20 +32,22 @@ object Livestreaming {
 
     /**
      * 注意：参数 userId 代表用户的唯一标识符，用于区分不同的用户
-     * 用户登录时主动调取updateUid/application中也做一次调取
+     * application中做一次调取，如果隐私协议未启用则延后调取
      */
     @JvmStatic
-    fun init(application: Application) {
-        //初始化推送/获取当前登录用户的userid
+    fun init(application: Application, crashEnabled: Boolean = true) {
+        //初始化直播/获取当前登录用户的userid，未登录sdk内部会随机产生一组uid
         StreamingEnv.init(application.applicationContext, AccountHelper.getUserId().ifEmpty { "" })
         //设置日志等级
         StreamingEnv.setLogLevel(Log.INFO)
         //开启日志的本地保存，保存在应用私有目录(getExternalFilesDir) 或者 getFilesDir 文件目录下的 Pili 文件夹中
-        //默认为关闭
-        StreamingEnv.setLogfileEnabled(true)
-        //开启日志收集
-        openCrash(application)
-        //保活
+        if (crashEnabled) {
+            //默认为关闭
+            StreamingEnv.setLogfileEnabled(true)
+            //开启日志收集
+            openCrash(application)
+        }
+        //保活服务/<service android:name="com.example.thirdparty.live.service.KeepAppAliveService" />
         AppStateTracker.track(application, object : AppStateTracker.AppStateChangeListener {
             override fun appTurnIntoForeground() {
                 stopService(application)
@@ -60,9 +63,6 @@ object Livestreaming {
         })
     }
 
-    /**
-     * application种attachBaseContext调取
-     */
     private fun openCrash(mContext: Context) {
         if (!initialized) {
             initialized = true
@@ -100,7 +100,7 @@ object Livestreaming {
         }
     }
 
-    internal fun startService(mContext: Context) {
+    private fun startService(mContext: Context) {
         if (mServiceIntent == null) {
             mServiceIntent = Intent(mContext, KeepAppAliveService::class.java)
         }
@@ -108,7 +108,7 @@ object Livestreaming {
         mIsServiceAlive = true
     }
 
-    internal fun stopService(mContext: Context) {
+    private fun stopService(mContext: Context) {
         if (mIsServiceAlive) {
             mContext.stopService(mServiceIntent)
             mServiceIntent = null
@@ -117,7 +117,8 @@ object Livestreaming {
     }
 
     /**
-     * 主动上报日志
+     * 主动上报日志，默认位置是七牛云自己在android/包名文件夹下创建的Pili文件
+     * sdk并未提供清空的api，故而主动上报后做一次清空
      */
     @JvmStatic
     fun reportLog() {
@@ -139,7 +140,7 @@ object Livestreaming {
     }
 
     /**
-     * 针对项目用户登录退出时候的userid
+     * 用户登录退出时候调取
      * 如果uid为空，sdk内部会通过随机数自动产生一个
      */
     @JvmStatic
