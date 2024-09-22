fd52fb13abdf60780d6dace0502e67695c08e853
wangyanbin <wangyanbin_29@126.com>
2024-09-22 10:25:32 +0800

    推送框架

--- lib_common/src/main/java/com/example/common/utils/file/FileBuilder.kt
+++ lib_common/src/main/java/com/example/common/utils/file/FileBuilder.kt
@@ -21,6 +21,8 @@ import com.example.common.utils.function.loadBitmap
 import com.example.common.utils.function.loadLayout
 import com.example.common.utils.function.saveBit
 import com.example.framework.utils.function.doOnDestroy
+import com.example.framework.utils.function.value.DateFormat.EN_YMDHMS
+import com.example.framework.utils.function.value.convert
 import com.example.framework.utils.function.value.toSafeInt
 import com.example.framework.utils.logWTF
 import com.example.glide.ImageLoader
@@ -34,6 +36,7 @@ import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.FileOutputStream
+import java.util.Date
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 import kotlin.coroutines.CoroutineContext
@@ -57,7 +60,7 @@ class FileBuilder(observer: LifecycleOwner) : CoroutineScope {
         /**
          * 存储图片
          */
-        suspend fun suspendingSavePic(bitmap: Bitmap, root: String, fileName: String, deleteDir: Boolean = false, format: Bitmap.CompressFormat = JPEG): String? {
+        suspend fun suspendingSavePic(bitmap: Bitmap?, root: String = getStoragePath("保存图片"), fileName: String = EN_YMDHMS.convert(Date()), deleteDir: Boolean = false, format: Bitmap.CompressFormat = JPEG): String? {
             return withContext(IO) { saveBit(bitmap, root, fileName, deleteDir, format) }
         }
 
@@ -453,7 +456,7 @@ class FileBuilder(observer: LifecycleOwner) : CoroutineScope {
         onStart()
         viewJob?.cancel()
         viewJob = launch {
-            val filePath = withContext(IO) { saveBit(suspendingSaveView(view, width, height)) }
+            val filePath = suspendingSavePic(suspendingSaveView(view, width, height))
             onResult.invoke(filePath)
         }
     }
