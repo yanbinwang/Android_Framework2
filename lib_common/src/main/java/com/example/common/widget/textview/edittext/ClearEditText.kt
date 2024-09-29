012b7960b0fda169cccf06b2225cd49cc0843777
wangyanbin <wangyanbin_29@126.com>
2024-09-29 11:15:52 +0800

    规整

--- lib_common/src/main/java/com/example/common/widget/textview/edittext/ClearEditText.kt
+++ lib_common/src/main/java/com/example/common/widget/textview/edittext/ClearEditText.kt
@@ -40,7 +40,7 @@ class ClearEditText @JvmOverloads constructor(context: Context, attrs: Attribute
     private var isDisabled = false//是否不可操作
     private var isShowBtn = true//是否显示清除按钮
     private var onTextChanged: ((s: Editable?) -> Unit)? = null
-    private var onFocusChange: ((v: View?, hasFocus: Boolean?) -> Unit)? = null
+//    private var onFocusChange: ((v: View?, hasFocus: Boolean?) -> Unit)? = null
     private val mBinding by lazy { ViewClearEditBinding.bind(context.inflate(R.layout.view_clear_edit)) }
     val editText get() = mBinding.etClear
 
@@ -52,9 +52,9 @@ class ClearEditText @JvmOverloads constructor(context: Context, attrs: Attribute
                 mBinding.ivClear.visibility = if (it.toString().isEmpty()) View.GONE else View.VISIBLE
                 onTextChanged?.invoke(it)
             }
-            onFocusChangeListener = OnFocusChangeListener { v, hasFocus ->
-                onFocusChange?.invoke(v, hasFocus)
-            }
+//            onFocusChangeListener = OnFocusChangeListener { v, hasFocus ->
+//                onFocusChange?.invoke(v, hasFocus)
+//            }
         }
         mBinding.ivClear.click { mBinding.etClear.setText("") }
         //以下属性在xml中前缀使用app:调取
@@ -89,6 +89,7 @@ class ClearEditText @JvmOverloads constructor(context: Context, attrs: Attribute
             val maxLine = typedArray.getInt(R.styleable.ClearEditText_maxLine, -1)
             if (minLine > 0 || maxLine > 0) {
                 mBinding.etClear.isSingleLine = false
+//                mBinding.etClear.setPaddingRelative(0,12.pt,0,12.pt)
             } else {
                 mBinding.etClear.isSingleLine = true
                 mBinding.etClear.maxLines = 1
@@ -226,7 +227,8 @@ class ClearEditText @JvmOverloads constructor(context: Context, attrs: Attribute
     }
 
     fun setOnFocusChangeListener(onFocusChange: ((v: View?, hasFocus: Boolean?) -> Unit)) {
-        this.onFocusChange = onFocusChange
+//        this.onFocusChange = onFocusChange
+        onFocusChangeListener = OnFocusChangeListener { v, hasFocus -> onFocusChange.invoke(v, hasFocus) }
     }
 
     fun setOnEditorActionListener(listener: TextView.OnEditorActionListener) {
