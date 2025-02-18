5a9faec81656270c2391f1a2078fa927d91406b9
wangyanbin <wangyanbin_29@126.com>
2025-02-18 10:49:57 +0800

    规整

--- lib_common/src/main/java/com/example/common/base/page/PageHandler.kt
+++ lib_common/src/main/java/com/example/common/base/page/PageHandler.kt
@@ -20,24 +20,24 @@ import com.example.framework.utils.function.value.orZero
 import java.io.Serializable
 
 /**
- * 页面工具类
- * 1.接口提示
- * 2.遮罩层操作
- */
-fun ViewGroup?.setState(resId: Int? = null, resText: Int? = null, index: Int = 1) {
-    this ?: return
-    val emptyLayout = if (this is EmptyLayout) this else getEmptyView(index)
-    emptyLayout?.error(resId, resText)
-}
-
-/**
  * 列表页调取方法
  */
 fun XRecyclerView?.setState(length: Int = 0, resId: Int? = null, resText: Int? = null) {
     this ?: return
     finishRefreshing()
     //判断集合长度，有长度不展示emptyview只做提示
-    if (length <= 0) empty?.setState(resId, resText)
+    if (length <= 0) empty.setEmptyState(resId, resText)
+}
+
+/**
+ * 页面工具类
+ * 1.接口提示
+ * 2.遮罩层操作
+ */
+fun ViewGroup?.setEmptyState(resId: Int? = null, resText: Int? = null, index: Int = 1) {
+    this ?: return
+    val emptyLayout = if (this is EmptyLayout) this else getEmptyView(index)
+    emptyLayout?.error(resId, resText)
 }
 
 /**
