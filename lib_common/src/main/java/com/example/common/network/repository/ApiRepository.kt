87103474f8392b8aa9fd92f56de48d3b18c640c9
wangyanbin <wangyanbin_29@126.com>
2025-03-02 17:52:11 +0800

    规整

--- lib_common/src/main/java/com/example/common/network/repository/ApiRepository.kt
+++ lib_common/src/main/java/com/example/common/network/repository/ApiRepository.kt
@@ -14,11 +14,6 @@ import com.example.common.utils.toJson
 import com.example.framework.utils.logE
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.withContext
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody
@@ -211,31 +206,6 @@ suspend fun <T> requestLayer(
         log("结束请求")
         end()
     }
-//    log("开始请求")
-//    flow {
-//        val value = withContext(IO) {
-//            log("发起请求")
-//            coroutineScope()
-//        }
-//        emit(value)
-//    }.flowOn(Main).catch {
-//        if (isShowToast) "".responseToast()
-//        //可根据具体异常显示具体错误提示,此处可能是框架/服务器报错（没有提供规定的json结构体）或者json结构解析错误
-//        err(Triple(FAILURE, "", it as? Exception))
-//    }.onCompletion {
-//        log("结束请求")
-//        end()
-//    }.collect {
-//        log("处理结果")
-//        if (it.successful()) {
-//            resp(it)
-//        } else {
-//            //如果不是被顶号才会有是否提示的逻辑
-//            if (!it.tokenExpired()) if (isShowToast) it.content.responseToast()
-//            //不管结果如何，失败的回调是需要执行的
-//            err(Triple(it.statusCode, it.content, null))
-//        }
-//    }
 }
 
 suspend fun <T> requestAffair(
@@ -253,17 +223,6 @@ suspend fun <T> requestAffair(
     } finally {
         end()
     }
-//    flow {
-//        val value = withContext(IO) { coroutineScope() }
-//        emit(value)
-//    }.flowOn(Main).catch {
-//        if (isShowToast) "".responseToast()
-//        err(Triple(FAILURE, "", it as? Exception))
-//    }.onCompletion {
-//        end()
-//    }.collect {
-//        resp(it)
-//    }
 }
 
 private fun log(msg: String) = "${msg}->当前线程：${Thread.currentThread().name}".logE("repository")
