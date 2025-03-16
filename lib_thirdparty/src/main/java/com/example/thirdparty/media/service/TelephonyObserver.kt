c8623249396aa361ffbaefe40a56ae30407bab73
wangyanbin <wangyanbin_29@126.com>
2025-03-17 00:10:36 +0800

    替换evenbus

--- lib_thirdparty/src/main/java/com/example/thirdparty/media/service/TelephonyObserver.kt
+++ lib_thirdparty/src/main/java/com/example/thirdparty/media/service/TelephonyObserver.kt
@@ -21,37 +21,10 @@ class TelephonyObserver(private val mActivity: FragmentActivity) : LifecycleEven
     private val phoneState by lazy { MyPhoneStateListener() }
     private val callState by lazy { MyCallStateListener() }
     private val highVersion get() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
-
-    companion object {
-        private var listener: OnTelephonyListener? = null
-
-        @JvmStatic
-        @Synchronized
-        private fun onChanged(state: Int) {
-            when (state) {
-                //手机状态：空闲状态
-                TelephonyManager.CALL_STATE_IDLE -> listener?.onIdle()
-                //手机状态：来电话状态
-                TelephonyManager.CALL_STATE_RINGING -> listener?.onRinging()
-                //手机状态：接电话状态
-                TelephonyManager.CALL_STATE_OFFHOOK -> listener?.onOffHook()
-            }
-        }
-    }
+    private var listener: OnTelephonyListener? = null
 
     init {
         mActivity.lifecycle.addObserver(this)
-    }
-
-    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
-        when (event) {
-            Lifecycle.Event.ON_CREATE -> register()
-            Lifecycle.Event.ON_DESTROY -> {
-                unregister()
-                mActivity.lifecycle.removeObserver(this)
-            }
-            else -> {}
-        }
     }
 
     private fun register() {
@@ -68,6 +41,7 @@ class TelephonyObserver(private val mActivity: FragmentActivity) : LifecycleEven
         } else {
             manager?.listen(phoneState, PhoneStateListener.LISTEN_NONE)
         }
+        listener = null
     }
 
     /**
@@ -97,18 +71,47 @@ class TelephonyObserver(private val mActivity: FragmentActivity) : LifecycleEven
         fun onOffHook()
     }
 
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        when (event) {
+            Lifecycle.Event.ON_CREATE -> register()
+            Lifecycle.Event.ON_DESTROY -> {
+                unregister()
+                mActivity.lifecycle.removeObserver(this)
+            }
+            else -> {}
+        }
+    }
+
+    /**
+     * inner class 是一种特殊的内部类，它可以访问外部类的成员（包括私有成员），并且持有一个对外部类实例的引用。
+     * 这使得内部类能够与外部类进行更紧密的交互，方便实现一些需要访问外部类状态的功能，比如事件监听器、迭代器等。
+     */
     @RequiresApi(api = Build.VERSION_CODES.S)
-    private class MyCallStateListener : TelephonyCallback(), CallStateListener {
+    private inner class MyCallStateListener : TelephonyCallback(), CallStateListener {
         override fun onCallStateChanged(state: Int) {
             onChanged(state)
         }
     }
 
-    private class MyPhoneStateListener : PhoneStateListener() {
+    private inner class MyPhoneStateListener : PhoneStateListener() {
         @Deprecated("Deprecated in Java")
         override fun onCallStateChanged(state: Int, phoneNumber: String) {
             super.onCallStateChanged(state, phoneNumber)
             onChanged(state)
+        }
+    }
+
+    /**
+     * 全局回调处理
+     */
+    private fun onChanged(state: Int) {
+        when (state) {
+            // 手机状态：空闲状态
+            TelephonyManager.CALL_STATE_IDLE -> listener?.onIdle()
+            // 手机状态：来电话状态
+            TelephonyManager.CALL_STATE_RINGING -> listener?.onRinging()
+            // 手机状态：接电话状态
+            TelephonyManager.CALL_STATE_OFFHOOK -> listener?.onOffHook()
         }
     }
 
