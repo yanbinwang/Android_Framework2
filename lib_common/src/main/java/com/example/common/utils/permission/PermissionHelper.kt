909f22c6b33bb9076a27b30a0ba31cfa453fd82c
wangyanbin <wangyanbin_29@126.com>
2025-04-19 20:07:45 +0800

    kts替换/升级

--- lib_common/src/main/java/com/example/common/utils/permission/PermissionHelper.kt
+++ lib_common/src/main/java/com/example/common/utils/permission/PermissionHelper.kt
@@ -51,7 +51,9 @@ class PermissionHelper(private val context: Context) {
                         if (force) onDenied(permissions)
                     }
                 })
-        } else listener.invoke(true)
+        } else {
+            listener.invoke(true)
+        }
     }
 
     /**
--- lib_common/src/main/java/com/example/common/utils/permission/XXPermissionsGroup.kt
+++ lib_common/src/main/java/com/example/common/utils/permission/XXPermissionsGroup.kt
@@ -2,6 +2,7 @@ package com.example.common.utils.permission
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.core.app.ActivityCompat
 import com.hjq.permissions.Permission
 
@@ -17,7 +18,11 @@ object XXPermissionsGroup {
     val PHONE = arrayOf(Permission.READ_PHONE_STATE, Permission.CALL_PHONE, Permission.READ_CALL_LOG, Permission.WRITE_CALL_LOG, Permission.ADD_VOICEMAIL, Permission.USE_SIP, Permission.PROCESS_OUTGOING_CALLS)
     val SENSORS = arrayOf(Permission.BODY_SENSORS)
     val SMS = arrayOf(Permission.SEND_SMS, Permission.RECEIVE_SMS, Permission.READ_SMS, Permission.RECEIVE_WAP_PUSH, Permission.RECEIVE_MMS)
-    val STORAGE = arrayOf(Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE)
+    val STORAGE = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        arrayOf(Permission.READ_MEDIA_IMAGES, Permission.READ_MEDIA_VIDEO, Permission.READ_MEDIA_AUDIO)
+    } else {
+        arrayOf(Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE)
+    }
 }
 
 /**
