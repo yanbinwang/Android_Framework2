963da01d61ce3579239038cf7c95570729ef737b
wangyanbin <wangyanbin_29@126.com>
2025-04-27 22:55:11 +0800

    kts替换/升级

--- lib_framework/src/main/java/com/example/framework/widget/BaseViewGroup.kt
+++ lib_framework/src/main/java/com/example/framework/widget/BaseViewGroup.kt
@@ -2,72 +2,62 @@ package com.example.framework.widget
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isEmpty
+import androidx.core.view.isNotEmpty
 
 /**
  * 自定义控件继承viewgroup需要清除边距，使用当前类做处理
  * 自定义控件如果宽度是手机宽度，则可用当前BaseViewGroup，否则推荐使用继承FrameLayout
  */
 abstract class BaseViewGroup @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : ViewGroup(context, attrs, defStyleAttr) {
-    //检测布局绘制
+    //检测布局绘制->只容许容器内有一个插入的xml
     protected val isInflate get() = childCount <= 0
 
+    /**
+     * MeasureSpec.EXACTLY：父布局已经明确指定了子布局的大小，子布局应该按照这个指定大小来布局。
+     * MeasureSpec.AT_MOST：子布局最大可以达到父布局指定的大小，但子布局可以根据自身内容调整大小，不过不能超过父布局指定的最大值。
+     * MeasureSpec.UNSPECIFIED：父布局对子布局的大小没有限制，子布局可以根据自身内容来决定大小。
+     */
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        for (i in 0 until childCount) {
-            getChildAt(i).measure(widthMeasureSpec, heightMeasureSpec)
+        // 判空
+        if (isEmpty()) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+            return
         }
+        val child = getChildAt(0)
+        // 强制子视图使用父容器的测量规格（无论子视图布局参数）
+        child.measure(widthMeasureSpec, heightMeasureSpec)
+        // 父容器尺寸与子视图完全一致
+        setMeasuredDimension(child.measuredWidth, child.measuredHeight)
     }
 
+    /**
+     * 所有子类的子视图都撑满容器
+     */
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-        for (i in 0 until childCount) {
-            getChildAt(i).layout(0, 0, r, b)
+        if (isNotEmpty()) {
+            val child = getChildAt(0)
+            // 子视图撑满父容器
+            child.layout(0, 0, measuredWidth, measuredHeight)
         }
     }
 
-//    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-//        var maxWidth = 0
-//        var maxHeight = 0
-//
-//        // 测量每个子视图
-//        for (i in 0 until childCount) {
-//            val child = getChildAt(i)
-//            measureChild(child, widthMeasureSpec, heightMeasureSpec)
-//            maxWidth = maxWidth.coerceAtLeast(child.measuredWidth)
-//            maxHeight = maxHeight.coerceAtLeast(child.measuredHeight)
-//        }
-//
-//        // 根据测量模式计算最终的宽度和高度
-//        val widthMode = MeasureSpec.getMode(widthMeasureSpec)
-//        val widthSize = MeasureSpec.getSize(widthMeasureSpec)
-//        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
-//        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
-//
-//        val finalWidth = when (widthMode) {
-//            MeasureSpec.EXACTLY -> widthSize
-//            MeasureSpec.AT_MOST -> maxWidth.coerceAtMost(widthSize)
-//            else -> maxWidth
-//        }
-//
-//        val finalHeight = when (heightMode) {
-//            MeasureSpec.EXACTLY -> heightSize
-//            MeasureSpec.AT_MOST -> maxHeight.coerceAtMost(heightSize)
-//            else -> maxHeight
-//        }
-//
-//        setMeasuredDimension(finalWidth, finalHeight)
-//    }
-//
-//    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-//        for (i in 0 until childCount) {
-//            val child = getChildAt(i)
-//            child.layout(0, 0, child.measuredWidth, child.measuredHeight)
-//        }
-//    }
-
+    /**
+     * 当页面被绘制好了，如果本身内部没有容器，回调onInflate抽象方法，让自定义view去在onInflate方法里去addView
+     */
     override fun onFinishInflate() {
         super.onFinishInflate()
         if (isInflate) onInflate()
+    }
+
+    /**
+     * 虽然自定义view在调用addView的时候已经用了isInflate做判断，但是我们还是重写一下该方法，抛一个错
+     */
+    override fun addView(child: View?, index: Int, params: LayoutParams?) {
+        require(isEmpty()) { "容器只能包含一个子视图（XML 根布局）" }
+        super.addView(child, index, params)
     }
 
     /**
