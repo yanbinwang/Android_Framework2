b1922dd7491514a2567aa98a56770cc473088617
wangyanbin <wangyanbin_29@126.com>
2025-04-28 13:43:26 +0800

    kts替换/升级

--- lib_common/src/main/java/com/example/common/widget/popup/select/SelectMenuPopup.kt
+++ lib_common/src/main/java/com/example/common/widget/popup/select/SelectMenuPopup.kt
@@ -2,19 +2,22 @@ package com.example.common.widget.popup.select
 
 import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import androidx.fragment.app.FragmentActivity
 import com.example.common.R
 import com.example.common.base.BasePopupWindow
 import com.example.common.databinding.ViewPopupSelectMenuBinding
 import com.example.common.utils.ScreenUtil
+import com.example.common.utils.function.color
 import com.example.common.utils.function.pt
-import com.example.framework.utils.function.value.orZero
 import com.example.framework.utils.function.value.safeSize
-import com.example.framework.utils.function.view.color
+import com.example.framework.utils.function.view.doOnceAfterLayout
 import com.example.framework.utils.function.view.layoutGravity
 import com.example.framework.utils.function.view.margin
 import com.example.framework.utils.function.view.size
+import java.lang.ref.WeakReference
 
 /**
  * Created by wangyanbin
@@ -23,70 +26,95 @@ import com.example.framework.utils.function.view.size
  * private val occupationPopup by lazy { SelectBottomPopup<String>(this) { it }.apply { setParams(UserAuthBean.jobList) }}
  */
 class SelectMenuPopup<T>(activity: FragmentActivity, var formatter: (T?) -> String?) : BasePopupWindow<ViewPopupSelectMenuBinding>(activity, light = false) {
-    private var onCurrent: ((item: String, index: Int) -> Unit)? = null
+    private var lastMenuWidth = 0
+    private var onCurrent: ((item: String?, index: Int) -> Unit)? = null
 
-    fun setParams(list: List<T>, menuWidth: Int? = 0, horizontalMargin: Int? = 15.pt, location: Location? = Location.END) {
-        setParams(list, MenuBean().apply {
+    /**
+     * 假设弹出的上方的view是绘制好的
+     *  mBinding?.llFilter?.doOnceAfterLayout {
+     *    coin.setParams(listOf(string(R.string.unitINR), string(R.string.unitIDR)), mBinding?.llFilter?.measuredWidth.orZero)
+     *  }
+     */
+    fun setParams(list: List<T>, menuWidth: Int = 0, horizontalMargin: Int = 15.pt, verticalMargin: Int = 0, gravity: Int = Gravity.END) {
+        lastMenuWidth = menuWidth
+        setConfiguration(list, MenuBean().apply {
             this.menuWidth = menuWidth
             this.horizontalMargin = horizontalMargin
-            this.location = location
+            this.verticalMargin = verticalMargin
+            this.gravity = gravity
         })
     }
 
-    fun setParams(list: List<T>, bean: MenuBean? = MenuBean()) {
+    fun setParams(list: List<T>, view: WeakReference<View>, gravity: Int = Gravity.END) {
+        if (0 == lastMenuWidth) {
+            view.get()?.doOnceAfterLayout {
+                setParams(list, menuWidth = it.measuredWidth, gravity = gravity)
+            }
+        } else {
+            setParams(list, menuWidth = lastMenuWidth, gravity = gravity)
+        }
+    }
+
+    private fun setConfiguration(list: List<T>, bean: MenuBean = MenuBean()) {
         mBinding?.apply {
-            viewArrow.layoutGravity = bean?.getLayoutGravity().orZero
-            val horizontalMargin = bean?.horizontalMargin.orZero
-            val verticalMargin = bean?.verticalMargin.orZero
-            viewArrow.margin(
-                top = verticalMargin,
-                start = if (bean?.location == Location.START) 10.pt + horizontalMargin else 0,
-                end = if (bean?.location == Location.END) 10.pt + horizontalMargin else 0)
-            llItem.apply {
-                if (0 != bean?.menuWidth) size(width = bean?.menuWidth)
-                layoutGravity = bean?.getLayoutGravity().orZero
-                margin(
-                    top = 10.pt + verticalMargin,
-                    start = if (bean?.location == Location.START) horizontalMargin else 0,
-                    end = if (bean?.location == Location.END) horizontalMargin else 0)
-                removeAllViews()
-                list.forEachIndexed { index, t ->
-                    addView(SelectItemHolder(llItem, Pair(formatter(t).orEmpty(), index)).let {
-                        it.onItemClick = { item, index ->
-                            hidden()
-                            onCurrent?.invoke(item.orEmpty(), index)
-                        }
-                        it.mBinding.root
-                    })
-                    if (list.safeSize - 1 > index) {
-                        val view = View(context)
-                        view.size(MATCH_PARENT, 1.pt)
-                        view.setBackgroundColor(color(R.color.bgLine))
-                        view.margin(start = 1.pt, end = 1.pt)
-                        addView(view)
+            // 确定箭头大小/位置
+            setupArrow(viewArrow, bean)
+            // 确定容器大小/位置
+            setupContainer(llItem, list, bean)
+        }
+    }
+
+    private fun setupArrow(arrowView: View, bean: MenuBean) {
+        arrowView.layoutGravity = bean.gravity
+        arrowView.margin(
+            start = if (bean.gravity == Gravity.START) 10.pt + bean.horizontalMargin else 0,
+            top = bean.verticalMargin,
+            end = if (bean.gravity == Gravity.END) 10.pt + bean.horizontalMargin else 0
+        )
+    }
+
+    private fun setupContainer(container: ViewGroup, list: List<T>, bean: MenuBean) {
+        container.apply {
+            if (bean.menuWidth != 0) size(bean.menuWidth, WRAP_CONTENT)
+            layoutGravity = bean.gravity
+            margin(
+                start = if (bean.gravity == Gravity.START) bean.horizontalMargin else 0,
+                top = 10.pt + bean.verticalMargin,
+                end = if (bean.gravity == Gravity.END) bean.horizontalMargin else 0
+            )
+            removeAllViews()
+            list.forEachIndexed { index, t ->
+                addView(SelectItemHolder(this, formatter(t), index).also {
+                    it.onItemClick = { item, clickIndex ->
+                        hidden()
+                        onCurrent?.invoke(item, clickIndex)
                     }
+                }.mBinding.root)
+                // 绘制下划线
+                if (list.safeSize - 1 > index) {
+                    addDivider(this)
                 }
             }
         }
     }
 
-    fun setOnItemClickListener(onCurrent: ((item: String, index: Int) -> Unit)) {
+    private fun addDivider(container: ViewGroup) {
+        val view = View(container.context)
+        view.setBackgroundColor(color(R.color.bgLine))
+        view.size(MATCH_PARENT, 1.pt)
+        view.margin(start = 1.pt, end = 1.pt)
+        container.addView(view)
+    }
+
+    fun setOnItemClickListener(onCurrent: ((item: String?, index: Int) -> Unit)) {
         this.onCurrent = onCurrent
     }
 
 }
 
 class MenuBean {
-    var menuWidth: Int? = ScreenUtil.screenWidth//父item的宽度，不传则为手机宽度
-    var verticalMargin: Int? = 0//距上下默认位置（默认就在view下放，贴合）
-    var horizontalMargin: Int? = 15.pt//距左右侧默认位置
-    var location: Location? = Location.END//菜单位置（左右侧）
-
-    fun getLayoutGravity(): Int {
-        return if (location == Location.END) Gravity.END else Gravity.START
-    }
-}
-
-enum class Location {
-    START, END
+    var menuWidth: Int = ScreenUtil.screenWidth//父item的宽度，不传则为手机宽度
+    var horizontalMargin: Int = 15.pt//距左右侧默认位置
+    var verticalMargin: Int = 0//距上下默认位置（默认就在view下放，贴合）
+    var gravity: Int = Gravity.END//菜单位置（左右侧）
 }
