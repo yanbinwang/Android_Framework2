bd725c25483c4200f40446cc481ad807e60cc9c0
wangyanbin <wangyanbin_29@126.com>
2025-04-30 13:46:13 +0800

    kts替换/升级

--- lib_thirdparty/src/main/java/com/example/thirdparty/pay/network/PayApi.kt
+++ lib_thirdparty/src/main/java/com/example/thirdparty/pay/network/PayApi.kt
@@ -1,5 +1,6 @@
-package com.example.thirdparty.pay.subscribe
+package com.example.thirdparty.pay.network
 
+import com.example.common.network.factory.RetrofitFactory
 import com.example.common.network.repository.ApiResponse
 import com.example.common.network.repository.EmptyBean
 import com.example.thirdparty.pay.bean.BalanceBean
@@ -16,15 +17,32 @@ import retrofit2.http.Path
  * 充值接口类
  */
 interface PayApi {
+
+    companion object {
+        val instance by lazy { RetrofitFactory.instance.createByServer(PayApi::class.java) }
+    }
+
+    /**
+     * 获取充值接口
+     */
     @POST("swallow/recharge/pay")
     suspend fun getRechargeApi(@Body requestBody: RequestBody): ApiResponse<PayBean>
 
+    /**
+     * 获取继续支付接口
+     */
     @POST("swallow/recharge/pay/{orderNo}")
     suspend fun getOrderPayApi(@Path("orderNo") orderNo: String, @Body requestBody: RequestBody): ApiResponse<PayBean>
 
+    /**
+     * 获取保全币充值接口
+     */
     @PUT("swallow/attestation/site/pay/{attestationId}")
     suspend fun getSitePayApi(@Path("attestationId") attestationId: String): ApiResponse<EmptyBean>
 
+    /**
+     * 获取用户余额（包含套餐，余额）
+     */
     @GET("swallow-user/account/price/info")
     suspend fun getBalanceApi(): ApiResponse<BalanceBean>
 }
