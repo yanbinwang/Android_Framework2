e1679b4a2a0abdd4029249c153d9c55b6667f20b
wangyanbin <wangyanbin_29@126.com>
2025-04-30 12:04:34 +0800

    kts替换/升级

--- lib_common/src/main/java/com/example/common/subscribe/CommonApi.kt
+++ lib_common/src/main/java/com/example/common/subscribe/CommonApi.kt
@@ -1,6 +1,5 @@
 package com.example.common.subscribe
 
-import com.example.common.network.factory.RetrofitFactory
 import com.example.common.network.repository.ApiResponse
 import com.example.common.network.repository.EmptyBean
 import retrofit2.http.*
@@ -9,29 +8,7 @@ import retrofit2.http.*
  * author:wyb
  * 通用接口类
  */
-//interface CommonApi {
-//
-//    @Streaming
-//    @GET
-//    suspend fun getDownloadApi(@Url downloadUrl: String): okhttp3.ResponseBody
-//
-//    @FormUrlEncoded
-//    @POST("http://www.baidu.com")
-//    suspend fun getSendVerificationApi(@Header("Retry-Agent") retryAgent: String, @FieldMap map: Map<String, String>): ApiResponse<EmptyBean>
-//
-//    @FormUrlEncoded
-//    @POST("http://www.baidu.com")
-//    suspend fun getVerificationApi(@FieldMap map: Map<String, String>): ApiResponse<EmptyBean>
-//
-//    @GET("test")
-//    suspend fun getTestApi(@FieldMap map: Map<String, String>): ApiResponse<EmptyBean>
-//
-//}
 interface CommonApi {
-
-    companion object {
-        val instance by lazy { RetrofitFactory.instance.createByServer(CommonApi::class.java) }
-    }
 
     @Streaming
     @GET
