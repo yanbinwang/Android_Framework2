fa7fe2b41015c0584e99716b7985917a925fb614
wangyanbin <wangyanbin_29@126.com>
2025-05-02 23:25:03 +0800

    kts替换/升级

--- lib_framework/src/main/java/com/example/framework/utils/function/value/FunctionsString.kt
+++ lib_framework/src/main/java/com/example/framework/utils/function/value/FunctionsString.kt
@@ -115,11 +115,12 @@ fun String.limitLength(maxLength: Int = 4000): String {
 /**
  * 检测正则
  */
-fun String?.regCheck(reg: String): Boolean {
+fun String?.matches(regex: String): Boolean {
     this ?: return false
-    val pattern = Pattern.compile(reg)
-    val matcher = pattern.matcher(this)
-    return matcher.matches()
+    return this.matches(Regex(regex))
+//    val pattern = Pattern.compile(regex)
+//    val matcher = pattern.matcher(this)
+//    return matcher.matches()
 }
 
 /**
@@ -195,7 +196,7 @@ fun String?.addUrlParam(key: String?, value: String?): String? {
 fun String?.hidePhoneNumber(): String {
     this ?: return ""
     var value = ""
-    if (regCheck(MOBILE)) {
+    if (matches(MOBILE)) {
         val ch = toCharArray()
         for (index in ch.indices) {
             if (index in 3..6) {
