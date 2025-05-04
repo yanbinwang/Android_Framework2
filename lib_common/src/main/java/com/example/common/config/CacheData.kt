b35513521f9c6f032954e88ca8ef5faff1d097ed
wangyanbin <wangyanbin_29@126.com>
2025-05-04 09:46:10 +0800

    kts替换/升级

--- lib_common/src/main/java/com/example/common/config/CacheData.kt
+++ lib_common/src/main/java/com/example/common/config/CacheData.kt
@@ -3,9 +3,9 @@ package com.example.common.config
 import com.example.common.bean.LanguageBean
 import com.example.common.bean.UserBean
 import com.example.common.bean.UserInfoBean
-import com.example.common.utils.DataBooleanCacheUtil
-import com.example.common.utils.DataCacheUtil
-import com.example.common.utils.DataStringCacheUtil
+import com.example.common.utils.DataBooleanCache
+import com.example.common.utils.DataParcelableCache
+import com.example.common.utils.DataStringCache
 
 /**
  * @description 全局mmkv存储
@@ -14,30 +14,30 @@ import com.example.common.utils.DataStringCacheUtil
 object CacheData {
     //设备id
     private const val DEVICE_ID = "device_id"
-    internal val deviceId = DataStringCacheUtil(DEVICE_ID)
+    internal val deviceId = DataStringCache(DEVICE_ID)
 
     //推送token
     private const val DEVICE_TOKEN = "device_token"
-    internal val deviceToken = DataStringCacheUtil(DEVICE_TOKEN)
+    internal val deviceToken = DataStringCache(DEVICE_TOKEN)
 
     //当前选择语言
     private const val LANGUAGE = "language"
-    internal val language = DataStringCacheUtil(LANGUAGE)
+    internal val language = DataStringCache(LANGUAGE)
 
     //语言类
     private const val LANGUAGE_BEAN = "language_bean"
-    internal val languageBean = DataCacheUtil(LANGUAGE_BEAN, LanguageBean::class.java)
+    internal val languageBean = DataParcelableCache(LANGUAGE_BEAN, LanguageBean::class.java)
 
     //是否同意告知书
     private const val PRIVACY_AGREED = "privacy_agreed"
-    internal val privacyAgreed = DataBooleanCacheUtil(PRIVACY_AGREED)
+    internal val privacyAgreed = DataBooleanCache(PRIVACY_AGREED)
 
     //用户类
     private const val USER_BEAN = "user_bean"
-    internal val userBean = DataCacheUtil(USER_BEAN, UserBean::class.java)
+    internal val userBean = DataParcelableCache(USER_BEAN, UserBean::class.java)
 
     //用户信息类
     private const val USER_INFO_BEAN = "user_info_bean"
-    internal val userInfoBean = DataCacheUtil(USER_INFO_BEAN, UserInfoBean::class.java)
+    internal val userInfoBean = DataParcelableCache(USER_INFO_BEAN, UserInfoBean::class.java)
 
 }
