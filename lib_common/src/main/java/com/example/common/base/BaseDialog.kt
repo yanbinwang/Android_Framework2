27214f947be840446ee1a6bdaea1447e282e39e2
wangyanbin <wangyanbin_29@126.com>
2025-05-06 10:28:28 +0800

    kts替换/升级

--- lib_common/src/main/java/com/example/common/base/BaseDialog.kt
+++ lib_common/src/main/java/com/example/common/base/BaseDialog.kt
@@ -22,15 +22,21 @@ import java.lang.reflect.ParameterizedType
 
 /**
  * Created by WangYanBin on 2020/7/13.
- * 所有弹框的基类
- * 外层无需绘制额外布局，但需要指定宽高，默认情况下是居中的，可设置对应角度
- * 需要注意binding使用了lateinit，但是不可能会不给值（VDB），稳妥期间，引用到view的地方，使用dialogView
- * window?.setWindowAnimations(R.style.pushRightAnimStyle)
- * window?.setGravity(Gravity.TOP xor Gravity.END)
+ * 所有弹框的基类(外层无需绘制额外布局，但需要指定宽高，默认情况下是居中的，可设置对应角度)
+ *
+ * 1.传入 Activity 作为 Context
+ * 如果传入的 Context 实际上是一个 Activity 实例（因为 Activity 继承自 Context）
+ * 那么 Dialog 会正确关联到这个 Activity，在这种情况下，ownerActivity 会被正确设置为传入的 Activity
+ * 2.传入 Application 上下文作为 Context
+ * 如果传入的是 Application 上下文，那么会对 ownerActivity 产生不良影响:
+ * (1) ownerActivity缺失：
+ *   Application 上下文并非 Activity 实例，所以 Dialog 无法正确关联到一个 Activity，ownerActivity 不会被正确设置。这可能导致 Dialog 在显示和管理方面出现问题
+ * (2) 生命周期管理问题：
+ *   Application 上下文的生命周期贯穿整个应用程序的生命周期，而不是某个具体 Activity 的生命周期。如果使用 Application 上下文创建 Dialog，Dialog 不会随着 Activity 的销毁而销毁，可能会导致内存泄漏和显示异常
  */
 @Suppress("LeakingThis", "UNCHECKED_CAST")
 abstract class BaseDialog<VDB : ViewDataBinding>(context: Context, dialogWidth: Int = 320, dialogHeight: Int = WRAP_CONTENT, gravity: Int = CENTER, themeResId: Int = R.style.DialogStyle, animation: Boolean = true, close: Boolean = true) : Dialog(context, themeResId) {
-    private var dialogView: View? = null
+    private val rootView get() = mBinding?.root
     protected var mBinding: VDB? = null
 
     init {
@@ -44,9 +50,9 @@ abstract class BaseDialog<VDB : ViewDataBinding>(context: Context, dialogWidth: 
         }
         if (animation) {
             //当布局show出来的时候执行开始动画
-            setOnShowListener { dialogView?.startAnimation(context.elasticityEnter()) }
+            setOnShowListener { rootView?.startAnimation(context.elasticityEnter()) }
             //当布局销毁时执行结束动画
-            setOnDismissListener { dialogView?.startAnimation(context.elasticityExit()) }
+            setOnDismissListener { rootView?.startAnimation(context.elasticityExit()) }
         }
         if (close) {
             setOnKeyListener { _: DialogInterface?, _: Int, _: KeyEvent? -> true }
@@ -63,7 +69,6 @@ abstract class BaseDialog<VDB : ViewDataBinding>(context: Context, dialogWidth: 
                 val method = vdbClass?.getMethod("inflate", LayoutInflater::class.java)
                 mBinding = method?.invoke(null, layoutInflater) as? VDB
                 mBinding?.root?.let { setContentView(it) }
-                dialogView = mBinding?.root
             } catch (e: Exception) {
                 e.printStackTrace()
             }
