a771a452ff197c99c010c97b6da15d119b3fbc23
老王 <wangyanbin_29@126.com>
2025-05-15 12:00:26 +0800

    调整

--- lib_common/src/main/java/com/example/common/base/BaseTitleActivity.kt
+++ lib_common/src/main/java/com/example/common/base/BaseTitleActivity.kt
@@ -1,17 +1,9 @@
 package com.example.common.base
 
-import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup.LayoutParams.MATCH_PARENT
-import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
-import android.widget.FrameLayout
-import android.widget.LinearLayout
 import androidx.databinding.ViewDataBinding
 import com.example.common.databinding.ActivityBaseBinding
 import com.example.common.utils.builder.TitleBuilder
-import com.example.common.widget.AppToolbar
-import com.example.framework.utils.function.view.size
-import com.example.framework.utils.function.view.weight
 
 /**
  * Created by WangYanBin on 2020/6/10.
@@ -28,11 +20,11 @@ abstract class BaseTitleActivity<VDB : ViewDataBinding> : BaseActivity<VDB>() {
 //            orientation = LinearLayout.VERTICAL
 //        }
 //    }
-//    private val rootView by lazy { FrameLayout(this).apply {
-//        size(MATCH_PARENT, WRAP_CONTENT)
-//    }}
 //    private val titleBar by lazy { AppToolbar(this).apply {
 //        size(MATCH_PARENT, WRAP_CONTENT)
+//    }}
+//    private val rootView by lazy { FrameLayout(this).apply {
+//        size(MATCH_PARENT, MATCH_PARENT)
 //    }}
 //    protected val titleBuilder get() = titleBar //标题栏
 //    protected val viewGroup get() = rootView//标题页面的父容器，用于添加empty，如果不需要标题头的baseactivity，则在外层绘制一个FrameLayout
@@ -57,15 +49,14 @@ abstract class BaseTitleActivity<VDB : ViewDataBinding> : BaseActivity<VDB>() {
     }
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
+//        titleBar.bind(this)
 //        root.addView(titleBar)
 //        root.addView(rootView)
-//        titleBar.bind(this)
-//        rootView.weight = 1f
 //    }
 //
 //    override fun setContentView(view: View?) {
 //        rootView.addView(mBinding?.root)
-//        super.setContentView(rootView)
+//        super.setContentView(root)
 //    }
 //
 //    protected fun setBackgroundResource(resid: Int) {
