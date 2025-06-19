09aa52bc39caad2933232c3c666e52ce4dca9db8
GoudanYan <wangyanbin_29@126.com>
2025-06-19 09:10:10 +0800

    kts替换/升级

--- module_account/src/main/java/com/example/account/activity/LoginActivity.kt
+++ module_account/src/main/java/com/example/account/activity/LoginActivity.kt
@@ -2,28 +2,27 @@ package com.example.account.activity
 
 import android.os.Bundle
 import android.util.LayoutDirection
-import android.view.animation.AnimationUtils
 import androidx.core.text.TextUtilsCompat
 import com.example.account.R
 import com.example.account.databinding.ActivityLoginBinding
 import com.example.common.base.BaseActivity
+import com.example.framework.utils.function.doOnDestroy
+import com.example.framework.utils.function.view.startAnimation
 import java.util.Locale
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>() {
 
     override fun initView(savedInstanceState: Bundle?) {
         super.initView(savedInstanceState)
-    }
-
-    /**
-     * 背景微动画
-     */
-    private fun startBgAnimation() {
-        val animId = if (TextUtilsCompat.getLayoutDirectionFromLocale(Locale.getDefault()) == LayoutDirection.RTL) {
+        doOnDestroy {
+            mBinding?.ivLoginBg?.clearAnimation()
+        }
+        //背景微动画
+        mBinding?.ivLoginBg?.startAnimation(if (TextUtilsCompat.getLayoutDirectionFromLocale(Locale.getDefault()) == LayoutDirection.RTL) {
             R.anim.set_login_translate_rtl
         } else {
             R.anim.set_login_translate
-        }
-        mBinding?.ivLoginBg?.startAnimation(AnimationUtils.loadAnimation(this, animId))
+        })
     }
+
 }
