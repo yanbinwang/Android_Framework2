bcf81a99e72ea3de0215405236db6e13f97b1009
GoudanYan <wangyanbin_29@126.com>
2025-07-02 14:37:13 +0800

    kts替换/升级

--- lib_local/glide/src/main/java/com/example/glide/GlideBindingAdapter.kt
+++ lib_local/glide/src/main/java/com/example/glide/GlideBindingAdapter.kt
@@ -3,6 +3,7 @@ package com.example.glide
 import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.example.framework.utils.function.value.areDrawablesSame
 import com.example.framework.utils.function.value.toSafeInt
 import com.example.framework.utils.function.view.disable
 import com.example.framework.utils.function.view.enable
@@ -10,6 +11,7 @@ import com.example.glide.ImageLoader.Companion.DEFAULT_CIRCULAR_RESOURCE
 import com.example.glide.ImageLoader.Companion.DEFAULT_CORNER_RADIUS
 import com.example.glide.ImageLoader.Companion.DEFAULT_RESOURCE
 import com.example.glide.ImageLoader.Companion.DEFAULT_ROUNDED_RESOURCE
+import java.lang.ref.WeakReference
 
 /**
  * Created by WangYanBin on 2020/6/10.
@@ -20,26 +22,46 @@ object GlideBindingAdapter {
     // <editor-fold defaultstate="collapsed" desc="imageview绑定方法">
     /**
      * 图片样式
+     * 问题一：相同资源可能生成不同的 Drawable 实例
+     * 当你通过 Context.getDrawable() 或 Resources.getDrawable() 获取同一个资源 ID 的 Drawable 时，每次获取的可能是不同的实例
+     * val drawable1 = context.getDrawable(R.drawable.my_drawable)
+     * val drawable2 = context.getDrawable(R.drawable.my_drawable)
+     * // 很可能返回 false
+     * println(drawable1 == drawable2)
+     *
+     * 问题二：状态可变性导致的比较问题
+     * 即使两个 Drawable 来源于同一个资源，它们的状态也可能不同
+     * val drawable1 = context.getDrawable(R.drawable.my_drawable).mutate()
+     * val drawable2 = context.getDrawable(R.drawable.my_drawable).mutate()
+     * drawable1.setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN)
+     * // 即使资源相同，状态不同也会导致比较失败
+     * println(drawable1 == drawable2) // false
+     *
+     * 方法一：比较资源 ID（如果适用）
+     * 如果你存储的是资源 ID 而非 Drawable 实例，可以直接比较资源 ID
+     *
+     * 方法二：比较 Drawable 的恒定属性
+     * 如果你确实需要比较 Drawable 实例，可以比较其内在属性
      */
     @JvmStatic
     @BindingAdapter(value = ["res", "drawable", "visibility"], requireAll = false)
     fun bindingImageViewTheme(view: ImageView, res: Int?, drawable: Drawable?, visibility: Int?) {
-        if (res != null) {
+        res?.let { newRes ->
             val resKey = R.id.glide_res_tag
             val oldSrcRes = view.getTag(resKey) as? Int
-            if (oldSrcRes != res) {
-                view.setImageResource(res)
-                view.setTag(resKey, res)
-            }
-        } else if (drawable != null) {
-            val drawableKey = R.id.glide_drawable_tag
-            val oldDrawable = view.getTag(drawableKey) as? Drawable
-            if (oldDrawable != drawable) {
-                view.setImageDrawable(drawable)
-                view.setTag(drawableKey, drawable)
+            if (oldSrcRes != newRes) {
+                view.setImageResource(newRes)
+                view.setTag(resKey, newRes)
             }
         }
-        //处理可见性设置
+        drawable?.let { newDrawable ->
+            val drawableKey = R.id.glide_drawable_tag
+            val oldDrawable = (view.getTag(drawableKey) as? WeakReference<Drawable>)?.get()
+            if (oldDrawable == null || !areDrawablesSame(oldDrawable, newDrawable)) {
+                view.setImageDrawable(newDrawable)
+                view.setTag(drawableKey, WeakReference(newDrawable))
+            }
+        }
         visibility?.let { newVisibility ->
             val visibilityKey = R.id.glide_visibility_tag
             val oldVisibility = view.getTag(visibilityKey) as? Int
