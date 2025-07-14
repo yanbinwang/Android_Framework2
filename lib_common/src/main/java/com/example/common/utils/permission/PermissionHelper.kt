76e43032a1f041d82f799199294c7b5d3856620f
GoudanYan <wangyanbin_29@126.com>
2025-07-14 20:32:51 +0800

    kts替换/升级

--- lib_common/src/main/java/com/example/common/utils/permission/PermissionHelper.kt
+++ lib_common/src/main/java/com/example/common/utils/permission/PermissionHelper.kt
@@ -4,11 +4,12 @@ import android.content.Context
 import android.os.Build
 import com.example.common.R
 import com.example.common.utils.i18n.string
-import com.example.common.utils.permission.XXPermissionsGroup.CAMERA
-import com.example.common.utils.permission.XXPermissionsGroup.STORAGE
+import com.example.common.utils.permission.XXPermissionsGroup.CAMERA_GROUP
+import com.example.common.utils.permission.XXPermissionsGroup.STORAGE_GROUP
 import com.example.common.widget.dialog.AndDialog
 import com.hjq.permissions.OnPermissionCallback
 import com.hjq.permissions.XXPermissions
+import com.hjq.permissions.permission.base.IPermission
 
 /**
  * author: wyb
@@ -19,8 +20,8 @@ import com.hjq.permissions.XXPermissions
 class PermissionHelper(private val context: Context) {
     private val andDialog by lazy { AndDialog(context) }
     private val permsGroup = arrayOf(
-        CAMERA,//拍摄照片，录制视频
-        STORAGE)//访问照片。媒体。内容和文件
+        CAMERA_GROUP,//拍摄照片，录制视频
+        STORAGE_GROUP)//访问照片。媒体。内容和文件
 
     /**
      * 检测权限(默认拿全部，可单独拿某个权限组)
@@ -29,18 +30,18 @@ class PermissionHelper(private val context: Context) {
         requestPermissions(*permsGroup, listener = listener)
     }
 
-    fun requestPermissions(vararg groups: Array<String>, listener: (hasPermissions: Boolean) -> Unit = {}, force: Boolean = true) {
+    fun requestPermissions(vararg groups: List<IPermission>, listener: (hasPermissions: Boolean) -> Unit = {}, force: Boolean = true) {
         //6.0+系统做特殊处理
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             XXPermissions.with(context)
-                .permission(*groups)
+                .permissions(groups.toMutableList().flatten())
                 .request(object : OnPermissionCallback {
-                    override fun onGranted(permissions: MutableList<String>, allGranted: Boolean) {
+                    override fun onGranted(permissions: MutableList<IPermission>, allGranted: Boolean) {
                         //allGranted->标记是否是获取部分权限成功，部分未正常授予，true全拿，false部分拿到
                         listener.invoke(allGranted)
                     }
 
-                    override fun onDenied(permissions: MutableList<String>, doNotAskAgain: Boolean) {
+                    override fun onDenied(permissions: MutableList<IPermission>, doNotAskAgain: Boolean) {
                         super.onDenied(permissions, doNotAskAgain)
                         //doNotAskAgain->被永久拒绝授权，请手动授予
                         listener.invoke(false)
@@ -55,12 +56,12 @@ class PermissionHelper(private val context: Context) {
     /**
      * 彈出授權彈框
      */
-    private fun onDenied(permissions: MutableList<String>?) {
+    private fun onDenied(permissions: MutableList<IPermission>?) {
         if (permissions.isNullOrEmpty()) return
         //拼接用戶拒絕後的提示参数
         var reason = ""
         for (index in permsGroup.indices) {
-            if (listOf(*permsGroup[index]).contains(permissions[0])) {
+            if (permsGroup[index].contains(permissions[0])) {
                 reason += "${onReason(index)}\n"
             }
         }
