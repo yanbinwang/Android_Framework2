9d2d5404438aa3f349695efd00521c887c62e72e
GoudanYan <wangyanbin_29@126.com>
2025-08-04 09:42:46 +0800

    剔除旧相册权限相关逻辑

--- lib_common/src/main/java/com/example/common/widget/dialog/AppDialog.kt
+++ lib_common/src/main/java/com/example/common/widget/dialog/AppDialog.kt
@@ -1,15 +1,16 @@
 package com.example.common.widget.dialog
 
+import android.text.Spannable
 import android.view.Gravity
 import androidx.fragment.app.FragmentActivity
 import com.example.common.R
 import com.example.common.base.BaseDialog
 import com.example.common.databinding.ViewDialogBinding
 import com.example.common.utils.function.string
-import com.example.framework.utils.function.dimen
 import com.example.framework.utils.function.view.click
 import com.example.framework.utils.function.view.gone
 import com.example.framework.utils.function.view.textColor
+import com.example.framework.utils.function.view.textSize
 import com.example.framework.utils.function.view.visible
 
 /**
@@ -18,47 +19,50 @@ import com.example.framework.utils.function.view.visible
  * 类似苹果的弹出窗口类
  */
 class AppDialog(activity: FragmentActivity) : BaseDialog<ViewDialogBinding>(activity) {
-    private var onConfirm: (() -> Unit)? = null
-    private var onCancel: (() -> Unit)? = null
+    private var onPositive: (() -> Unit)? = null
+    private var onNegative: (() -> Unit)? = null
 
     /**
-     * 确定
+     * 确定样式
      */
-    fun setPositive(title: String? = string(R.string.hint), message: String? = null, positiveText: String? = string(R.string.sure)): AppDialog {
-        return setParams(title, message, positiveText, "")
+    fun setPositive(title: String? = string(R.string.hint), message: String? = null, positiveText: String? = string(R.string.sure), gravity: Int = Gravity.CENTER, hasTitle: Boolean = true): AppDialog {
+        return setParams(title, message, positiveText, "", gravity, hasTitle)
     }
 
     /**
-     * 确定/取消
+     * 确定/取消 样式
      */
-    fun setParams(title: String? = string(R.string.hint), message: String? = null, positiveText: String? = string(R.string.sure), negativeText: String? = string(R.string.cancel), gravity: Int = Gravity.CENTER): AppDialog {
+    fun setParams(title: String? = string(R.string.hint), message: String? = null, positiveText: String? = string(R.string.sure), negativeText: String? = string(R.string.cancel), gravity: Int = Gravity.CENTER, hasTitle: Boolean = true): AppDialog {
         mBinding?.apply {
-            if (title.isNullOrEmpty()) {
-                tvTip.gone()
+            // 标题
+            if (!hasTitle) {
+                tvTitle.gone()
             } else {
-                tvTip.visible()
-                tvTip.text = title
+                tvTitle.visible()
+                tvTitle.text = title
             }
+            // 内容
             tvMessage.gravity = gravity
             tvMessage.text = message
-            tvSure.apply {
+            // 操作键
+            tvPositive.apply {
                 text = positiveText
                 click {
                     dismiss()
-                    onConfirm?.invoke()
+                    onPositive?.invoke()
                 }
             }
             if (negativeText.isNullOrEmpty()) {
                 viewLine.gone()
-                tvCancel.gone()
+                tvNegative.gone()
             } else {
                 viewLine.visible()
-                tvCancel.apply {
+                tvNegative.apply {
                     visible()
                     text = negativeText
                     click {
                         dismiss()
-                        onCancel?.invoke()
+                        onNegative?.invoke()
                     }
                 }
             }
@@ -66,33 +70,53 @@ class AppDialog(activity: FragmentActivity) : BaseDialog<ViewDialogBinding>(acti
         return this
     }
 
-    fun setDialogListener(onConfirm: () -> Unit = {}, onCancel: () -> Unit = {}): AppDialog {
-        this.onConfirm = onConfirm
-        this.onCancel = onCancel
+    /**
+     * 标题样式
+     */
+    fun setTitle(colorRes: Int = R.color.textPrimary, sizeRes: Int = R.dimen.textSize14): AppDialog {
+        mBinding?.tvTitle.textColor(colorRes)
+        mBinding?.tvTitle?.textSize(sizeRes)
         return this
     }
 
-    fun setTipTheme(colorRes: Int = R.color.textPrimary, sizeRes: Int = R.dimen.textSize14): AppDialog {
-        mBinding?.tvTip.textColor(colorRes)
-        mBinding?.tvTip?.textSize = context.dimen(sizeRes)
-        return this
-    }
-
-    fun setMessageTheme(colorRes: Int = R.color.textPrimary, sizeRes: Int = R.dimen.textSize14): AppDialog {
+    /**
+     * 内容样式
+     */
+    fun setMessage(colorRes: Int = R.color.textPrimary, sizeRes: Int = R.dimen.textSize14): AppDialog {
         mBinding?.tvMessage.textColor(colorRes)
-        mBinding?.tvMessage?.textSize = context.dimen(sizeRes)
+        mBinding?.tvMessage?.textSize(sizeRes)
         return this
     }
 
-    fun setSureTheme(colorRes: Int = R.color.appTheme, sizeRes: Int = R.dimen.textSize14): AppDialog {
-        mBinding?.tvSure.textColor(colorRes)
-        mBinding?.tvSure?.textSize = context.dimen(sizeRes)
+    fun setMessage(spannable: Spannable, ): AppDialog {
+        mBinding?.tvMessage?.text = spannable
         return this
     }
 
-    fun setCancelTheme(colorRes: Int = R.color.appTheme, sizeRes: Int = R.dimen.textSize14): AppDialog {
-        mBinding?.tvCancel.textColor(colorRes)
-        mBinding?.tvCancel?.textSize = context.dimen(sizeRes)
+    /**
+     * 确认样式
+     */
+    fun setPositiveTheme(colorRes: Int = R.color.appTheme, sizeRes: Int = R.dimen.textSize14): AppDialog {
+        mBinding?.tvPositive.textColor(colorRes)
+        mBinding?.tvPositive?.textSize(sizeRes)
+        return this
+    }
+
+    /**
+     * 取消样式
+     */
+    fun setNegativeTheme(colorRes: Int = R.color.appTheme, sizeRes: Int = R.dimen.textSize14): AppDialog {
+        mBinding?.tvNegative.textColor(colorRes)
+        mBinding?.tvNegative?.textSize(sizeRes)
+        return this
+    }
+
+    /**
+     * 回调监听
+     */
+    fun setDialogListener(onPositive: () -> Unit = {}, onNegative: () -> Unit = {}): AppDialog {
+        this.onPositive = onPositive
+        this.onNegative = onNegative
         return this
     }
 
