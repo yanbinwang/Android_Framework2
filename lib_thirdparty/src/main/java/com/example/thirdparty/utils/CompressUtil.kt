51f1698d87354e4aa0d872924d4911630fbe3a48
GoudanYan <wangyanbin_29@126.com>
2025-09-12 10:41:02 +0800

    资源规整

--- lib_thirdparty/src/main/java/com/example/thirdparty/utils/CompressUtil.kt
+++ lib_thirdparty/src/main/java/com/example/thirdparty/utils/CompressUtil.kt
@@ -1,5 +1,6 @@
 package com.example.thirdparty.utils
 
+import android.content.Context
 import android.graphics.Bitmap
 import com.example.common.BaseApplication
 import com.example.common.utils.builder.shortToast
@@ -19,17 +20,17 @@ import java.io.File
  * @author yan
  */
 object CompressUtil {
-    private var resWidth = 1000
-    private var resHeight = 1000
+    private const val resWidth = 1000
+    private const val resHeight = 1000
 
     @JvmStatic
-    suspend fun compressFile(sourcePath: String?): File? {
+    suspend fun compressFile(context: Context = BaseApplication.instance.applicationContext, sourcePath: String?): File? {
         sourcePath ?: throw RuntimeException("文件为空")
-        return compressFile(File(sourcePath))
+        return compressFile(context, File(sourcePath))
     }
 
     @JvmStatic
-    suspend fun compressFile(file: File?): File? {
+    suspend fun compressFile(context: Context = BaseApplication.instance.applicationContext, file: File?): File? {
         file ?: throw RuntimeException("文件为空")
         return withContext(IO) {
             try {
@@ -37,9 +38,12 @@ object CompressUtil {
                     string(R.string.compressError).shortToast()
                     null
                 } else {
-                    Compressor.compress(BaseApplication.instance, file) {
+                    Compressor.compress(context, file) {
+                        // 限制压缩后图片的最大分辨率 宽度不会超过 resWidth，高度不会超过 resHeight
                         resolution(resWidth, resHeight)
+                        // 控制图片压缩时的质量保留程度
                         quality(80)
+                        // 指定压缩后图片的输出格式
                         format(Bitmap.CompressFormat.JPEG)
                     }
                 }
