6238c8a03373c2b35691610397318a81e9d2d8f3
GoudanYan <wangyanbin_29@126.com>
2025-10-24 14:51:38 +0800

    完善相册

--- lib_thirdparty/src/main/java/com/example/thirdparty/media/utils/GSYVideoListHelper.kt
+++ lib_thirdparty/src/main/java/com/example/thirdparty/media/utils/GSYVideoListHelper.kt
@@ -11,7 +11,6 @@ import com.example.framework.utils.function.view.click
 import com.example.framework.utils.function.view.gone
 import com.shuyu.gsyvideoplayer.GSYVideoManager
 import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer
-import java.util.concurrent.ConcurrentHashMap
 import kotlin.math.max
 
 /**
@@ -25,49 +24,49 @@ import kotlin.math.max
  *     android:configChanges="keyboard|keyboardHidden|orientation|screenSize|screenLayout|smallestScreenSize|uiMode"
  *     android:screenOrientation="portrait" />
  */
-class GSYVideoListHelper(private val mActivity: FragmentActivity, private val recycler: RecyclerView?) : LifecycleEventObserver {
-    private val data by lazy { ConcurrentHashMap<Int, String>() }
-
-    init {
-        mActivity.lifecycle.addObserver(this)
-    }
-
-    /**
-     * 列表内部调取
-     * 适配器初始化传入helper，然后内部调取setUpLazy方法
-     */
-    @Synchronized
-    fun setUpLazy(player: StandardGSYVideoPlayer?, url: String, position: Int) {
-        //每一项都会调取dataManager
-        data[position] = url
-        //设置每个列表的url
-        player?.setUpLazy(url, true, null, null, "这是title")
-        //增加title
-        player?.titleTextView?.gone()
-        //设置返回键
-        player?.backButton?.gone()
-        //设置全屏按键功能
-        player?.fullscreenButton?.click {
-            player.startWindowFullscreen(player.context, false, true)
-        }
-        //防止错位设置(下标+url)
-        player?.playTag = "${position}::${url}"
-        player?.playPosition = position
-        //是否根据视频尺寸，自动选择竖屏全屏或者横屏全屏
-        player?.isAutoFullWithSize = true
-        //音频焦点冲突时是否释放
-        player?.isReleaseWhenLossAudio = false
-        //全屏动画
-        player?.isShowFullAnimation = true
-        //小屏时不触摸滑动
-        player?.setIsTouchWiget(false)
-    }
+object GSYVideoListHelper {
 
     /**
      * 设置列表监听
+     * //    /**
+     * //     * 全屏时写，写在系统的onBackPressed之前
+     * //     * @Override
+     * //     * public void onBackPressed() {
+     * //     * if (GSYVideoManager.backFromWindowFull(this)) {
+     * //     * return;
+     * //     * }
+     * //     * super.onBackPressed();
+     * //     * }
+     * //     */
+     * //    fun onBackPressed(): Boolean {
+     * //        return GSYVideoManager.backFromWindowFull(mActivity)
+     * //    }
      */
-    fun setOnScrollListener(listener: ((position: Int) -> Unit)) {
-        recycler?.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+    fun RecyclerView?.setOnScrollListener(activity: FragmentActivity, playTag: String, listener: ((position: Int) -> Unit)) {
+        this ?: return
+        /**
+         * 如果不同页面的播放器设置了不同的 playTag：releaseAllVideos() 只会释放「当前 playTag 关联的播放器」，不会影响其他页面的视频。
+         * 如果所有页面共用同一个 playTag（或未设置）：releaseAllVideos() 会释放全局所有播放器，包括其他页面的视频。
+         */
+        activity.lifecycle.addObserver(object : LifecycleEventObserver {
+            override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+                when (event) {
+                    Lifecycle.Event.ON_RESUME -> {
+                        GSYVideoManager.onResume()
+                    }
+                    Lifecycle.Event.ON_PAUSE -> {
+                        GSYVideoManager.onPause()
+                    }
+                    Lifecycle.Event.ON_DESTROY -> {
+                        GSYVideoManager.releaseAllVideos()
+                        clearOnScrollListeners()
+                        source.lifecycle.removeObserver(this)
+                    }
+                    else -> {}
+                }
+            }
+        })
+        addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
                 val layoutManager = recyclerView.layoutManager as? GridLayoutManager
@@ -77,19 +76,17 @@ class GSYVideoListHelper(private val mActivity: FragmentActivity, private val re
                 val visibleItemCount = max(0, layoutManager?.childCount.orZero)
                 // 获取最后一个可见的item的索引
                 val lastVisibleItem = firstVisibleItem + visibleItemCount - 1
-                //当前播放的位置
+                // 当前播放的位置
                 val position = GSYVideoManager.instance().playPosition
-                //大于0说明有播放
+                // 大于0说明有播放
                 if (position >= 0) {
-                    //当前播放tag
-                    val playTag = data[position]
-                    //对应的播放列表TAG
+                    // 对应的播放列表TAG
                     if (GSYVideoManager.instance().playTag.equals(playTag) && (position < firstVisibleItem || position > lastVisibleItem)) {
-                        if (GSYVideoManager.isFullState(mActivity)) {
+                        if (GSYVideoManager.isFullState(activity)) {
                             return
                         }
-                        //如果滑出去了上面和下面就是否，和今日头条一样
-//                        GSYVideoManager.releaseAllVideos()
+                        // 如果滑出去了上面和下面就是否，和今日头条一样
+                        GSYVideoManager.releaseAllVideos()
 //                        adapter.notifyDataSetChanged()
                         listener.invoke(position)
                     }
@@ -99,41 +96,37 @@ class GSYVideoListHelper(private val mActivity: FragmentActivity, private val re
     }
 
     /**
-     * 全屏时写，写在系统的onBackPressed之前
-     * @Override
-     * public void onBackPressed() {
-     * if (GSYVideoManager.backFromWindowFull(this)) {
-     * return;
-     * }
-     * super.onBackPressed();
-     * }
+     * 列表内部每一个选项都会调取
+     * 1.视频列表切记不能带有删除功能,如果有的情况下,部分方法需要重构
+     * 2.适配器初始化传入helper，然后内部调取setUpLazy方法
+     *
+     * @playTag
+     * 在 GSYVideoPlayer 的列表播放场景中，playTag 的核心作用是标识「当前播放所属的列表或播放组」，用于区分不同列表或不同播放场景的视频，避免播放状态混乱。
+     * 一个列表（或一个独立的播放场景）使用唯一的 playTag，而不是每个 Item 单独设置不同的 Tag。
      */
-    fun onBackPressed(): Boolean {
-        return GSYVideoManager.backFromWindowFull(mActivity)
-    }
-
-    /**
-     * 页面全局刷新时清空本地data后再覆盖赋值
-     */
-    fun onRefresh() {
-        data.clear()
-    }
-
-    /**
-     * 生命周期监听
-     */
-    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
-        when (event) {
-            Lifecycle.Event.ON_RESUME -> GSYVideoManager.onResume()
-            Lifecycle.Event.ON_PAUSE -> GSYVideoManager.onPause()
-            Lifecycle.Event.ON_DESTROY -> {
-                GSYVideoManager.releaseAllVideos()
-                recycler?.clearOnScrollListeners()
-                data.clear()
-                mActivity.lifecycle.removeObserver(this)
-            }
-            else -> {}
+    @Synchronized
+    fun setUpLazy(player: StandardGSYVideoPlayer?, url: String, position: Int, playTag: String) {
+        // 设置每个列表的url
+        player?.setUpLazy(url, true, null, null, "这是title")
+        // 增加title
+        player?.titleTextView?.gone()
+        // 设置返回键
+        player?.backButton?.gone()
+        // 设置全屏按键功能
+        player?.fullscreenButton?.click {
+            player.startWindowFullscreen(player.context, false, true)
         }
+        // 防止错位设置(针对列表所带的页面)
+        player?.playTag = playTag
+        player?.playPosition = position
+        // 是否根据视频尺寸，自动选择竖屏全屏或者横屏全屏
+        player?.isAutoFullWithSize = true
+        // 音频焦点冲突时是否释放
+        player?.isReleaseWhenLossAudio = false
+        // 全屏动画
+        player?.isShowFullAnimation = true
+        // 小屏时不触摸滑动
+        player?.setIsTouchWiget(false)
     }
 
 }
