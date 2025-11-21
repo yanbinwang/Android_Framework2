62dabda283a59c290b1855b20a99abc5c9fc5bbb
GoudanYan <wangyanbin_29@126.com>
2025-11-21 10:59:44 +0800

    完善相册

--- lib_common/src/main/java/com/example/common/network/repository/ApiResponse.kt
+++ lib_common/src/main/java/com/example/common/network/repository/ApiResponse.kt
@@ -6,14 +6,14 @@ package com.example.common.network.repository
  * 接口外层地址（与服务器约定好对应格式）
  */
 data class ApiResponse<T>(
-    var success: Boolean? = null,//后台这个参数和errCode都成功才算请求成功
-    var errCode: String? = null,//请求状态码，只做成功判断，失败后会判断statusCode
-    var errMessage: String? = null,//请求信息，不拿取只承接
-    var data: T? = null,//数据
-    //失败接口特有，请求失败时会返回
-    var statusCode: Int? = null,//错误状态码
-    var content: String? = null,//错误信息
-    //分页接口特有，在分页回调的时候会返回对应的值
+    var success: Boolean? = null, // 后台这个参数和errCode都成功才算请求成功
+    var errCode: String? = null, // 请求状态码，只做成功判断，失败后会判断statusCode
+    var errMessage: String? = null, // 请求信息，不拿取只承接
+    var data: T? = null, // 数据
+    // 失败接口特有，请求失败时会返回
+    var statusCode: Int? = null, // 错误状态码
+    var content: String? = null, // 错误信息
+    // 分页接口特有，在分页回调的时候会返回对应的值
     var totalCount: Int? = null
 )
 
--- lib_common/src/main/java/com/example/common/network/repository/ResponseWrapper.kt
+++ lib_common/src/main/java/com/example/common/network/repository/ResponseWrapper.kt
@@ -4,7 +4,7 @@ package com.example.common.network.repository
  * 交互失败统一返回的对象
  */
 data class ResponseWrapper(
-    var errCode: String? = null,//状态码（和code本质没区别，多了自定义的FAILURE->-1）
-    var errMessage: String? = null,//错误信息
-    var throwable: Exception? = null,//异常信息
+    var errCode: String? = null, // 状态码（和code本质没区别，多了自定义的FAILURE->-1）
+    var errMessage: String? = null, // 错误信息
+    var throwable: Exception? = null, // 异常信息
 ) : Throwable()
