f5f77cc0820c8d8702b9dd011770bad4e58817e1
GoudanYan <wangyanbin_29@126.com>
2025-11-30 10:55:51 +0800

    完善相册

--- lib_common/src/main/java/com/example/common/config/CacheData.kt
+++ lib_common/src/main/java/com/example/common/config/CacheData.kt
@@ -11,19 +11,19 @@ import com.example.common.utils.DataStringCache
  * @author yan
  */
 object CacheData {
-    //设备id
+    // 设备id
     private const val DEVICE_ID = "device_id"
     internal val deviceId = DataStringCache(DEVICE_ID)
 
-    //是否同意告知书
+    // 是否同意告知书
     private const val PRIVACY_AGREED = "privacy_agreed"
     internal val privacyAgreed = DataBooleanCache(PRIVACY_AGREED)
 
-    //用户类
+    // 用户类
     private const val USER_BEAN = "user_bean"
     internal val userBean = DataParcelableCache(USER_BEAN, UserBean::class.java)
 
-    //用户信息类
+    // 用户信息类
     private val USER_INFO_BEAN = "user_info_bean"
     internal val userInfoBean = DataParcelableCache(USER_INFO_BEAN, UserInfoBean::class.java)
 
