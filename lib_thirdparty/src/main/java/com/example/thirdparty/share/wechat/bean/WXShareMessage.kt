e7166486bcbaecffa8b176e59f22085180ceb8eb
GoudanYan <wangyanbin_29@126.com>
2026-03-07 17:13:50 +0800

    完善相册

--- lib_thirdparty/src/main/java/com/example/thirdparty/share/wechat/bean/WXShareMessage.kt
+++ lib_thirdparty/src/main/java/com/example/thirdparty/share/wechat/bean/WXShareMessage.kt
@@ -5,7 +5,7 @@ package com.example.thirdparty.share.wechat.bean
  * 针对WXMediaMessage构建
  */
 class WXShareMessage(
-    var title: String? = null,//消息标题->限制长度不超过 512Bytes
-    var description: String? = null,//消息描述->限制长度不超过 1KB
-    var messageExt: String? = null//微信跳回应用时会带上
+    var title: String? = null, // 消息标题->限制长度不超过 512Bytes
+    var description: String? = null, // 消息描述->限制长度不超过 1KB
+    var messageExt: String? = null // 微信跳回应用时会带上
 )
