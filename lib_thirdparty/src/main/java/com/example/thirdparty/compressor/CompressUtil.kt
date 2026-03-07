38bf45431f1a1912d144625f5bb931c150f37fc4
GoudanYan <wangyanbin_29@126.com>
2026-03-07 15:05:50 +0800

    完善相册

--- lib_thirdparty/src/main/java/com/example/thirdparty/compressor/CompressUtil.kt
+++ lib_thirdparty/src/main/java/com/example/thirdparty/compressor/CompressUtil.kt
@@ -1,11 +1,13 @@
 package com.example.thirdparty.compressor
 
+import android.content.Context
 import android.graphics.Bitmap
 import com.example.common.BaseApplication
 import com.example.common.utils.builder.shortToast
 import com.example.common.utils.i18n.string
 import com.example.thirdparty.R
 import com.example.thirdparty.firebase.utils.CrashlyticsUtil
+import com.example.thirdparty.utils.CompressUtil
 import id.zelory.compressor.Compressor
 import id.zelory.compressor.constraint.format
 import id.zelory.compressor.constraint.quality
@@ -19,27 +21,29 @@ object CompressUtil {
     private var resWidth = 1000
     private var resHeight = 1000
 
-    suspend fun compressFile(fileName: String?): File? {
-        fileName ?: return null
-        return compressFile(File(fileName))
+    suspend fun compressFile(context: Context, pathname: String?): File {
+        pathname ?: throw RuntimeException("文件路径为空")
+        return CompressUtil.compressFile(context, File(pathname))
     }
 
-    suspend fun compressFile(file: File?): File? {
-        file ?: return null
+    suspend fun compressFile(context: Context, file: File?): File {
+        file ?: throw RuntimeException("文件为空")
+        if (file.length() > 10 * 1024 * 1024) {
+            throw RuntimeException(string(R.string.compressError))
+        }
+        // 内部已切换到了io线程
         return try {
-//            file ?: throw Exception()
-            if (file.length() > 10 * 1024 * 1024) {
-                string(R.string.compressError).shortToast()
-                return null
-            }
-            Compressor.compress(BaseApplication.instance, file) {
+            Compressor.compress(context, file) {
+                // 限制压缩后图片的最大分辨率 宽度不会超过 resWidth，高度不会超过 resHeight
                 resolution(resWidth, resHeight)
+                // 控制图片压缩时的质量保留程度
                 quality(80)
+                // 指定压缩后图片的输出格式
                 format(Bitmap.CompressFormat.JPEG)
             }
         } catch (e: Exception) {
             CrashlyticsUtil.recordException("compressFile", e)
-            null
+            throw e
         }
     }
 
