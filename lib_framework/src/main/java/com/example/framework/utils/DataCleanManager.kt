280b3629898dca892f48989ee68e2f7088b7d6bd
GoudanYan <wangyanbin_29@126.com>
2026-03-16 09:59:17 +0800

    完善相册

--- lib_framework/src/main/java/com/example/framework/utils/DataCleanManager.kt
+++ lib_framework/src/main/java/com/example/framework/utils/DataCleanManager.kt
@@ -14,13 +14,13 @@ import java.io.File
  * launch {
  * flow {
  * withContext(IO) { mContext?.cleanInternalCache() }
- * emit(retrieveCacheFormatters(mContext))
+ * emit(getFormattedCacheSize(mContext))
  * }.withHandling(mView, isShowToast = true).collect {
  * cache.postValue(it)
  * }
  * }.manageJob()
  * } else {
- * cache.postValue(retrieveCacheFormatters(mContext))
+ * cache.postValue(getFormattedCacheSize(mContext))
  * }
  */
 
