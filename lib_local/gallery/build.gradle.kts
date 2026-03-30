631942b8e8513f5443e2144ce845f46d2566002f
GoudanYan <wangyanbin_29@126.com>
2026-03-30 18:06:51 +0800

    完善相册

--- lib_local/gallery/build.gradle.kts
+++ lib_local/gallery/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 @Suppress("DEPRECATION")
@@ -29,6 +30,10 @@ android {
     defaultConfig {
         minSdk = libs.versions.minSdkVersion.get().toInt()
         testInstrumentationRunner = libs.versions.testInstrumentationRunner.get()
+    }
+
+    buildFeatures {
+        dataBinding = true
     }
 
     kotlin {
---
+++ lib_local/gallery/src/main/java/com/example/gallery/activity/DurbanActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gallery.activity
+
+import com.example.common.base.BaseTitleActivity
+
+class DurbanActivity : BaseTitleActivity<>{
+}
