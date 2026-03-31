2547e4174de933162c6fdb68828242badb199194
GoudanYan <wangyanbin_29@126.com>
2026-03-31 15:11:39 +0800

    完善相册

--- lib_local/gallery/src/main/java/com/example/gallery/base/source/ActivitySource.kt
+++ lib_local/gallery/src/main/java/com/example/gallery/base/source/ActivitySource.kt
@@ -13,6 +13,7 @@ import androidx.appcompat.widget.Toolbar
 import com.example.common.utils.function.orEmpty
 import com.example.framework.utils.function.drawable
 import com.example.gallery.R
+import com.example.gallery.base.BaseActivity.Companion.setSupportToolbar
 
 /**
  * Activity 载体实现类
@@ -134,7 +135,7 @@ class ActivitySource(activity: Activity) : Source<Activity>(activity) {
     override fun prepare() {
         mHost.findViewById<Toolbar>(R.id.toolbar).let { toolbar ->
             setActionBar(toolbar)
-//            setSupportToolbar(toolbar)
+            setSupportToolbar(toolbar)
         }
     }
 
