923a00858243649dde18f13f9b6af04a915a4b08
GoudanYan <wangyanbin_29@126.com>
2026-05-28 10:14:48 +0800

    完善相册

--- lib_common/src/main/java/com/example/common/widget/textview/TimerTextView.kt
+++ lib_common/src/main/java/com/example/common/widget/textview/TimerTextView.kt
@@ -41,7 +41,7 @@ class TimerTextView @JvmOverloads constructor(context: Context, attrs: Attribute
         timerBuilder?.stopCountDown(timerTag)
     }
 
-    fun start(tag: String? = "", second: Int = 60) {
+    fun start(tag: String? = "", millisInFuture: Int = 60) {
         if (!tag.isNullOrEmpty()) timerTag = tag
         timerBuilder?.startCountDown(timerTag, {
             disable()
@@ -49,7 +49,7 @@ class TimerTextView @JvmOverloads constructor(context: Context, attrs: Attribute
         }, {
             enable()
             text = i18String(R.string.timerDisabled)
-        }, second.second)
+        }, millisInFuture.second)
     }
 
 }
