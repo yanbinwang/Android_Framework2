81367bb29463d248cbc74a6965b1f31f813e5f76
GoudanYan <wangyanbin_29@126.com>
2026-06-30 13:28:38 +0800

    完善相册

--- lib_common/src/main/java/com/example/common/event/EventCode.kt
+++ lib_common/src/main/java/com/example/common/event/EventCode.kt
@@ -3,34 +3,34 @@ package com.example.common.event
 import com.example.common.bean.UserInfoBean
 
 object EventCode {
-    //用户登录
+    // 用户登录
     val EVENT_USER_LOGIN = Code<Any>()
 
-    //用户注销
+    // 用户注销
     val EVENT_USER_LOGIN_OUT = Code<Any>()
 
-    //用户个人信息刷新
+    // 用户个人信息刷新
     val EVENT_USER_INFO_REFRESH = Code<UserInfoBean?>()
 
-    //联网
+    // 联网
     val EVENT_ONLINE = Code<Any>()
 
-    //断网
+    // 断网
     val EVENT_OFFLINE = Code<Any>()
 
-    //按下Home键,菜单键,电源键
+    // 按下Home键,菜单键,电源键
     val EVENT_MENU_ACTION = Code<Any>()
 
-    //证据列表更新（返回fileType）
+    // 证据列表更新（返回fileType）
     val EVENT_EVIDENCE_UPDATE = Code<String>()
 
-    //支付成功
+    // 支付成功
     val EVENT_PAY_SUCCESS = Code<Any>()
 
-    //支付取消
+    // 支付取消
     val EVENT_PAY_CANCEL = Code<Any>()
 
-    //支付失败
+    // 支付失败
     val EVENT_PAY_FAILURE = Code<Any>()
 
 }
