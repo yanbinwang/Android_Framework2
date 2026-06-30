73395f38d4b4628dc544aeaf948d8a033f7f366d
GoudanYan <wangyanbin_29@126.com>
2026-06-30 13:29:31 +0800

    完善相册

--- lib_common/src/main/java/com/example/common/event/EventCode.kt
+++ lib_common/src/main/java/com/example/common/event/EventCode.kt
@@ -3,22 +3,22 @@ package com.example.common.event
 import com.example.common.bean.UserInfoBean
 
 object EventCode {
-    //語言切換
+    // 語言切換
     val EVENT_LANGUAGE_CHANGE = Code<Any>()
 
-    //联网
+    // 联网
     val EVENT_ONLINE = Code<Any>()
 
-    //断网
+    // 断网
     val EVENT_OFFLINE = Code<Any>()
 
-    //用户登录
+    // 用户登录
     val EVENT_USER_LOGIN = Code<Any>()
 
-    //用户注销
+    // 用户注销
     val EVENT_USER_LOGIN_OUT = Code<Any>()
 
-    //用户个人信息刷新
+    // 用户个人信息刷新
     val EVENT_USER_INFO_REFRESH = Code<UserInfoBean?>()
 
 }
