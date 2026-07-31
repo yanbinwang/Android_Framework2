85ba568676879ccb735cdf039c2d9fd3315721b9
GoudanYan <wangyanbin_29@126.com>
2026-07-31 17:06:39 +0800

    完善相册

--- lib_common/src/main/java/com/example/common/BaseApplication.kt
+++ lib_common/src/main/java/com/example/common/BaseApplication.kt
@@ -36,6 +36,7 @@ import com.example.common.utils.i18n.i18String
 import com.example.common.utils.manager.AppManager
 import com.example.common.widget.xrecyclerview.refresh.ProjectRefreshFooter
 import com.example.common.widget.xrecyclerview.refresh.ProjectRefreshHeader
+import com.example.framework.utils.function.serviceStateMap
 import com.example.framework.utils.function.value.DateFormat.clearThreadLocalCache
 import com.example.framework.utils.function.value.DateFormat.resetServiceTime
 import com.example.framework.utils.function.value.isDebug
@@ -96,6 +97,8 @@ abstract class BaseApplication : Application() {
         // 初次赋值
         lastClickTime.set(SystemClock.elapsedRealtime())
         isFirstLaunch.set(true)
+        // 进程重启后，所有服务必然已停止，直接清空
+        serviceStateMap.clear()
         // 布局初始化
         AutoSizeConfig.getInstance()
             .setBaseOnWidth(true)
