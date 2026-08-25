f26e7da3d215ecac2410c380298f1525a6793403
GoudanYan <wangyanbin_29@126.com>
2026-08-25 15:21:35 +0800

    完善相册

--- lib_local/glide/build.gradle.kts
+++ lib_local/glide/build.gradle.kts
@@ -54,8 +54,7 @@ dependencies {
 //    api("androidx.palette:palette:1.0.0")
     // Glide 依赖
     api("com.github.bumptech.glide:glide:5.0.9")
-//    kapt("com.github.bumptech.glide:compiler:5.0.5")
     // OkHttp 依赖
-    api("com.squareup.okhttp3:okhttp:5.4.0")
+    api("com.squareup.okhttp3:okhttp:5.5.0")
     implementation("com.github.bumptech.glide:okhttp3-integration:5.0.9")
 }
