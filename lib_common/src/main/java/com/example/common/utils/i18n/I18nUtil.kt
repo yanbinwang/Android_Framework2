afe48c256c8e7fafdc81c52ea6994b8f034e7a59
GoudanYan <wangyanbin_29@126.com>
2026-09-14 19:23:58 +0800

    完善相册

--- lib_common/src/main/java/com/example/common/utils/i18n/I18nUtil.kt
+++ lib_common/src/main/java/com/example/common/utils/i18n/I18nUtil.kt
@@ -176,7 +176,7 @@ object I18nUtil {
      * 讀取 assets下的 font字體文件
      */
     fun getLanguageBeanFromAsset(language: String): LanguageBean? {
-        val pack = LanguageUtil.getLanguageFromAsset(language)
+        val pack = LanguageUtil.getLanguagePackFromAsset(language)
         val assetManager = BaseApplication.instance.applicationContext.assets
         return try {
             assetManager.open(pack).use { stream ->
