a5ebc8d0dc2107206fafc713eb82720e896a825f
GoudanYan <wangyanbin_29@126.com>
2026-09-24 09:11:37 +0800

    完善相册

--- lib_local/gallery/src/main/java/com/example/gallery/widget/ColorProgressBar.kt
+++ lib_local/gallery/src/main/java/com/example/gallery/widget/ColorProgressBar.kt
@@ -2,6 +2,7 @@ package com.example.gallery.widget
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.widget.ProgressBar
 import androidx.annotation.ColorInt
 import com.example.framework.utils.function.drawable
@@ -9,17 +10,6 @@ import com.example.gallery.R
 
 /**
  * 可自定义颜色的 ProgressBar
- * override fun onVisibilityChanged(changedView: View, visibility: Int) {
- *     super.onVisibilityChanged(changedView, visibility)
- *     // visibility: VISIBLE / INVISIBLE / GONE
- *     // changedView: 触发变化的 View（可能是自身或子 View）
- *     if (changedView === this) {
- *         when (visibility) {
- *             View.VISIBLE -> onBecameVisible()
- *             View.GONE, View.INVISIBLE -> onBecameHidden()
- *         }
- *     }
- * }
  */
 class ColorProgressBar @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : ProgressBar(context, attrs, defStyleAttr) {
 
@@ -29,12 +19,30 @@ class ColorProgressBar @JvmOverloads constructor(context: Context, attrs: Attrib
         indeterminateDrawable = loadingDrawable
     }
 
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        // 页面销毁兜底，停止动画释放资源
+        isIndeterminate = false
+    }
+
+    override fun onVisibilityChanged(changedView: View, visibility: Int) {
+        super.onVisibilityChanged(changedView, visibility)
+        if (changedView === this) {
+            when (visibility) {
+                VISIBLE -> isIndeterminate = true
+                GONE, INVISIBLE -> isIndeterminate = false
+            }
+        }
+    }
+
     /**
      * 给加载条设置颜色
      */
     fun setColorFilter(@ColorInt color: Int) {
         // 获取系统自带的旋转动画条 / mutate() 让这个 Drawable 独立，不影响其他地方的 ProgressBar / 着色后设置回去
-        indeterminateDrawable?.mutate()?.setTint(color)
+        indeterminateDrawable = indeterminateDrawable?.mutate()?.apply {
+            setTint(color)
+        }
     }
 
 }
